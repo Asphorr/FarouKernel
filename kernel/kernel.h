7571dda851f06a946873c70c6e07ef7520353cec
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-10-09 17:32:33 +0700

    Update kernel.h

--- kernel/kernel.h
+++ kernel/kernel.h
@@ -2,17 +2,20 @@
 #include <stdbool.h>
 #include <string.h>
 
+// Define macros for null values
 #define THREAD_NULL ((struct thread *)0x0)
 #define PROCESS_NULL ((struct process *)0x0)
 #define MEMORY_REGION_NULL ((struct memory_region *)0x0)
 
-typedef unsigned int uint;
-typedef signed int sint;
-typedef unsigned char uchar;
-typedef signed char schar;
+// Use standard fixed-width integer types
+typedef uint32_t uint;
+typedef int32_t sint;
+typedef uint8_t uchar;
+typedef int8_t schar;
 typedef float fp;
 typedef double dp;
 
+// Structure definitions
 struct thread {
     void (*func)(void);
     struct thread *next;
@@ -29,61 +32,175 @@ struct memory_region {
     size_t size;
 };
 
-static inline void kernel_init() {}
-static inline void kernel_exit() {}
+// Function prototypes
+static inline void kernel_init();
+static inline void kernel_exit();
+static inline int kernel_create_process(void (*entry)(void));
+static inline int kernel_create_thread(void (*func)(void));
+static inline void kernel_yield();
+static inline void kernel_sleep(uint milliseconds);
+static inline void kernel_wakeup(struct thread *thread);
+static inline void kernel_terminate_process(struct process *process);
+static inline void kernel_free_memory(struct memory_region *region);
+static inline void* kmalloc(size_t size);
+static inline void kfree(void* ptr);
+static inline void kio(void* buffer, size_t size, bool is_read);
 
+// Global variables
+extern volatile uint64_t ticks;
+extern volatile uint64_t timer_freq;
+extern volatile uint64_t timer_count;
+extern volatile uint64_t timer_overflow;
+
+// Kernel initialization function
+static inline void kernel_init() {
+    // Initialize the tick count and frequency
+    ticks = 0;
+    timer_freq = 1000;
+    timer_count = 0;
+    timer_overflow = 0;
+}
+
+// Kernel exit function
+static inline void kernel_exit() {
+    // Nothing to do here
+}
+
+// Create a new process
 static inline int kernel_create_process(void (*entry)(void)) {
+    // Allocate space for the process structure
     struct process *process = malloc(sizeof(*process));
     if (!process) {
         return -1;
     }
+    
+    // Set up the process structure
     process->entry = entry;
     process->threads = NULL;
     process->pid = 0;
+    
+    // Return success
     return 0;
 }
 
+// Create a new thread in the current process
 static inline int kernel_create_thread(void (*func)(void)) {
+    // Get the current process
+    struct process *current_process = get_current_process();
+    if (!current_process) {
+        return -1;
+    }
+    
+    // Allocate space for the thread structure
     struct thread *thread = malloc(sizeof(*thread));
     if (!thread) {
         return -1;
     }
+    
+    // Set up the thread structure
     thread->func = func;
     thread->next = NULL;
+    
+    // Add the thread to the list of threads in the current process
+    thread->next = current_process->threads;
+    current_process->threads = thread;
+    
+    // Return success
     return 0;
 }
 
-static inline void kernel_yield() {}
+// Yield control back to the kernel
+static inline void kernel_yield() {
+    // Save the current context
+    save_context();
+    
+    // Switch to the next ready task
+    switch_to_ready_task();
+    
+    // Restore the saved context
+    restore_context();
+}
 
-static inline void kernel_sleep(uint milliseconds) {}
+// Sleep for a specified number of milliseconds
+static inline void kernel_sleep(uint milliseconds) {
+    // Calculate the number of ticks to sleep
+    uint64_t num_ticks = milliseconds / timer_freq;
+    
+    // Check if there are any tasks waiting to run
+    while (num_ticks-- && !is_idle()) {
+        // Wait for the next tick
+        wait_tick();
+        
+        // Update the timer overflow variable
+        update_timer_overflow();
+    }
+}
 
-static inline void kernel_wakeup(struct thread *thread) {}
+// Wake up a specific thread
+static inline void kernel_wakeup(struct thread *thread) {
+    // Mark the thread as ready to run
+    thread->state = READY;
+    
+    // Add the thread to the ready queue
+    add_to_ready_queue(thread);
+}
 
+// Terminate a process
 static inline void kernel_terminate_process(struct process *process) {
+    // Free the process structure
     free(process);
 }
 
+// Free a block of memory allocated by kmalloc
 static inline void kernel_free_memory(struct memory_region *region) {
+    // Free the memory region
     free(region->base);
 }
 
+// Allocate a block of memory using kmalloc
 static inline void* kmalloc(size_t size) {
-    void* ptr = NULL;
-    if (size > 0) {
-        ptr = (void*)((uintptr_t)heap + heap_size - size);
-        heap_size -= size;
+    // Check if the requested size is valid
+    if (size == 0 || size >= HEAP_SIZE) {
+        return NULL;
     }
-    return ptr;
+    
+    // Find the first available block of memory that fits the request
+    struct memory_block *block = find_first_fit(size);
+    if (!block) {
+        return NULL;
+    }
+    
+    // Split the block into two smaller blocks if necessary
+    split_block(block, size);
+    
+    // Mark the block as allocated
+    block->flags |= BLOCK_ALLOCATED;
+    
+    // Return a pointer to the start of the block
+    return block->start;
 }
 
+// Free a block of memory allocated by kmalloc
 static inline void kfree(void* ptr) {
-    if (ptr != NULL) {
-        size_t size = (size_t)((uintptr_t)ptr - (uintptr_t)heap);
-        heap_size += size;
-        memset(ptr, 0, size);
+    // Check if the pointer is within the range of the heap
+    if (ptr < heap || ptr >= heap + HEAP_SIZE) {
+        return;
     }
+    
+    // Find the corresponding memory block
+    struct memory_block *block = find_block(ptr);
+    if (!block) {
+        return;
+    }
+    
+    // Mark the block as unused
+    block->flags &= ~BLOCK_ALLOCATED;
+    
+    // Coalesce adjacent free blocks
+    coalesce_blocks(block);
 }
 
+// Perform I/O operations
 static inline void kio(void* buffer, size_t size, bool is_read) {
-    // Todo: Implement I/O operations here
+    // TODO: Implement I/O operations here
 }
