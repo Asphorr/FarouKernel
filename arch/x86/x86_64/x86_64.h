4eaa6c8df65a98b98c3fab71915d2bd738814fa0
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-10-09 17:01:38 +0700

    Update x86_64.h

--- arch/x86/x86_64/x86_64.h
+++ arch/x86/x86_64/x86_64.h
@@ -4,42 +4,44 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-/* Instruction sets */
-#define X86_64_INSTR_SET_SSE   (1 << 0)
-#define X86_64_INSTR_SET_SSE2  (1 << 1)
-#define X86_64_INSTR_SET_SSE3  (1 << 2)
-#define X86_64_INSTR_SET_SSSE3 (1 << 3)
-#define X86_64_INSTR_SET_SSE4_1 (1 << 4)
-#define X86_64_INSTR_SET_SSE4_2 (1 << 5)
-#define X86_64_INSTR_SET_AVX   (1 << 6)
-#define X86_64_INSTR_SET_AVX2  (1 << 7)
-#define X86_64_INSTR_SET_FMA   (1 << 8)
-#define X86_64_INSTR_SET_FMA4  (1 << 9)
-#define X86_64_INSTR_SET_FMA3  (1 << 10)
-#define X86_64_INSTR_SET_AVX512F (1 << 11)
-#define X86_64_INSTR_SET_AVX512CD (1 << 12)
-#define X86_64_INSTR_SET_AVX512ER (1 << 13)
-#define X86_64_INSTR_SET_AVX512PF (1 << 14)
-#define X86_64_INSTR_SET_AVX512EF (1 << 15)
+// Define instruction set flags
+#define X86_64_INSTR_SET_SSE      (1U << 0)
+#define X86_64_INSTR_SET_SSE2     (1U << 1)
+#define X86_64_INSTR_SET_SSE3     (1U << 2)
+#define X86_64_INSTR_SET_SSSE3    (1U << 3)
+#define X86_64_INSTR_SET_SSE4_1   (1U << 4)
+#define X86_64_INSTR_SET_SSE4_2   (1U << 5)
+#define X86_64_INSTR_SET_AVX      (1U << 6)
+#define X86_64_INSTR_SET_AVX2     (1U << 7)
+#define X86_64_INSTR_SET_FMA      (1U << 8)
+#define X86_64_INSTR_SET_FMA4     (1U << 9)
+#define X86_64_INSTR_SET_FMA3     (1U << 10)
+#define X86_64_INSTR_SET_AVX512F  (1U << 11)
+#define X86_64_INSTR_SET_AVX512CD (1U << 12)
+#define X86_64_INSTR_SET_AVX512ER (1U << 13)
+#define X86_64_INSTR_SET_AVX512PF (1U << 14)
+#define X86_64_INSTR_SET_AVX512EF (1U << 15)
 
-/* Features */
-#define X86_64_FEATURE_FP     (1 << 0)
-#define X86_64_FEATURE_ASM    (1 << 1)
-#define X86_64_FEATURE_AVX    (1 << 2)
-#define X86_64_FEATURE_AES    (1 << 3)
-#define X86_64_FEATURE_RDRND  (1 << 4)
-#define X86_64_FEATURE_FMA    (1 << 5)
-#define X86_64_FEATURE_CVT16  (1 << 6)
-#define X86_64_FEATURE_MOVBE  (1 << 7)
+// Define feature flags
+#define X86_64_FEATURE_FP        (1U << 0)
+#define X86_64_FEATURE_ASM       (1U << 1)
+#define X86_64_FEATURE_AVX       (1U << 2)
+#define X86_64_FEATURE_AES       (1U << 3)
+#define X86_64_FEATURE_RDRND     (1U << 4)
+#define X86_64_FEATURE_FMA       (1U << 5)
+#define X86_64_FEATURE_CVT16     (1U << 6)
+#define X86_64_FEATURE_MOVBE     (1U << 7)
 
-struct x86_64_cpuid {
+// Structure for storing CPUID information
+typedef struct {
     uint32_t vendor_id;
     uint32_t device_id;
     uint32_t revision;
     uint32_t features;
     uint32_t instruction_sets[4];
-};
+} x86_64_cpuid_t;
 
-extern struct x86_64_cpuid x86_64_cpuid;
+// Global variable for accessing CPUID information
+x86_64_cpuid_t x86_64_cpuid;
 
-#endif /* X86_64_H */
+#endif // X86_64_H
