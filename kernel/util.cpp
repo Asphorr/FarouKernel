986142919c855c0d553e007c71a0633f3878548c
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-10-09 20:07:19 +0700

    Update util.cpp

--- kernel/util.cpp
+++ kernel/util.cpp
@@ -1,22 +1,43 @@
-#include "util.h"
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <iterator>
 
-using namespace std;
+template <typename T>
+struct UtilityFunctions {
+    static void printVector(const std::vector<T>& v) {
+        std::cout << '[';
+        std::copy(v.begin(), v.end(), std::ostream_iterator<T>(std::cout, ", "));
+        std::cout << ']';
+    }
+    
+    template <typename U>
+    static std::vector<U> getUniqueElements(const std::vector<U>& v) {
+        std::sort(v.begin(), v.end());
+        std::vector<U> result;
+        std::set_difference(v.begin(), v.end(), result.begin(), result.end());
+        return result;
+    }
+    
+    template <typename V>
+    static bool hasDuplicates(const std::vector<V>& v) {
+        return !std::is_permutation(v.begin(), v.end(), v.begin());
+    }
+};
 
 int main() {
-    // Create sample data
     constexpr size_t n = 10;
-    vector<int> vec(n);
-    iota(vec.begin(), vec.end(), 0);
-
-    // Call utility functions
-    util::printVector(vec);
-    auto uniqueVec = util::getUniqueElements(vec);
-    bool hasDups = util::hasDuplicates(uniqueVec);
-
-    // Output results
-    cout << "Unique elements: ";
-    util::printVector(uniqueVec);
-    cout << "Has duplicates: " << hasDups << endl;
-
+    std::vector<int> vec(n);
+    std::iota(vec.begin(), vec.end(), 0);
+    
+    UtilityFunctions<int>::printVector(vec);
+    auto uniqueVec = UtilityFunctions<int>::getUniqueElements(vec);
+    bool hasDups = UtilityFunctions<int>::hasDuplicates(uniqueVec);
+    
+    std::cout << "Unique elements: ";
+    UtilityFunctions<int>::printVector(uniqueVec);
+    std::cout << "Has duplicates: " << hasDups << '\n';
+    
     return 0;
 }
