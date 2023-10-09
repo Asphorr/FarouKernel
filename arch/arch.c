e8b90d5a804b15e5905c8e0230fc0e87ac87be78
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-10-09 17:08:20 +0700

    Update arch.c

--- arch/arch.c
+++ arch/arch.c
@@ -1,121 +1,140 @@
+#ifndef _ARCH_H
+#define _ARCH_H
+
 #include <stdint.h>
 #include <stdbool.h>
-#include "arch.h"
 
-// CPU architecture specific implementations
+/* Architecture-specific definitions */
 
-// Instruction set architecture (ISA)
-static inline void isa_assert(void) {
-    // Assertions for ISA-specific behavior
-}
+/* Instruction Set Architecture (ISA) /
+typedef enum {
+X86 = 1, / x86 family processors /
+AMD64, / AMD64/x86-64 family processors /
+ARMv7, / ARMv7 family processors /
+ARMv8, / ARMv8 family processors /
+RISCV, / RISC-V family processors */
+} arch_isa_e;
 
-static inline uint32_t isa_read_cr0(void) {
-    return 0; // Return a default value for CR0
-}
+extern const char *const arch_isa_names[];
 
-static inline void isa_write_cr0(uint32_t value) {
-    // Do nothing, CR0 is read-only
-}
+/* Endianness */
+typedef enum {
+LITTLE_ENDIAN = 1,
+BIG_ENDIAN,
+} endianness_e;
 
-static inline uint32_t isa_read_cr4(void) {
-    return 0; // Return a default value for CR4
-}
+/* Word size in bits */
+typedef enum {
+WORD_SIZE_32 = 1,
+WORD_SIZE_64,
+} word_size_e;
 
-static inline void isa_write_cr4(uint32_t value) {
-    // Do nothing, CR4 is read-only
-}
+/* Address Space Layout Randomization (ASLR) /
+typedef enum {
+NO_ASLR = 1, / No ASLR /
+FULL_ASLR, / Full ASLR /
+PARTIAL_ASLR, / Partial ASLR */
+} aslr_mode_e;
 
-// Endianness
-static inline bool is_little_endian(void) {
-    return ENDIANNESS_LITTLE;
-}
+/* Stack growth direction /
+typedef enum {
+UPWARD_GROWTH = 1, / Grows upward from low addresses /
+DOWNWARD_GROWTH, / Grows downward from high addresses */
+} stack_growth_dir_e;
 
-// Word size
-static inline uint32_t get_word_size(void) {
-    return WORD_SIZE;
-}
+/* Page table entry count /
+typedef enum {
+SMALL_PAGE_TABLE = 1, / Small page table /
+LARGE_PAGE_TABLE, / Large page table */
+} page_table_entry_count_e;
 
-// Address space layout
-static inline uint32_t get_address_space_layout(void) {
-    return ASLR;
-}
+/* Page table shift amount /
+typedef enum {
+SHIFT_AMOUNT_SMALL = 1, / Shift amount for small page tables /
+SHIFT_AMOUNT_LARGE, / Shift amount for large page tables */
+} page_table_shift_amount_e;
 
-// Stack layout
-static inline uint32_t get_stack_growth_direction(void) {
-    return STACK_GROWTH_DIRECTION;
-}
+/* Virtual memory start address /
+typedef enum {
+VIRTUAL_ADDRESS_SPACE_BEGIN = 1, / Start of virtual address space /
+VIRTUAL_ADDRESS_SPACE_END, / End of virtual address space */
+} virtual_mem_addr_e;
 
-// Page table layout
-static inline uint32_t get_page_table_entries(void) {
-    return PAGE_TABLE_ENTRIES;
-}
+/* Interrupt Controller Type /
+typedef enum {
+APIC = 1, / Advanced Programmable Interrupt Controller (APIC) /
+IOAPIC, / Input/Output Advanced Programmable Interrupt Controller (IOAPIC) /
+LOCAL_APIC, / Local Advanced Programmable Interrupt Controller (LAPIC) /
+HPET, / High Precision Event Timer (HPET) */
+} interrupt_controller_type_e;
 
-static inline uint32_t get_page_table_shift(void) {
-    return PAGE_TABLE_SHIFT;
-}
+/* Timekeeping frequency /
+typedef enum {
+CLOCK_TICKS_PER_SECOND = 1, / Number of clock ticks per second /
+CLOCK_TICKS_PER_MILLISECOND, / Number of clock ticks per millisecond */
+} timekeeping_freq_e;
 
-// Virtual memory layout
-static inline uint32_t get_virtual_memory_start(void) {
-    return VIRTUAL_MEMORY_START;
-}
+/* Console Output Buffer Size /
+typedef enum {
+CONSOLE_OUTPUT_BUFFER_SIZE_FIXED = 1, / Fixed size console output buffer /
+CONSOLE_OUTPUT_BUFFER_SIZE_VARIABLE, / Variable size console output buffer */
+} console_outbuf_size_e;
 
-static inline uint32_t get_virtual_memory_end(void) {
-    return VIRTUAL_MEMORY_END;
-}
+/* Networking Enabled Flag /
+typedef enum {
+NETWORKING_DISABLED = 1, / Networking disabled /
+NETWORKING_ENABLED, / Networking enabled */
+} networking_flag_e;
 
-// Interrupt controller
-static inline int ic_get_type(void) {
-    return INTERRUPT_CONTROLLER_APIC;
-}
+/* File System Type /
+typedef enum {
+FAT32 = 1, / FAT32 file system /
+EXT2, / ext2 file system /
+EXT3, / ext3 file system /
+EXT4, / ext4 file system /
+REISERFS, / ReiserFS file system /
+JFS, / Journaled File System (JFS) /
+XFS, / XFS file system /
+OCFS2, / Oracle Cluster Filesystem 2 (OCFS2) /
+BTRFS, / Btrfs file system /
+ZFS, / ZFS file system /
+UDF, / Universal Disk Format (UDF) /
+ISO9660, / ISO 9660 file system /
+HPFS, / High Performance File System (HPFS) /
+NTFS, / New Technology File System (NTFS) */
+} fs_type_e;
 
-// Timekeeping
-static inline uint32_t get_timekeeping_frequency(void) {
-    return TIMEKEEPING_FREQUENCY;
-}
+/* Memory Management Enabled Flag /
+typedef enum {
+MEMORY_MANAGEMENT_DISABLED = 1, / Memory management disabled /
+MEMORY_MANAGEMENT_ENABLED, / Memory management enabled */
+} mem_manage_flag_e;
 
-// Console output
-static inline void console_output_buffer_set_size(uint32_t size) {
-    // Do nothing, constant buffer size
-}
+/* Kernel Debugging Enabled Flag /
+typedef enum {
+DEBUGGING_DISABLED = 1, / Kernel debugging disabled /
+DEBUGGING_ENABLED, / Kernel debugging enabled */
+} kernel_debug_flag_e;
 
-static inline uint32_t console_output_buffer_get_size(void) {
-    return CONSOLE_OUTPUT_BUFFER_SIZE;
-}
-
-// Networking
-static inline bool is_networking_enabled(void) {
-    return NETWORKING_ENABLED;
-}
-
-// File system
-static inline uint32_t get_file_system_type(void) {
-    return FILE_SYSTEM_TYPE_FAT32;
-}
-
-// Memory management
-static inline bool is_memory_management_enabled(void) {
-    return MEMORY_MANAGEMENT_ENABLED;
-}
-
-// Kernel debugging
-static inline bool is_kernel_debugging_enabled(void) {
-    return KERNEL_DEBUGGING_ENABLED;
-}
-
-// Platform specific implementations
-
-// Define the platform specific instructions
-static inline void platform_specific_instructions(void) {
-    // Do nothing, no platform-specific instructions
-}
-
-// Define the platform specific data structures
+/* Platform Specific Data Structures */
 struct platform_specific_data {
-    uint32_t foo;
-    uint64_t bar;
-};
-
-// Define the platform specific functions
-void platform_specific_function(void);
-
-#endif // ARCH_C
+union {
+struct {
+uint32_t foo;
+uint64_t bar;
+} x86;
+struct {
+uint32_t baz;
+uint64_t quux;
+} amd64;
+struct {
+uint32_t corge;
+uint64_t grault;
+} armv7;
+struct {
+uint32_t garply;
+uint64_t waldo;
+} armv8;
+struct {
+uint32_t fred;
+uint64_t plugh;
