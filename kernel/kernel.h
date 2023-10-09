180f1814d44f30d05564fbc8d47087e87cf7611f
Asphorr <144871094+Asphorr@users.noreply.github.com>
2023-10-09 13:57:51 +0700

    Update kernel.h

--- kernel/kernel.h
+++ kernel/kernel.h
@@ -1,7 +1,11 @@
-#ifndef _KERNEL_H
-#define _KERNEL_H
+#include <stddef.h>
+#include <stdbool.h>
+#include <string.h>
 
-// Data Types
+#define THREAD_NULL ((struct thread *)0x0)
+#define PROCESS_NULL ((struct process *)0x0)
+#define MEMORY_REGION_NULL ((struct memory_region *)0x0)
+
 typedef unsigned int uint;
 typedef signed int sint;
 typedef unsigned char uchar;
@@ -9,7 +13,6 @@ typedef signed char schar;
 typedef float fp;
 typedef double dp;
 
-// Basic Structures
 struct thread {
     void (*func)(void);
     struct thread *next;
@@ -26,25 +29,45 @@ struct memory_region {
     size_t size;
 };
 
-// Function Prototypes
-void kernel_init(void);
-void kernel_exit(void);
-int kernel_create_process(void (*entry)(void));
-int kernel_create_thread(void (*func)(void));
-void kernel_yield(void);
-void kernel_sleep(uint milliseconds);
-void kernel_wakeup(struct thread *thread);
-void kernel_terminate_process(struct process *process);
-void kernel_free_memory(struct memory_region *region);
+static inline void kernel_init() {}
+static inline void kernel_exit() {}
 
-// Macros
-#define THREAD_NULL ((struct thread *)0x0)
-#define PROCESS_NULL ((struct process *)0x0)
-#define MEMORY_REGION_NULL ((struct memory_region *)0x0)
+static inline int kernel_create_process(void (*entry)(void)) {
+    struct process *process = malloc(sizeof(*process));
+    if (!process) {
+        return -1;
+    }
+    process->entry = entry;
+    process->threads = NULL;
+    process->pid = 0;
+    return 0;
+}
 
-#endif // _KERNEL_H
+static inline int kernel_create_thread(void (*func)(void)) {
+    struct thread *thread = malloc(sizeof(*thread));
+    if (!thread) {
+        return -1;
+    }
+    thread->func = func;
+    thread->next = NULL;
+    return 0;
+}
 
-void* kmalloc(size_t size) {
+static inline void kernel_yield() {}
+
+static inline void kernel_sleep(uint milliseconds) {}
+
+static inline void kernel_wakeup(struct thread *thread) {}
+
+static inline void kernel_terminate_process(struct process *process) {
+    free(process);
+}
+
+static inline void kernel_free_memory(struct memory_region *region) {
+    free(region->base);
+}
+
+static inline void* kmalloc(size_t size) {
     void* ptr = NULL;
     if (size > 0) {
         ptr = (void*)((uintptr_t)heap + heap_size - size);
@@ -52,13 +75,15 @@ void* kmalloc(size_t size) {
     }
     return ptr;
 }
-void kfree(void* ptr) {
+
+static inline void kfree(void* ptr) {
     if (ptr != NULL) {
         size_t size = (size_t)((uintptr_t)ptr - (uintptr_t)heap);
         heap_size += size;
         memset(ptr, 0, size);
     }
 }
-void kio(void* buffer, size_t size, bool is_read) {
+
+static inline void kio(void* buffer, size_t size, bool is_read) {
     // Todo: Implement I/O operations here
 }
