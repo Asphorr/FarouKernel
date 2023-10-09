d963819affc1ceca9d96e30e388da53a57c4e57c
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-10-09 18:24:09 +0700

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,77 +1,92 @@
-#include "Kernel.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+#include <functional>
+#include <algorithm>
+#include <filesystem>
 
-static file_t files[MAX_FILES];
+namespace fs = std::filesystem;
+using namespace std::literals;
 
-void init() {
-    // Initialize the file system
-    fs_init();
-
-    // Set up the interrupt handlers
-    setup_interrupts();
-
-    // Start the first thread
-    start_first_thread();
-}
-
-int openFile(const char *pathname, int flags) {
-    // Check if the file exists
-    if (!fs_exists(pathname)) {
-        return -ENOENT;
+class KernelModule : public fs::module {
+public:
+    explicit KernelModule(std::string name) : _name(std::move(name)), _files(), _threads() {}
+    
+    ~KernelModule() override {
+        stopAllThreads();
     }
-
-    // Open the file
-    int fd = fs_open(pathname, flags);
-    if (fd < 0) {
-        return -EIO;
+    
+    bool initialize() override {
+        try {
+            _files.emplace_back("kernel", fs::mode::read | fs::mode::write);
+            _files.emplace_back("modules", fs::mode::read | fs::mode::write);
+            
+            auto& kernelFile = _files.front().get();
+            kernelFile.setPosition(0);
+            kernelFile.write("Hello, world!"sv);
+            
+            auto& modulesFile = _files.at(1).get();
+            modulesFile.setPosition(0);
+            modulesFile.write("This is a test."sv);
+            
+            return true;
+        } catch (...) {
+            return false;
+        }
     }
-
-    // Return the file descriptor
-    return fd;
-}
-
-ssize_t readFile(int fd, void *buf, size_t count) {
-    // Read data from the file
-    ssize_t bytesRead = fs_read(fd, buf, count);
-    if (bytesRead <= 0) {
-        return -EIO;
+    
+    void terminate() override {
+        stopAllThreads();
+        
+        _files.clear();
     }
-
-    // Return the number of bytes read
-    return bytesRead;
-}
-
-ssize_t writeFile(int fd, const void *buf, size_t count) {
-    // Write data to the file
-    ssize_t bytesWritten = fs_write(fd, buf, count);
-    if (bytesWritten <= 0) {
-        return -EIO;
+    
+    void run() override {
+        while (_running) {
+            switchToNextThread();
+        }
     }
-
-    // Return the number of bytes written
-    return bytesWritten;
-}
-
-off_t lseekFile(int fd, off_t offset, int whence) {
-    // Seek to the specified position in the file
-    off_t pos = fs_lseek(fd, offset, whence);
-    if (pos < 0) {
-        return -EINVAL;
+    
+    void addThread(std::unique_ptr<fs::thread>&& thread) {
+        _threads.push_back(std::move(thread));
     }
-
-    // Return the current position in the file
-    return pos;
-}
-
-int closeFile(int fd) {
-    // Close the file
-    int result = fs_close(fd);
-    if (result < 0) {
-        return -EIO;
+    
+    void removeThread(fs::thread* thread) {
+        _threads.erase(std::remove(_threads.begin(), _threads.end(), thread), _threads.end());
     }
+    
+    void stopAllThreads() {
+        for (auto& thread : _threads) {
+            thread->stop();
+        }
+    }
+    
+    void switchToNextThread() {
+        if (_currentThreadIndex >= _threads.size()) {
+            _currentThreadIndex = 0;
+        }
+        
+        auto nextThread = _threads[_currentThreadIndex].get();
+        ++_currentThreadIndex;
+        
+        nextThread->run();
+    }
+    
+private:
+    std::string _name;
+    std::vector<std::unique_ptr<fs::file>> _files;
+    std::vector<std::unique_ptr<fs::thread>> _threads;
+    size_t _currentThreadIndex = 0;
+};
 
-    // Return success
+int main() {
+    KernelModule kernelModule("My Kernel Module");
+    kernelModule.initialize();
+    kernelModule.addThread(std::make_unique<fs::thread>([](){
+        std::cout << "Hello, world!";
+    }));
+    kernelModule.run();
+    kernelModule.terminate();
     return 0;
 }
-
-// Switch to the new thread
-switch_to(new_thread);
