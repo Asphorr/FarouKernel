740228b25b36574007d8971bb945b64a3f610c6c
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-10-09 17:58:09 +0700

    Update arch.h

--- arch/arch.h
+++ arch/arch.h
@@ -1,67 +1,67 @@
 #ifndef ARCH_H
 #define ARCH_H
 
-// CPU architecture specific definitions
+/* CPU Architecture Specific Definitions */
 
-// Instruction set architecture (ISA)
+/* Instruction Set Architecture (ISA) */
 #define ISA_X86_64 1
 
-// Endianness
+/* Endianness */
 #define ENDIANNESS_LITTLE 1
 
-// Word size
+/* Word Size */
 #define WORD_SIZE 64
 
-// Address space layout
+/* Address Space Layout */
 #define ASLR 1
 
-// Stack layout
+/* Stack Layout */
 #define STACK_GROWTH_DIRECTION 1
 
-// Page table layout
+/* Page Table Layout */
 #define PAGE_TABLE_ENTRIES 1024
 #define PAGE_TABLE_SHIFT 12
 
-// Virtual memory layout
+/* Virtual Memory Layout */
 #define VIRTUAL_MEMORY_START 0x10000000
 #define VIRTUAL_MEMORY_END 0x20000000
 
-// Interrupt controller
+/* Interrupt Controller */
 #define INTERRUPT_CONTROLLER_APIC 1
 
-// Timekeeping
+/* Timekeeping */
 #define TIMEKEEPING_FREQUENCY 1000
 
-// Console output
+/* Console Output */
 #define CONSOLE_OUTPUT_BUFFER_SIZE 4096
 
-// Networking
+/* Networking */
 #define NETWORKING_ENABLED 1
 
-// File system
+/* File System */
 #define FILE_SYSTEM_TYPE_FAT32 1
 
-// Memory management
+/* Memory Management */
 #define MEMORY_MANAGEMENT_ENABLED 1
 
-// Kernel debugging
+/* Kernel Debugging */
 #define KERNEL_DEBUGGING_ENABLED 0
 
-// Platform specific definitions
+/* Platform-Specific Definitions */
 
-// Define the platform specific instructions
+/* Define the platform-specific instructions */
 #define PLATFORM_SPECIFIC_INSTRUCTIONS \
     X86_INSTRUCTION(0x0f, 0xc7, 0xc0) \
     X86_INSTRUCTION(0x0f, 0xc7, 0xe0) \
     X86_INSTRUCTION(0x0f, 0xc7, 0xf0)
 
-// Define the platform specific data structures
-struct platform_specific_data {
+/* Define the platform-specific data structures */
+typedef struct _platform_specific_data {
     uint32_t foo;
     uint64_t bar;
-};
+} platform_specific_data;
 
-// Define the platform specific functions
-void platform_specific_function(void);
+/* Define the platform-specific functions */
+void platform_specific_function();
 
-#endif // ARCH_H
+#endif /* ARCH_H */
