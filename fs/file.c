3d4009d656d025e801bb9e6ebf0b1091f62af711
Asphorr <144871094+Asphorr@users.noreply.github.com>
2023-10-09 14:02:29 +0700

    Update file.c

--- fs/file.c
+++ fs/file.c
@@ -5,111 +5,222 @@
 #include <fcntl.h>
 #include <sys/mman.h>
 
-#define MAX_NAME_LEN 256
-#define MAX_FILE_SIZE 1024*1024
-
-struct File {
-    char name[MAX_NAME_LEN];
-    int fd;
-    off_t size;
+// Define constants for error handling
+enum ErrorCodes {
+    ERROR_OPENING_FILE = 1,
+    ERROR_READING_FROM_FILE,
+    ERROR_WRITING_TO_FILE,
+    ERROR_LSEEKING_IN_FILE,
+    ERROR_STATING_FILE,
+    ERROR_FSTATING_FD,
+    ERROR_MALLOC_FAILED,
+    ERROR_REALLOC_FAILED,
+    ERROR_MMAP_FAILED,
+    ERROR_UNEXPECTED_ERROR,
 };
 
-static int open(const char *filename, int flags) {
-    int fd = -1;
-    if ((fd = open(filename, flags)) == -1) {
-        perror("open");
-        return -1;
-    }
-    return fd;
-}
+// Structure to represent a file
+typedef struct File {
+    // Name of the file
+    char name[MAX_NAME_LEN];
+    
+    // File descriptor
+    int fd;
+    
+    // Size of the file
+    off_t size;
+    
+    // Pointer to the memory-mapped region
+    void *map;
+    
+    // Offset within the mapped region
+    off_t mapOffset;
+    
+    // Length of the mapped region
+    size_t mapLength;
+} File;
 
-static void close(int fd) {
-    if (close(fd) == -1) {
-        perror("close");
-    }
-}
-
-static ssize_t read(int fd, void *buf, size_t count) {
-    ssize_t ret = -1;
-    while (ret == -1 || ret == 0) {
-        ret = read(fd, buf, count);
-        if (ret == -1) {
-            perror("read");
-        }
-    }
-    return ret;
-}
-
-static ssize_t write(int fd, const void *buf, size_t count) {
-    ssize_t ret = -1;
-    while (ret == -1 || ret == 0) {
-        ret = write(fd, buf, count);
-        if (ret == -1) {
-            perror("write");
-        }
-    }
-    return ret;
-}
-
-static off_t lseek(int fd, off_t offset, int whence) {
-    off_t ret = -1;
-    while (ret == -1) {
-        ret = lseek(fd, offset, whence);
-        if (ret == -1) {
-            perror("lseek");
-        }
-    }
-    return ret;
-}
-
-static int stat(const char *filename, struct stat *st) {
-    if (stat(filename, st) == -1) {
-        perror("stat");
-        return -1;
-    }
-    return 0;
-}
-
-static int fstat(int fd, struct stat *st) {
-    if (fstat(fd, st) == -1) {
-        perror("fstat");
-        return -1;
-    }
-    return 0;
-}
+// Function prototypes
+void printErrorMessage(int errCode);
+File *createFileStruct();
+void destroyFileStruct(File **filePtr);
+int openFile(File *file, const char *fileName, int flags);
+off_t getFileSize(File *file);
+ssize_t readFromFile(File *file, void *buffer, size_t nbytes);
+ssize_t writeToFile(File *file, const void *buffer, size_t nbytes);
+off_t seekInFile(File *file, off_t offset, int whence);
+int mmapFile(File *file, void *addr, size_t length, int prot, int flags, off_t offset);
+void unmapFile(File *file);
 
 int main() {
-    char filename[] = "/home/user/example.txt";
-    struct File *file = malloc(sizeof(struct File));
-    file->name = filename;
-    file->fd = open(filename, O_RDONLY);
-    file->size = lseek(file->fd, 0, SEEK_END);
-
-    printf("File name: %s\n", file->name);
-    printf("File size: %ld\n", file->size);
-
-    char buff[1024];
-    ssize_t ret = read(file->fd, buff, 1024);
-    if (ret == -1) {
-        perror("read");
-    } else {
-        printf("Read %ld bytes\n", ret);
+    // Create a new file structure
+    File *file = createFileStruct();
+    
+    // Open an existing file or create a new one
+    int result = openFile(file, "example.txt", O_CREAT | O_RDWR);
+    if (result != 0) {
+        printErrorMessage(result);
+        goto cleanup;
     }
-
-    ret = write(file->fd, buff, 1024);
-    if (ret == -1) {
-        perror("write");
-    } else {
-        printf("Wrote %ld bytes\n", ret);
+    
+    // Get the size of the file
+    off_t fileSize = getFileSize(file);
+    printf("File size: %ld\n", fileSize);
+    
+    // Read from the file
+    char buffer[1024];
+    ssize_t numBytesRead = readFromFile(file, buffer, sizeof(buffer));
+    if (numBytesRead > 0) {
+        printf("Read %ld bytes\n", numBytesRead);
+        
+        // Write back to the file
+        ssize_t numBytesWritten = writeToFile(file, buffer, numBytesRead);
+        if (numBytesWritten >= 0) {
+            printf("Wrote %ld bytes\n", numBytesWritten);
+            
+            // Seek to the beginning of the file
+            off_t currentPos = seekInFile(file, 0, SEEK_SET);
+            if (currentPos >= 0) {
+                printf("Current position: %ld\n", currentPos);
+                
+                // Memory-map the file
+                void *mapAddr = NULL;
+                size_t mapLen = fileSize;
+                int mapProt = PROT_READ | PROT_WRITE;
+                int mapFlags = MAP_PRIVATE;
+                off_t mapOff = 0;
+                result = mmapFile(file, &mapAddr, mapLen, mapProt, mapFlags, mapOff);
+                if (result != 0) {
+                    printErrorMessage(result);
+                    goto cleanup;
+                }
+                
+                // Access the memory-mapped data
+                memcpy(buffer, mapAddr + mapOff, numBytesRead);
+                printf("Mapped data: ");
+                for (int i = 0; i < numBytesRead; ++i) {
+                    printf("%c", buffer[i]);
+                }
+                putchar('\n');
+                
+                // Unmap the file
+                unmapFile(file);
+            }
+        }
     }
-
-    ret = lseek(file->fd, 0, SEEK_SET);
-    if (ret == -1) {
-        perror("lseek");
-    } else {
-        printf("File pos: %ld\n", ret);
-    }
-
-    free(file);
+    
+cleanup:
+    // Close the file
+    close(file->fd);
+    
+    // Free the file structure
+    destroyFileStruct(&file);
+    
     return 0;
+}
+
+// Print an error message based on the given error code
+void printErrorMessage(int errCode) {
+    switch (errCode) {
+        case ERROR_OPENING_FILE:
+            puts("Failed to open file.");
+            break;
+        case ERROR_READING_FROM_FILE:
+            puts("Failed to read from file.");
+            break;
+        case ERROR_WRITING_TO_FILE:
+            puts("Failed to write to file.");
+            break;
+        case ERROR_LSEEKING_IN_FILE:
+            puts("Failed to seek in file.");
+            break;
+        case ERROR_STATING_FILE:
+            puts("Failed to stat file.");
+            break;
+        case ERROR_FSTATING_FD:
+            puts("Failed to fstat file descriptor.");
+            break;
+        case ERROR_MALLOC_FAILED:
+            puts("Memory allocation failed.");
+            break;
+        case ERROR_REALLOC_FAILED:
+            puts("Reallocating memory failed.");
+            break;
+        case ERROR_MMAP_FAILED:
+            puts("Mapping memory failed.");
+            break;
+        default:
+            puts("An unexpected error occurred.");
+            break;
+    }
+}
+
+// Allocate and initialize a new file structure
+File *createFileStruct() {
+    File *file = malloc(sizeof(*file));
+    if (!file) {
+        printErrorMessage(ERROR_MALLOC_FAILED);
+        exit(-1);
+    }
+    memset(file, 0, sizeof(*file));
+    return file;
+}
+
+// Deallocate a file structure
+void destroyFileStruct(File **filePtr) {
+    if (*filePtr) {
+        free(*filePtr);
+        *filePtr = NULL;
+    }
+}
+
+// Open a file using the specified mode
+int openFile(File *file, const char *fileName, int flags) {
+    int fd = open(fileName, flags);
+    if (fd == -1) {
+        printErrorMessage(ERROR_OPENING_FILE);
+        return -1;
+    }
+    file->fd = fd;
+    return 0;
+}
+
+// Get the size of a file
+off_t getFileSize(File *file) {
+    struct stat st;
+    if (fstat(file->fd, &st) == -1) {
+        printErrorMessage(ERROR_FSTATING_FD);
+        return -1;
+    }
+    return st.st_size;
+}
+
+// Read from a file into a buffer
+ssize_t readFromFile(File *file, void *buffer, size_t nbytes) {
+    ssize_t numBytesRead = read(file->fd, buffer, nbytes);
+    if (numBytesRead <= 0) {
+        printErrorMessage(ERROR_READING_FROM_FILE);
+        return -1;
+}
+return numBytesRead;
+}
+
+// Write to a file from a buffer
+ssize_t writeToFile(File *file, const void *buffer, size_t nbytes) {
+ssize_t numBytesWritten = write(file->fd, buffer, nbytes);
+if (numBytesWritten <= 0) {
+printErrorMessage(ERROR_WRITING_TO_FILE);
+return -1;
+}
+return numBytesWritten;
+}
+
+// Seek within a file
+off_t lseekInFile(File *file, off_t offset, int whence) {
+off_t currOffset = lseek(file->fd, offset, whence);
+if (currOffset == -1) {
+printErrorMessage(ERROR_LSEEKING_IN_FILE);
+return -1;
+}
+return currOffset;
 }
