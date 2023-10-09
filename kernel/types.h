56ffefc73c363d3489497a7890c165226791ce98
Asphorr <144871094+Asphorr@users.noreply.github.com>
2023-10-09 10:29:31 +0700

    Create types.h

---
+++ kernel/types.h
@@ -0,0 +1,18 @@
+#ifndef _TYPES_H
+#define _TYPES_H
+
+typedef unsigned long uint64_t;
+typedef signed long sint64_t;
+
+typedef unsigned int uint32_t;
+typedef signed int sint32_t;
+
+typedef unsigned short uint16_t;
+typedef signed short sint16_t;
+
+typedef unsigned char uint8_t;
+typedef signed char sint8_t;
+
+typedef void (*func)(void);
+
+#endif /* _TYPES_H */
