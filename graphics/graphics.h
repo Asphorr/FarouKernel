7afb136c639b80350e360a37ff9c7a74149c3ba5
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-10-09 19:21:44 +0700

    Update graphics.h

--- graphics/graphics.h
+++ graphics/graphics.h
@@ -1,24 +1,24 @@
-#ifndef GRAPHICS_H
-#define GRAPHICS_H
-
-// Functions for drawing shapes
-void draw_point(int x, int y);
-void draw_line(int x1, int y1, int x2, int y2);
-void draw_rectangle(int x, int y, int width, int height);
-void draw_circle(int x, int y, int radius);
-
-// Functions for drawing text
-void draw_text(const char* text, int x, int y, int font_size);
-void draw_string(const char* text, int x, int y, int font_size, int length);
-
-// Functions for drawing images
-void draw_image(const unsigned char* image_data, int x, int y, int width, int height);
-
-// Functions for setting graphics modes
-void set_graphics_mode(int mode);
-
-// Functions for getting graphics information
-int get_screen_width();
-int get_screen_height();
-
-#endif // GRAPHICS_H
+class Graphics {
+public:
+    void drawPoint(int x, int y);
+    void drawLine(int x1, int y1, int x2, int y2);
+    void drawRectangle(int x, int y, int width, int height);
+    void drawCircle(int x, int y, int radius);
+    
+    template<typename T>
+    void drawText(T&& text, int x, int y, int fontSize);
+    template<typename T>
+    void drawString(T&& text, int x, int y, int fontSize, int length);
+    
+    void drawImage(const unsigned char* imageData, int x, int y, int width, int height);
+    
+    void setGraphicsMode(int mode);
+    
+    int getScreenWidth() const;
+    int getScreenHeight() const;
+    
+private:
+    int screenWidth_;
+    int screenHeight_;
+    int currentMode_;
+};
