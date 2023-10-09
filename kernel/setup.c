2dfb62a1ef949fd8175de14a7fa46ae585592604
Asphorr <144871094+Asphorr@users.noreply.github.com>
2023-10-09 14:17:12 +0700

    Update setup.c

--- kernel/setup.c
+++ kernel/setup.c
@@ -5,59 +5,75 @@
 #include <fcntl.h>
 #include <sys/mman.h>
 
+// Define constants
 #define MAX_ARGS 64
+#define BUFFER_SIZE 1024
+
+// Function prototypes
+void parseCommandLineArguments(char** args, int maxArgs);
+void initializeSyscallTable();
+void setUpProgramEnvironment();
+void startProgram(int argc, char** args);
+void waitForProgramToFinish();
+void cleanupAndExit();
 
 int main(void) {
+    // Allocate memory for command line arguments
+    char** args = (char**)malloc(MAX_ARGS * sizeof(char*));
+    memset(args, 0, MAX_ARGS * sizeof(char*));
+
     // Parse command line arguments
-    char **args = malloc(sizeof(char *) * MAX_ARGS);
-    int argc = parse_command_line(args, MAX_ARGS);
+    int argc = parseCommandLineArguments(args, MAX_ARGS);
 
     // Initialize the system call table
-    initialize_syscall_table();
+    initializeSyscallTable();
 
     // Set up the program environment
-    setup_program_environment();
+    setUpProgramEnvironment();
 
     // Start the program
-    start_program(argc, args);
+    startProgram(argc, args);
 
     // Wait for the program to finish
-    wait_for_program_to_finish();
+    waitForProgramToFinish();
 
     // Clean up and exit
-    cleanup_and_exit();
+    cleanupAndExit();
 
     return 0;
 }
 
-int parse_command_line(char **args, int max_args) {
+void parseCommandLineArguments(char** args, int maxArgs) {
     // Parse the command line arguments
-    int argc = 0;
-    char *argv[max_args];
-
-    while ((argc < max_args) && (*++argv = strtok(NULL, " \t")) != NULL) {
-        args[argc++] = argv[0];
+    int i = 0;
+    char buffer[BUFFER_SIZE];
+    while (i < maxArgs && fgets(buffer, BUFFER_SIZE, stdin)) {
+        args[i++] = strdup(buffer);
     }
-
-    return argc;
 }
 
-void setup_program_environment() {
+void initializeSyscallTable() {
+    // Initialize the system call table
+    // ...
+}
+
+void setUpProgramEnvironment() {
     // Set up the program environment
     // ...
 }
 
-void start_program(int argc, char **args) {
+void startProgram(int argc, char** args) {
     // Start the program
     // ...
 }
 
-void wait_for_program_to_finish() {
+void waitForProgramToFinish() {
     // Wait for the program to finish
     // ...
 }
 
-void cleanup_and_exit() {
+void cleanupAndExit() {
     // Clean up and exit
-    // ...
+    free((void*)args);
+    exit(EXIT_SUCCESS);
 }
