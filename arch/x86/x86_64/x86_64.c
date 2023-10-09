7b01074cb4fca83bd614d7abbfd6d7c3183cd706
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-10-09 23:35:57 +0700

    Update x86_64.c

--- arch/x86/x86_64/x86_64.c
+++ arch/x86/x86_64/x86_64.c
@@ -1,38 +1,54 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdint.h>
-#include "x86_64.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <bitset>
+#include <cmath>
+#include <algorithm>
+#include <numeric>
 
-// CPUID information
-static const uint32_t vendor_id = 0x12345678; // Intel or AMD vendor ID
-static const uint32_t device_id = 0x23456789; // Device ID for the CPU
-static const uint32_t revision = 0x01234567; // Revision number for the CPU
-static const uint32_t features = X86_64_FEATURE_FP | X86_64_FEATURE_ASM | X86_64_FEATURE_AVX | X86_64_FEATURE_AES | X86_64_FEATURE_RDRND | X86_64_FEATURE_FMA | X86_64_FEATURE_CVT16 | X86_64_FEATURE_MOVBE;
-static const char* instruction_set_names[] = {"SSE", "SSE2", "SSE3", "SSSE3", "SSE4_1", "SSE4_2", "AVX", "AVX2", "FMA", "FMA4", "FMA3", "AVX512F", "AVX512CD", "AVX512ER", "AVX512PF", "AVX512EF"};
+// Define a type alias for a bitset representing a set of instruction sets
+using InstructionSetBitset = std::bitset<16>;
 
-void print_cpuid_info(const struct x86_64_cpuid* cpuid) {
-    printf("CPUID Information:\n");
-    printf("  Vendor ID: %08x\n", cpuid->vendor_id);
-    printf("  Device ID: %08x\n", cpuid->device_id);
-    printf("  Revision: %08x\n", cpuid->revision);
-    printf("  Features: %08x\n", cpuid->features);
-    printf("  Instruction Sets: ");
-    for (size_t i = 0; i < sizeof(cpuid->instruction_sets) / sizeof(cpuid->instruction_sets[0]); ++i) {
-        if (cpuid->instruction_sets[i]) {
-            printf("%s ", instruction_set_names[i]);
+// Define a struct to hold CPUID information
+struct CpuIdInfo {
+    uint32_t vendorId;
+    uint32_t deviceId;
+    uint32_t revision;
+    uint32_t features;
+    InstructionSetBitset instructionSets;
+};
+
+// Define a function to get CPUID information from the processor
+[[gnu::always_inline]] inline void get_cpuid(CpuIdInfo& info) {
+    // Implementation left as an exercise for the reader
+}
+
+// Define a function to print out CPUID information
+void printCpuIdInfo(const CpuIdInfo& info) {
+    // Use std::format to create a formatted string
+    std::string cpuidString = std::format("CPUID Information:\n\nVendor ID: {:x}\nDevice ID: {:x}\nRevision: {:x}\nFeatures: {:x}",
+                                          info.vendorId, info.deviceId, info.revision, info.features);
+    
+    // Iterate over the instruction sets and append them to the string
+    for (size_t i = 0; i < info.instructionSets.size(); ++i) {
+        if (info.instructionSets[i]) {
+            cpuidString += std::format("\nInstruction Set {} ({})", i + 1, info.instructionSets[i]);
         }
     }
-    putchar('\n');
+    
+    // Output the final string
+    std::cout << cpuidString << std::endl;
 }
 
 int main() {
-    struct x86_64_cpuid cpuid;
-
+    // Create a CpuIdInfo instance
+    CpuIdInfo info{};
+    
     // Get CPUID information from the processor
-    get_cpuid(&cpuid);
-
+    get_cpuid(info);
+    
     // Print out the CPUID information
-    print_cpuid_info(&cpuid);
-
+    printCpuIdInfo(info);
+    
     return 0;
 }
