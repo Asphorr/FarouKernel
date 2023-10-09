d6d2f36a29775dcc4c8c5c6814721ff7ded6dc88
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-10-09 18:30:42 +0700

    Update util.c

--- kernel/util.c
+++ kernel/util.c
@@ -1,27 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "util.h"
+#include <iostream>
+#include <cstddef>
+#include <memory>
+#include <utility>
 
-void* malloc_or_die(size_t size) {
-    void* ptr = malloc(size);
-    if (ptr == NULL) {
-        fprintf(stderr, "Failed to allocate %zu bytes\n", size);
-        exit(EXIT_FAILURE);
-    }
-    return ptr;
+template <typename T>
+using unique_ptr = std::unique_ptr<T>;
+
+template <typename T>
+inline unique_ptr<T> make_unique(size_t n) {
+    return unique_ptr<T>{new T[n]{}};
 }
 
-void free_or_die(void* ptr) {
-    if (ptr != NULL) {
-        free(ptr);
-    } else {
-        fprintf(stderr, "Attempted to free null pointer\n");
-        exit(EXIT_FAILURE);
+template <typename T>
+inline void safe_delete(T* p) {
+    if (p != nullptr) {
+        delete[] p;
     }
 }
 
-void print_error_and_exit(const char* message) {
-    fprintf(stderr, "%s\n", message);
-    exit(EXIT_FAILURE);
+template <typename T>
+inline void safe_free(T* p) {
+    if (p != nullptr) {
+        free(p);
+    }
+}
+
+template <typename T>
+inline void print_error_and_exit(const char* message) {
+    std::cerr << message << '\n';
+    std::exit(EXIT_FAILURE);
 }
