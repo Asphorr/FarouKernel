c85418f167109adc848fc8799cf437f2a0d00321
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-10-10 00:48:01 +0700

    Update heap.h

--- mm/heap.h
+++ mm/heap.h
@@ -1,26 +1,39 @@
-#ifndef HEAP_H
-#define HEAP_H
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
 
-#include <stdbool.h>
-#include <stdint.h>
+// Define a custom comparator function for sorting elements
+auto compare = [] (const auto& lhs, const auto& rhs) { return lhs > rhs; };
 
-// Heap structure
-typedef struct heap_s {
-    uint32_t capacity;      // Capacity of the heap
-    uint32_t size;         // Current number of elements in the heap
-    void** data;          // Array of pointers to heap elements
-} heap_t;
+class Heap {
+public:
+    // Constructor
+    Heap(std::initializer_list<int> list) : _data{list} {}
+    
+    // Destructor
+    ~Heap() {}
+    
+    // Insert element into the heap
+    void insert(int value) {
+        _data.push_back(value);
+        std::make_heap(_data.begin(), _data.end());
+    }
+    
+    // Remove top element from the heap
+    int removeTop() {
+        std::pop_heap(_data.begin(), _data.end());
+        return _data.front();
+    }
+    
+private:
+    std::vector<int> _data;
+};
 
-// Function prototypes
-heap_t* heap_create(uint32_t capacity);
-void heap_destroy(heap_t* heap);
-void heap_insert(heap_t* heap, void* element);
-void heap_remove(heap_t* heap);
-void heap_sort(heap_t* heap);
-
-// Helper functions
-static inline bool heap_is_empty(heap_t* heap);
-static inline bool heap_is_full(heap_t* heap);
-static inline void heap_swap(heap_t* heap, uint32_t i, uint32_t j);
-
-#endif  // HEAP_H
+int main() {
+    Heap h({1, 2, 3, 4, 5});
+    h.insert(6);
+    h.removeTop();
+    std::cout << "The top element is: " << h._data[0] << "\n";
+    return 0;
+}
