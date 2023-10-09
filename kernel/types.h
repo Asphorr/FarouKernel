2e6cf84d9cdb587506926e1ff542cc51292ce7d0
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-10-09 17:29:40 +0700

    Update types.h

--- kernel/types.h
+++ kernel/types.h
@@ -1,18 +1,21 @@
-#ifndef _TYPES_H
-#define _TYPES_H
+#pragma once
 
-typedef unsigned long uint64_t;
-typedef signed long sint64_t;
+#include <cstdint>
 
-typedef unsigned int uint32_t;
-typedef signed int sint32_t;
-
-typedef unsigned short uint16_t;
-typedef signed short sint16_t;
-
-typedef unsigned char uint8_t;
-typedef signed char sint8_t;
-
-typedef void (*func)(void);
-
-#endif /* _TYPES_H */
+namespace my {
+    // Alias for unsigned integers
+    template<size_t N>
+    using uinteger = std::uint_fastN_t;
+    
+    // Alias for signed integers
+    template<size_t N>
+    using sinteger = std::int_fastN_t;
+    
+    // Alias for floating point numbers
+    template<typename T>
+    using fpnumber = typename std::conditional<sizeof(T) == sizeof(float), float, double>::type;
+    
+    // Alias for function pointers
+    template<typename R, typename... Args>
+    using fnptr = R(*)(Args...);
+}
