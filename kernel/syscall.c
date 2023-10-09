640f91650fbb36946e00ade3016b963c793c277b
Asphorr <144871094+Asphorr@users.noreply.github.com>
2023-10-09 10:52:30 +0700

    Update syscall.c

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -1,122 +1,100 @@
-#include "syscall.h"
-#include "memory.h"
-#include "processes.h"
-#include "driver.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <signal.h>
+#include <time.h>
 
-// Define the system call table
-static syscall_t syscall_table[SYSCALL_NUMBER_MAX];
+#define MAX_FILENAME_LENGTH 256
+#define MAX_PATH_LENGTH 4096
 
-// Initialize the system call table
-void initialize_syscall_table() {
-    syscall_table[SYSCALL_NUMBER_exit] = (syscall_t)exit;
-    syscall_table[SYSCALL_NUMBER_read] = (syscall_t)read;
-    syscall_table[SYSCALL_NUMBER_write] = (syscall_t)write;
-    syscall_table[SYSCALL_NUMBER_open] = (syscall_t)open;
-    syscall_table[SYSCALL_NUMBER_close] = (syscall_t)close;
-    syscall_table[SYSCALL_NUMBER_create] = (syscall_t)create;
-    syscall_table[SYSCALL_NUMBER_delete] = (syscall_t)delete;
-    syscall_table[SYSCALL_NUMBER_getpid] = (syscall_t)getpid;
-    syscall_table[SYSCALL_NUMBER_sleep] = (syscall_t)sleep;
-}
+typedef struct _file_t {
+    int fd;
+    char filename[MAX_FILENAME_LENGTH];
+    unsigned long inode;
+    off_t offset;
+    size_t length;
+    time_t mtime;
+    bool deleted;
+} file_t;
 
-// Execute a system call
-int syscall(syscall_t *sc) {
-    // Check if the system call number is valid
-    if (sc->number >= SYSCALL_NUMBER_MAX) {
-        return -1;
-    }
+typedef enum _mode_t {
+    O_RDONLY = 0x0000,   /* Open for reading only */
+    O_WRONLY = 0x0001,   /* Open for writing only */
+    O_RDWR = 0x0002,     /* Open for reading and writing */
+    O_APPEND = 0x0008,   /* If set, append to the end of the file */
+    O_CREAT = 0x0200,    /* If set, create the file if it does not exist */
+    O_EXCL = 0x0800      /* If set, fail if the file already exists */
+} mode_t;
 
-    // Get the address of the system call function
-    void (*func)(void) = syscall_table[sc->number].function;
+typedef struct _process_t {
+    pid_t pid;           /* Process ID */
+    uid_t uid;           /* User ID */
+    gid_t gid;           /* Group ID */
+    char name[32];       /* Name of the process */
+    char cwd[MAX_PATH_LENGTH]; /* Current working directory */
+    char **argv;         /* Command line arguments */
+    char **envp;         /* Environment variables */
+    file_t files[32];    /* File descriptors */
+    int num_files;       /* Number of file descriptors */
+    int max_files;       /* Maximum number of file descriptors */
+    sigset_t signal_mask;/* Signal mask for the process */
+    volatile int state; /* State of the process (running, waiting, etc.) */
+    volatile int wakeup; /* Wake-up reason for the process */
+    volatile int error; /* Error code for the process */
+    volatile int retval; /* Return value for the process */
+} process_t;
 
-    // Call the system call function
-    return func();
-}
+typedef struct _syscall_args_t {
+    union {
+        int arg1;
+        int arg2;
+        int arg3;
+        int arg4;
+        int arg5;
+        int arg6;
+        int arg7;
+        int arg8;
+        int arg9;
+        int arg10;
+    };
+} syscall_args_t;
 
-// Implement the system call functions
-int exit(int status) {
-    // Release the process's resources
-    release_resources(current_process);
+typedef struct _syscall_result_t {
+    int result;          /* Result of the system call */
+    int errnum;          /* Errno associated with the result */
+} syscall_result_t;
 
-    // Terminate the process
-    terminate_process(current_process);
+typedef struct _syscall_entry_t {
+    int number;          /* System call number */
+    const char *name;    /* Name of the system call */
+    void (*handler)(struct _syscall_args_t *args, struct _syscall_result_t *res); /* Handler function for the system call */
+} syscall_entry_t;
 
-    // Return the status to the parent process
-    return status;
-}
+extern syscall_entry_t syscall_table[];
 
-int read(int fd, char *buf, int len) {
-    // Check if the file descriptor is valid
-    if (!validate_file_descriptor(fd)) {
-        return -1;
-    }
-
-    // Read data from the file
-    int bytes_read = read_file(fd, buf, len);
-
-    // Return the number of bytes read
-    return bytes_read;
-}
-
-int write(int fd, const char *buf, int len) {
-    // Check if the file descriptor is valid
-    if (!validate_file_descriptor(fd)) {
-        return -1;
-    }
-
-    // Write data to the file
-    int bytes_written = write_file(fd, buf, len);
-
-    // Return the number of bytes written
-    return bytes_written;
-}
-
-int open(const char *filename, int flags) {
-    // Create a new file object
-    file_t *file = create_file(filename, flags);
-
-    // Return the file descriptor
-    return file->fd;
-}
-
-int close(int fd) {
-    // Check if the file descriptor is valid
-    if (!validate_file_descriptor(fd)) {
-        return -1;
-    }
-
-    // Close the file
-    close_file(fd);
-
-    // Return success
-    return 0;
-}
-
-int create(const char *filename, int mode) {
-    // Create a new file object
-    file_t *file = create_file(filename, mode);
-
-    // Return the file descriptor
-    return file->fd;
-}
-
-int delete(const char *filename) {
-    // Delete the file
-    delete_file(filename);
-
-    // Return success
-    return 0;
-}
-
-int getpid() {
-    // Return the current process ID
-    return current_process->pid;
-}
-
-int sleep(int seconds) {
-    // Suspend the process for the specified number of seconds
-    suspend_process(seconds);
-
-    // Return success
-    return 0;
-}
+/* Function prototypes */
+void init_syscalls(void);
+void handle_system_call(struct _syscall_args_t *args, struct _syscall_result_t *res);
+void handle_exit(struct _syscall_args_t *args, struct _syscall_result_t *res);
+void handle_read(struct _syscall_args_t *args, struct _syscall_result_t *res);
+void handle_write(struct _syscall_args_t *args, struct _syscall_result_t *res);
+void handle_open(struct _syscall_args_t *args, struct _syscall_result_t *res);
+void handle_close(struct _syscall_args_t *args, struct _syscall_result_t *res);
+void handle_creat(struct _syscall_args_t *args, struct _syscall_result_t *res);
+void handle_link(struct _syscall_args_t *args, struct _syscall_result_t *res);
+void handle_unlink(struct _syscall_args_t *args, struct _syscall_result_t *res);
+void handle_execve(struct _syscall_args_t *args, struct _syscall_result_t *res);
+void handle_chdir(struct _syscall_args_t *args, struct _syscall_result_t *res);
+void handle_fork(struct _syscall_args_t *args, struct _syscall_result_t *res);
+void handle_wait(struct _syscall_args_t *args, struct _syscall_result_t *res);
+void handle_pipe(struct _syscall_args_t *args, struct _syscall_result_t *res);
+void handle_dup(struct _syscall_args_t *args, struct _syscall_result_t *res);
+void handle_dup2(struct _syscall_args_t *args, struct _syscall_result_t *res);
+void handle_getpid(struct _syscall_args_t *args, struct _syscall_result_t *res);
+void handle_brk(struct _syscall_args_t *args, struct _syscall_result_t *res);
+void handle_sleep(struct _syscall_args_t *args, struct _syscall_result_t *res);
+void handle_signal(struct _syscall_args_t *args, struct _syscall_result_t *res);
+void handle_kill(struct _syscall_args_t *args, struct _syscall_result_t *res);
