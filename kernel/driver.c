ce27135b52d16eb5302062df6e0c7508588a6952
Asphorr <144871094+Asphorr@users.noreply.github.com>
2023-10-09 11:28:38 +0700

    Update driver.c

--- kernel/driver.c
+++ kernel/driver.c
@@ -1,94 +1,45 @@
-#include "driver.h"
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <sys/mman.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/ioctl.h>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
 
-#define DEVICE_NAME "mydevice"
-#define DEVICE_MAJOR 123
-#define DEVICE_MINOR 456
+class Driver {
+public:
+    explicit Driver(const std::string& name): name_(name), status_(DriverStatus::UNLOADED) {}
 
-static int device_fd;
+    void load() {
+        if (status_ != DriverStatus::UNLOADED) {
+            throw std::runtime_error("Cannot load driver: already loaded");
+        }
+        // Perform any necessary initialization steps here
+        // ...
+        status_ = DriverStatus::LOADED;
+    }
 
-static int driver_init(struct driver *drv)
-{
-    printf("Initializing driver...\n");
+    void unload() {
+        if (status_ != DriverStatus::LOADED) {
+            throw std::runtime_error("Cannot unload driver: not loaded");
+        }
+        // Perform any necessary cleanup steps here
+        // ...
+        status_ = DriverStatus::UNLOADED;
+    }
 
-    // Perform any necessary initialization steps here
-    // ...
+private:
+    const std::string name_;
+    DriverStatus status_;
+};
 
-    drv->status = 0x02; // Set the driver status to "loaded"
-    return 0;
-}
+enum class DriverStatus { UNLOADED, LOADED };
 
-static int driver_load(struct driver *drv)
-{
-    printf("Loading driver...\n");
-
-    // Load the driver module into memory
-    // ...
-
-    drv->status = 0x02; // Set the driver status to "loaded"
-    return 0;
-}
-
-static int driver_unload(struct driver *drv)
-{
-    printf("Unloading driver...\n");
-
-    // Unload the driver module from memory
-    // ...
-
-    drv->status = 0x00; // Set the driver status to "unloaded"
-    return 0;
-}
-
-static int driver_send_command(struct driver *drv, uint8_t command)
-{
-    printf("Sending command %d to driver...\n", command);
-
-    // Send the command to the driver
-    // ...
-
-    return 0;
-}
-
-static uint8_t driver_receive_data(struct driver *drv)
-{
-    uint8_t data;
-
-    printf("Receiving data from driver...\n");
-
-    // Receive data from the driver
-    // ...
-
-    return data;
-}
-
-int main(void)
-{
-    struct driver drv;
-
-    // Initialize the driver
-    driver_init(&drv);
-
-    // Load the driver
-    driver_load(&drv);
-
-    // Send a command to the driver
-    driver_send_command(&drv, 0x01);
-
-    // Receive data from the driver
-    uint8_t data = driver_receive_data(&drv);
-
-    // Print the received data
-    printf("Received data: %d\n", data);
-
-    // Unload the driver
-    driver_unload(&drv);
-
-    return 0;
+int main() {
+    try {
+        Driver driver("mydevice");
+        driver.load();
+        // Do something with the driver
+        driver.unload();
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << '\n';
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
 }
