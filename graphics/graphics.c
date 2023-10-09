d1dcb317555d6649b1391c20b9820e218369987e
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-10-09 19:20:48 +0700

    Update graphics.c

--- graphics/graphics.c
+++ graphics/graphics.c
@@ -1,46 +1,156 @@
-#include "graphics.h"
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <algorithm>
+#include <memory>
 
-// Functions for drawing shapes
-void draw_point(int x, int y) {
-    printf("Drawing point at %d, %d\n", x, y);
+class Graphics {
+public:
+    enum Mode {
+        POINT, LINE, RECTANGLE, CIRCLE, TEXT, STRING, IMAGE
+    };
+
+private:
+    static constexpr int screenWidth = 800;
+    static constexpr int screenHeight = 600;
+
+    std::unique_ptr<unsigned char[]> imageData_;
+    int currentMode_ = -1;
+    int currentX_ = 0;
+    int currentY_ = 0;
+    int currentFontSize_ = 12;
+    int currentLength_ = 0;
+
+public:
+    explicit Graphics();
+    ~Graphics();
+
+    void setGraphicsMode(int mode);
+    void resetGraphicsMode();
+
+    void drawPoint(int x, int y);
+    void drawLine(int x1, int y1, int x2, int y2);
+    void drawRectangle(int x, int y, int width, int height);
+    void drawCircle(int x, int y, int radius);
+    void drawText(const char *text, int x, int y, int fontSize);
+    void drawString(const char *text, int x, int y, int fontSize, int length);
+    void drawImage(const unsigned char *imageData, int x, int y, int width, int height);
+
+    int getScreenWidth() const;
+    int getScreenHeight() const;
+
+private:
+    void updateCurrentMode_(int newMode);
+    void updateCurrentPosition_(int newX, int newY);
+    void updateCurrentFontSize_(int newFontSize);
+    void updateCurrentLength_(int newLength);
+};
+
+Graphics::Graphics() : imageData_{nullptr}, currentMode_{-1}, currentX_{0}, currentY_{0}, currentFontSize_{12}, currentLength_{0} {}
+
+Graphics::~Graphics() {
+    if (imageData_) {
+        free(imageData_.get());
+    }
 }
 
-void draw_line(int x1, int y1, int x2, int y2) {
-    printf("Drawing line from %d, %d to %d, %d\n", x1, y1, x2, y2);
+void Graphics::setGraphicsMode(int mode) {
+    switch (mode) {
+    case POINT:
+        break;
+    case LINE:
+        break;
+    case RECTANGLE:
+        break;
+    case CIRCLE:
+        break;
+    case TEXT:
+        break;
+    case STRING:
+        break;
+    default:
+        throw std::invalid_argument{"Invalid graphics mode"};
+    }
+
+    currentMode_ = mode;
 }
 
-void draw_rectangle(int x, int y, int width, int height) {
-    printf("Drawing rectangle at %d, %d with size %d x %d\n", x, y, width, height);
+void Graphics::resetGraphicsMode() {
+    currentMode_ = -1;
 }
 
-void draw_circle(int x, int y, int radius) {
-    printf("Drawing circle at %d, %d with radius %d\n", x, y, radius);
+void Graphics::drawPoint(int x, int y) {
+    if (!imageData_) {
+        throw std::runtime_error{"No image data available"};
+    }
+
+    auto pixelIndex = (y * screenWidth + x) * 4;
+    imageData_[pixelIndex] = 0xFF;
+    imageData_[pixelIndex + 1] = 0xFF;
+    imageData_[pixelIndex + 2] = 0xFF;
+    imageData_[pixelIndex + 3] = 0xFF;
 }
 
-// Functions for drawing text
-void draw_text(const char* text, int x, int y, int font_size) {
-    printf("Drawing text '%s' at %d, %d with font size %d\n", text, x, y, font_size);
+void Graphics::drawLine(int x1, int y1, int x2, int y2) {
+    if (!imageData_) {
+        throw std::runtime_error{"No image data available"};
+    }
+
+    auto dx = abs(x2 - x1);
+    auto dy = abs(y2 - y1);
+    auto sx = x1 < x2 ? 1 : -1;
+    auto sy = y1 < y2 ? 1 : -1;
+    auto err = dx - dy;
+
+    while (true) {
+        drawPoint(x1, y1);
+        if (x1 == x2 && y1 == y2) {
+            break;
+        }
+        auto e2 = 2 * err;
+        if (e2 > -dy) {
+            err -= dy;
+            x1 += sx;
+        }
+        if (e2 < dx) {
+            err += dx;
+            y1 += sy;
+        }
+    }
 }
 
-void draw_string(const char* text, int x, int y, int font_size, int length) {
-    printf("Drawing string '%s' at %d, %d with font size %d and length %d\n", text, x, y, font_size, length);
+void Graphics::drawRectangle(int x, int y, int width, int height) {
+    if (!imageData_) {
+        throw std::runtime_error{"No image data available"};
+    }
+
+    auto left = x;
+    auto right = x + width - 1;
+    auto top = y;
+    auto bottom = y + height - 1;
+
+    drawLine(left, top, right, top);
+    drawLine(right, top, right, bottom);
+    drawLine(right, bottom, left, bottom);
+    drawLine(left, bottom, left, top);
 }
 
-// Functions for drawing images
-void draw_image(const unsigned char* image_data, int x, int y, int width, int height) {
-    printf("Drawing image at %d, %d with size %d x %d\n", x, y, width, height);
+void Graphics::drawCircle(int x, int y, int radius) {
+    if (!imageData_) {
+        throw std::runtime_error{"No image data available"};
+    }
+
+    auto centerX = x;
+    auto centerY = y;
+    auto rSquared = radius * radius;
+
+    for (auto dy = -radius; dy <= radius; ++dy) {
+        auto dx = sqrt(rSquared - dy * dy);
+        drawPoint(centerX + dx, centerY + dy);
+        drawPoint(centerX - dx, centerY + dy);
+        drawPoint(centerX + dx, centerY - dy);
+        drawPoint(centerX - dx, centerY - dy);
+    }
 }
 
-// Functions for setting graphics modes
-void set_graphics_mode(int mode) {
-    printf("Setting graphics mode to %d\n", mode);
-}
-
-// Functions for getting graphics information
-int get_screen_width() {
-    return 800;
-}
-
-int get_screen_height() {
-    return 600;
-}
+void Graphics::drawText(const char *
