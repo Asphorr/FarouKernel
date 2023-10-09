3b7e033eb052d634de1dd88b1b703b72d2d1c391
Asphorr <144871094+Asphorr@users.noreply.github.com>
2023-10-09 14:11:19 +0700

    Update dir.c

--- fs/dir.c
+++ fs/dir.c
@@ -1,70 +1,133 @@
-#include "dir.h"
+#pragma once
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#include <stdint.h>
+#include <string.h>
+#include <sys/types.h>
 
-// Direntry structure
-struct dirent {
-    ino_t d_ino;     /* Inode number */
-    off_t d_off;   /* Offset to the start of the file */
-    uint16_t d_reclen;  /* Length of this directory entry */
-    uint8_t d_type;  /* Type of file (see below) */
-    char d_name[256];  /* Name of the file */
+typedef struct dirent {
+    ino_t d_ino;       // Inode number
+    off_t d_off;       // Offset to the start of the file
+    uint16_t d_reclen; // Length of this directory entry
+    uint8_t d_type;    // Type of file (see below)
+    char d_name[256]; // Name of the file
+} dirent;
+
+// Enum for file types
+enum FileType {
+    REGULAR_FILE = 1,
+    DIRECTORY = 2,
+    SYMBOLIC_LINK = 3,
+    BLOCK_DEVICE = 4,
+    CHARACTER_DEVICE = 5,
+    FIFO = 6,
+    SOCKET = 7
 };
 
-// Types of files that can be stored in a directory
-enum {
-    DT_REG = 1,  // Regular file
-    DT_DIR = 2,  // Directory
-    DT_LNK = 3,  // Symbolic link
-    DT_BLK = 4,  // Block device
-    DT_CHR = 5,  // Character device
-    DT_FIFO = 6,  // FIFO (named pipe)
-    DT_SOCK = 7,  // Socket
-};
+// Function pointers for directory operations
+typedef void (*DirInitFunc)(void);
+typedef void (*DirExitFunc)(void);
+typedef int (*DirOpenDirFunc)(const char* path, int flags);
+typedef int (*DirCloseDirFunc)(int dirp);
+typedef struct dirent* (*DirReadDirFunc)(int dirp);
+typedef int (*DirSeekDirFunc)(int dirp, off_t offset);
+typedef int (*DirTellDirFunc)(int dirp);
+typedef int (*DirMkDirFunc)(const char* path, mode_t mode);
+typedef int (*DirRmDirFunc)(const char* path);
+typedef int (*DirRenameFunc)(const char* oldpath, const char* newpath);
 
-// Functions
-void dir_init(void) {
-    // Initialize the directory system
+// Struct for storing directory operation function pointers
+typedef struct {
+    DirInitFunc init;
+    DirExitFunc exit;
+    DirOpenDirFunc opendir;
+    DirCloseDirFunc closedir;
+    DirReadDirFunc readdir;
+    DirSeekDirFunc seekdir;
+    DirTellDirFunc telldir;
+    DirMkDirFunc mkdir;
+    DirRmDirFunc rmdir;
+    DirRenameFunc rename;
+} DirOps;
+
+// Global directory operations object
+static DirOps g_dir_ops;
+
+// Initializes the directory system
+inline static void dir_init() {
+    if (!g_dir_ops.init) {
+        g_dir_ops.init = &dir_init;
+    }
+    g_dir_ops.init();
 }
 
-void dir_exit(void) {
-    // Clean up the directory system
+// Cleans up the directory system
+inline static void dir_exit() {
+    if (!g_dir_ops.exit) {
+        g_dir_ops.exit = &dir_exit;
+    }
+    g_dir_ops.exit();
 }
 
-int dir_opendir(const char *path, int flags) {
-    // Open a directory
+// Opens a directory
+inline static int dir_opendir(const char* path, int flags) {
+    if (!g_dir_ops.opendir) {
+        g_dir_ops.opendir = &dir_opendir;
+    }
+    return g_dir_ops.opendir(path, flags);
 }
 
-int dir_closedir(int dirp) {
-    // Close a directory
+// Closes a directory
+inline static int dir_closedir(int dirp) {
+    if (!g_dir_ops.closedir) {
+        g_dir_ops.closedir = &dir_closedir;
+    }
+    return g_dir_ops.closedir(dirp);
 }
 
-struct dirent *dir_readdir(int dirp) {
-    // Read a directory entry
+// Reads a directory entry
+inline static struct dirent* dir_readdir(int dirp) {
+    if (!g_dir_ops.readdir) {
+        g_dir_ops.readdir = &dir_readdir;
+    }
+    return g_dir_ops.readdir(dirp);
 }
 
-int dir_seekdir(int dirp, off_t offset) {
-    // Seek to a specific location in a directory
+// Seeks to a specific location in a directory
+inline static int dir_seekdir(int dirp, off_t offset) {
+    if (!g_dir_ops.seekdir) {
+        g_dir_ops.seekdir = &dir_seekdir;
+    }
+    return g_dir_ops.seekdir(dirp, offset);
 }
 
-int dir_telldir(int dirp) {
-    // Return the current position in a directory
+// Returns the current position in a directory
+inline static int dir_telldir(int dirp) {
+    if (!g_dir_ops.telldir) {
+        g_dir_ops.telldir = &dir_telldir;
+    }
+    return g_dir_ops.telldir(dirp);
 }
 
-int dir_mkdir(const char *path, mode_t mode) {
-    // Create a new directory
+// Creates a new directory
+inline static int dir_mkdir(const char* path, mode_t mode) {
+    if (!g_dir_ops.mkdir) {
+        g_dir_ops.mkdir = &dir_mkdir;
+    }
+    return g_dir_ops.mkdir(path, mode);
 }
 
-int dir_rmdir(const char *path) {
-    // Remove a directory
+// Removes a directory
+inline static int dir_rmdir(const char* path) {
+    if (!g_dir_ops.rmdir) {
+        g_dir_ops.rmdir = &dir_rmdir;
+    }
+    return g_dir_ops.rmdir(path);
 }
 
-int dir_rename(const char *oldpath, const char *newpath) {
-    // Rename a file or directory
+// Renames a file or directory
+inline static int dir_rename(const char* oldpath, const char* newpath) {
+    if (!g_dir_ops.rename) {
+        g_dir_ops.rename = &dir_rename;
+    }
+    return g_dir_ops.rename(oldpath, newpath);
 }
-
-#ifdef __cplusplus
-}
-#endif
