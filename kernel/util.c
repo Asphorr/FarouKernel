83d3c4ac8a7910a5f45408de319b9137e4fa22be
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-10-09 19:11:46 +0700

    Update util.c

--- kernel/util.c
+++ kernel/util.c
@@ -1,32 +1,59 @@
 #include <iostream>
-#include <cstddef>
 #include <memory>
+#include <type_traits>
 #include <utility>
 
-template <typename T>
-using unique_ptr = std::unique_ptr<T>;
+namespace mylib {
 
 template <typename T>
-inline unique_ptr<T> make_unique(size_t n) {
-    return unique_ptr<T>{new T[n]{}};
+class UniquePtr : public std::unique_ptr<T> {
+public:
+    explicit UniquePtr() {}
+
+    template <typename... Args>
+    explicit UniquePtr(Args&&... args) : std::unique_ptr<T>(new T{args...}) {}
+};
+
+template <>
+UniquePtr<void>::~UniquePtr() {
+    if (*this) {
+        delete[] get();
+    }
 }
 
 template <typename T>
-inline void safe_delete(T* p) {
-    if (p != nullptr) {
+struct Deleter {
+    static constexpr bool value = false;
+};
+
+template <typename T>
+Deleter<T> deleter(T*) {
+    return {};
+}
+
+template <typename T>
+auto make_unique(size_t n) -> UniquePtr<T> {
+    return UniquePtr<T>{new T[n]{}};
+}
+
+template <typename T>
+void safe_delete(T* p) {
+    if (p && !deleter(p)) {
         delete[] p;
     }
 }
 
 template <typename T>
-inline void safe_free(T* p) {
-    if (p != nullptr) {
+void safe_free(T* p) {
+    if (p && !deleter(p)) {
         free(p);
     }
 }
 
 template <typename T>
-inline void print_error_and_exit(const char* message) {
+void print_error_and_exit(const char* message) {
     std::cerr << message << '\n';
     std::exit(EXIT_FAILURE);
 }
+
+} // namespace mylib
