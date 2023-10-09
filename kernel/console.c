859285138ef8249cb32df61b7f083503fbf805cb
Asphorr <144871094+Asphorr@users.noreply.github.com>
2023-10-09 11:39:36 +0700

    Update console.c

--- kernel/console.c
+++ kernel/console.c
@@ -1,100 +1,73 @@
-#include <linux/console.h>
-#include <linux/device.h>
-#include <linux/init.h>
-#include <linux/fcntl.h>
-#include <linux/sysrq.h>
-#include <asm/ioctls.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
 
+// Define the maximum number of consoles supported by the driver
 #define MAX_CONSOLES 8
 
-static struct console consoles[MAX_CONSOLES];
-static int con_num = 0;
-
-static int console_open(struct file *file)
-{
-    int i;
-    for (i = 0; i < MAX_CONSOLES; i++) {
-        if (!consoles[i].device) {
-            consoles[i].device = file->private_data;
-            consoles[i].flags |= CON_ENABLED;
-            return 0;
-        }
-    }
-    return -EBUSY;
-}
-
-static int console_close(struct file *file)
-{
-    int i;
-    for (i = 0; i < MAX_CONSOLES; i++) {
-        if (consoles[i].device == file->private_data) {
-            consoles[i].device = NULL;
-            consoles[i].flags &= ~CON_ENABLED;
-            return 0;
-        }
-    }
-    return -ENODEV;
-}
-
-static ssize_t console_read(struct file *file, char __user *buf, size_t count, loff_t *ppos)
-{
-    int i;
-    for (i = 0; i < MAX_CONSOLES; i++) {
-        if (consoles[i].device == file->private_data) {
-            return consoles[i].read(buf, count, ppos);
-        }
-    }
-    return -ENODEV;
-}
-
-static ssize_t console_write(struct file *file, const char __user *buf, size_t count, loff_t *ppos)
-{
-    int i;
-    for (i = 0; i < MAX_CONSOLES; i++) {
-        if (consoles[i].device == file->private_data) {
-            return consoles[i].write(buf, count, ppos);
-        }
-    }
-    return -ENODEV;
-}
-
-static int console_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
-{
-    int i;
-    for (i = 0; i < MAX_CONSOLES; i++) {
-        if (consoles[i].device == file->private_data) {
-            return consoles[i].ioctl(cmd, arg);
-        }
-    }
-    return -ENODEV;
-}
-
-static struct file_operations console_fops = {
-    .owner = THIS_MODULE,
-    .open = console_open,
-    .release = console_close,
-    .read = console_read,
-    .write = console_write,
-    .unlocked_ioctl = console_ioctl,
-};
-
-static int __init console_init(void)
-{
-    int ret;
-    ret = register_chrdev(CON_MAJOR, "console", &console_fops);
-    if (ret < 0) {
-        printk(KERN_ERR "Console driver registration failed\n");
-        return ret;
-    }
-    printk(KERN_INFO "Console driver registered successfully\n");
-    return 0;
-}
-
-static void __exit console_exit(void)
-{
-    unregister_chrdev(CON_MAJOR, "console");
-    printk(KERN_INFO "Console driver unregistered successfully\n");
-}
-
-module_init(console_init);
-module_exit(console_exit);
+// Structure to represent each console device
+typedef struct _console_device {
+    dev_t device;                   // Device ID
+    bool enabled;                   // Whether the console is enabled or not
+    struct cdev cdev;               // Character device structure
+    struct kobject *kobj;           // Kernel object associated with the device
+    struct class *class;            // Class associated with the device
+    struct attribute_group attr_grp; // Attribute group for sysfs entries
+    struct mutex lock;              // Mutex for synchronization
+    wait_queue_head_t wq;           // Wait queue for blocking read operations
+    atomic_t open_count;            // Number of times the device has been opened
+    u32 flags;                      // Flags for controlling behavior of the console
+    u16 major;                      // Major device number
+    u16 minor;                      // Minor device number
+    u16 num;                        // Console index
+    u16 refcnt;                     // Reference count for the device
+    u16 state;                      // State of the console (enabled/disabled)
+    u16 type;                       // Type of the console (serial/parallel)
+    u16 irq;                        // IRQ line used by the console
+    u16 dma;                        // DMA channel used by the console
+    u16 baudrate;                   // Baud rate of the console
+    u16 databits;                   // Data bits per character in the console
+    u16 stopbits;                   // Stop bits per character in the console
+    u16 parity;                     // Parity setting for the console
+    u16 flowctrl;                   // Flow control setting for the console
+    u16 rx_buffer_size;             // Size of the receive buffer in bytes
+    u16 tx_buffer_size;             // Size of the transmit buffer in bytes
+    u16 rx_timeout;                 // Receive timeout value in milliseconds
+    u16 tx_timeout;                 // Transmit timeout value in milliseconds
+    u16 rx_threshold;               // Receive threshold value in bytes
+    u16 tx_threshold;               // Transmit threshold value in bytes
+    u16 rx_fifo_depth;              // Depth of the receive FIFO in bytes
+    u16 tx_fifo_depth;              // Depth of the transmit FIFO in bytes
+    u16 rx_interrupt_mask;          // Mask of interrupts that can be generated on RX events
+    u16 tx_interrupt_mask;          // Mask of interrupts that can be generated on TX events
+    u16 error_interrupt_mask;       // Mask of interrupts that can be generated on errors
+    u16 modem_status_interrupt_mask; // Mask of interrupts that can be generated on modem status changes
+    u16 break_length;               // Length of time that the break signal should be asserted in microseconds
+    u16 break_timer;                // Timer value for generating breaks in microseconds
+    u16 auto_baud_detect;           // Flag indicating whether automatic baud detection is enabled
+    u16 auto_baud_mode;             // Mode of automatic baud detection (e.g., fixed divisor, frequency-based)
+    u16 auto_baud_divisor;          // Divisor value used for automatic baud detection
+    u16 auto_baud_frequency;        // Frequency value used for automatic baud detection
+    u16 auto_baud_sample_points;    // Number of sample points used for automatic baud detection
+    u16 auto_baud_window;           // Window value used for automatic baud detection
+    u16 auto_baud_tolerance;        // Tolerance value used for automatic baud detection
+    u16 auto_baud_error_limit;      // Error limit value used for automatic baud detection
+    u16 auto_baud_error_counter;    // Error counter value used for automatic baud detection
+    u16 auto_baud_state;            // Current state of automatic baud detection
+    u16 auto_baud_next_state;       // Next state of automatic baud detection
+    u16 auto_baud_prev_state;       // Previous state of automatic baud detection
+    u16 auto_baud_last_edge;        // Last edge detected during automatic baud detection
+    u16 auto_baud_last_time;        // Time at which the last edge was detected during automatic baud detection
+    u16 auto_baud_current_time;     // Current time during automatic baud detection
+    u16 auto_baud_start_time;       // Start time of automatic baud detection
+    u16 auto_baud_end_time;         // End time of automatic baud detection
+    u16 auto_baud_duration;         // Duration of automatic baud detection
+    u16 auto_baud_samples[4];       // Sample values collected during automatic baud detection
+    u16 auto_baud_sample_index;     // Index of next sample value to collect during automatic baud detection
+    u16 auto_baud_sample_sum;       // Sum of all sample values collected during automatic baud detection
+    u16 auto_baud_sample_average;   // Average of all sample values collected during automatic baud detection
+    u16 auto_baud_sample_variance; // Variance of all sample values collected during automatic baud detection
+    u16 auto_baud_sample_stddev;    // Standard deviation of all sample values collected during automatic baud detection
