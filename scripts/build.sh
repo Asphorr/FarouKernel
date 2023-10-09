a648b5a5b2577f9daf0f78d7c5e38951eb3187c0
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-10-09 17:15:34 +0700

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-# Set the project name
+Set the project name
 PROJECT_NAME="funicod"
 
-# Set the target architecture
+Set the target architecture
 TARGET_ARCHITECTURE="x86_64"
 
 # Set the C compiler
@@ -40,4 +40,77 @@ $CXX $CXXFLAGS "main.cpp" "utils.cpp" -o "$PROJECT_NAME"
 $LD "${OBJECTS[@]}" $LIBS -o "$PROJECT_NAME"
 
 # Run the executable
+./"$PROJECT_NAME"
+
+# Additional optimizations
+Use the gold linker instead of ld
+export LD=/usr/local/bin/ld.gold
+
+# Enable link-time optimization
+export CFLAGS="${CFLAGS} -flto"
+export CXXFLAGS="${CXXFLAGS} -flto"
+
+# Optimize for size
+export CFLAGS="${CFLAGS} -Os"
+export CXXFLAGS="${CXXFLAGS} -Os"
+
+# Enable position independent execution
+export CFLAGS="${CFLAGS} -fPIE"
+export CXXFLAGS="${CXXFLAGS} -fPIE"
+
+# Disable stack protection
+export CFLAGS="${CFLAGS} -fno-stack-protector"
+export CXXFLAGS="${CXXFLAGS} -fno-stack-protector"
+
+# Enable aggressive loop unrolling
+export CFLAGS="${CFLAGS} -funroll-loops"
+export CXXFLAGS="${CXXFLAGS} -funroll-loops"
+
+# Enable function inlining
+export CFLAGS="${CFLAGS} -finline-functions"
+export CXXFLAGS="${CXXFLAGS} -finline-functions"
+
+# Enable dead code elimination
+export CFLAGS="${CFLAGS} -ffunction-sections -fdata-sections"
+export CXXFLAGS="${CXXFLAGS} -ffunction-sections -fdata-sections"
+
+# Enable whole program optimization
+export CFLAGS="${CFLAGS} -fwhole-program"
+export CXXFLAGS="${CXXFLAGS} -fwhole-program"
+
+# Enable profile guided optimization
+export CFLAGS="${CFLAGS} -fprofile-generate"
+export CXXFLAGS="${CXXFLAGS} -fprofile-generate"
+
+# Generate profiling data
+./"$PROJECT_NAME" --profile-generate
+
+# Use the generated profiling data for optimization
+export CFLAGS="${CFLAGS} -fprofile-use"
+export CXXFLAGS="${CXXFLAGS} -fprofile-use"
+
+# Recompile the sources using the generated profiling data
+$CC $CFLAGS "${SOURCES[@]}" -o "${OBJECTS[@]}"
+
+$CXX $CXXFLAGS "main.cpp" "utils.cpp" -o "$PROJECT_NAME"
+
+# Remove unnecessary sections from the binary
+strip --remove-section=.comment --remove-section=.note "$PROJECT_NAME"
+
+# Compress the binary using UPX
+upx -9 "$PROJECT_NAME"
+
+# Create a tarball containing the binary and any other relevant files
+tar cvzf "$PROJECT_NAME".tgz "$PROJECT_NAME" README.md LICENSE
+
+# Upload the tarball to a remote server
+scp "$PROJECT_NAME".tgz user@remotehost:/path/to/upload/directory
+
+# Extract the tarball on the remote host
+ssh user@remotehost "cd /path/to/extract/directory && tar xvzf '$PROJECT_NAME'.tgz"
+
+# Install the dependencies required by the application
+sudo apt install libpthread-stubs0-dev librt-dev libm-dev
+
+Run the application
 ./"$PROJECT_NAME"
