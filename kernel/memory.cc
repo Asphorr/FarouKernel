2b0e4eaf7a7e69b06595e027419f6ff9a6ae7b6f
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-10-09 19:26:14 +0700

    Update memory.cc

--- kernel/memory.cc
+++ kernel/memory.cc
@@ -1,51 +1,54 @@
 #include <iostream>
+#include <memory>
+#include <utility>
 #include <vector>
 #include <algorithm>
 #include <numeric>
-#include <functional>
 
-using namespace std;
-
-struct MemoryBlock {
-    uintptr_t startAddress;
-    uintptr_t endAddress;
-};
-
-auto allocate(size_t size) -> uintptr_t {
-    auto ptr = std::make_unique<MemoryBlock>();
-    ptr->startAddress = reinterpret_cast<uintptr_t>(malloc(size));
-    ptr->endAddress = ptr->startAddress + size;
-    blockList.push_back(move(ptr));
-    return ptr->startAddress;
-}
-
-void deallocate(uintptr_t addr) {
-    auto iter = find_if(begin(blockList), end(blockList), [&](const auto &p){return p->startAddress == addr;});
-    if (iter != end(blockList)) {
-        free((*iter)->startAddress);
-        (*iter).reset();
-        blockList.erase(iter);
+class MemoryManager {
+public:
+    static void allocate(std::size_t size) {
+        auto ptr = std::make_unique<MemoryBlock>();
+        ptr->startAddress = reinterpret_cast<uintptr_t>(malloc(size));
+        ptr->endAddress = ptr->startAddress + size;
+        blockList.emplace_back(std::move(ptr));
     }
-}
 
-void printFreeMemory() {
-    cout << "Total Free Memory: ";
-    auto totalFreeMem = accumulate(begin(blockList), end(blockList), 0, [](auto acc, const auto &p){return acc + (p->endAddress - p->startAddress);});
-    cout << totalFreeMem << endl;
-}
+    static void deallocate(uintptr_t addr) {
+        auto iter = std::find_if(blockList.begin(), blockList.end(), [&](const auto& p) { return p->startAddress == addr; });
+        if (iter != blockList.end()) {
+            free((*iter)->startAddress);
+            (*iter).reset();
+            blockList.erase(iter);
+        }
+    }
+
+    static void printFreeMemory() {
+        std::cout << "Total Free Memory: ";
+        auto totalFreeMem = std::accumulate(blockList.begin(), blockList.end(), 0, [](auto acc, const auto& p) { return acc + (p->endAddress - p->startAddress); });
+        std::cout << totalFreeMem << std::endl;
+    }
+private:
+    struct MemoryBlock {
+        uintptr_t startAddress;
+        uintptr_t endAddress;
+    };
+
+    static std::vector<std::unique_ptr<MemoryBlock>> blockList;
+};
 
 int main() {
     // Allocate some memory blocks
-    auto memPtr1 = allocate(4 * sizeof(int));
-    auto memPtr2 = allocate(8 * sizeof(double));
-    auto memPtr3 = allocate(16 * sizeof(char));
+    auto memPtr1 = MemoryManager::allocate(4 * sizeof(int));
+    auto memPtr2 = MemoryManager::allocate(8 * sizeof(double));
+    auto memPtr3 = MemoryManager::allocate(16 * sizeof(char));
 
     // Deallocate some memory blocks
-    deallocate(memPtr1);
-    deallocate(memPtr2);
+    MemoryManager::deallocate(memPtr1);
+    MemoryManager::deallocate(memPtr2);
 
     // Print free memory
-    printFreeMemory();
+    MemoryManager::printFreeMemory();
 
     return 0;
 }
