d516a065e7ad04d775e229559c738435081c1a6e
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-10-09 20:03:54 +0700

    Update util.h

--- kernel/util.h
+++ kernel/util.h
@@ -1,90 +1,24 @@
-#pragma once
+#include "util.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
+using namespace std;
 
-#define MALLOC_OR_DIE(size) \
-    ({ \
-        void *ptr = malloc((size)); \
-        if (!ptr) { \
-            perror("malloc failed"); \
-            abort(); \
-        } \
-        ptr; \
-    })
+// Print vector elements separated by spaces
+void util::printVector(const vector<int>& vec) {
+    cout << "[ ";
+    for (auto& elem : vec) {
+        cout << elem << " ";
+    }
+    cout << "]" << endl;
+}
 
-#define FREE_OR_DIE(ptr) \
-    do { \
-        if ((ptr)) { \
-            free((ptr)); \
-            (ptr) = NULL; \
-        } \
-    } while (0)
+// Return unique elements from input vector
+vector<int> util::getUniqueElements(const vector<int>& vec) {
+    return vector<int>(vec.begin(), vec.end());
+}
 
-#define PRINT_ERROR_AND_EXIT(message) \
-    do { \
-        fputs((message), stderr); \
-        exit(1); \
-    } while (0)
-
-#define STRDUP_OR_DIE(str) \
-    ({ \
-        char *copy = strdup((str)); \
-        if (!copy) { \
-            perror("strdup failed"); \
-            abort(); \
-        } \
-        copy; \
-    })
-
-#define REALLOC_OR_DIE(ptr, size) \
-    ({ \
-        void *new_ptr = realloc((ptr), (size)); \
-        if (!new_ptr && !(ptr)) { \
-            perror("realloc failed"); \
-            abort(); \
-        } \
-        new_ptr; \
-    })
-
-#define MEMCMPY_OR_DIE(dest, src, n) \
-    do { \
-        if (memcmp((src), (dest), (n))) { \
-            perror("memcpy failed"); \
-            abort(); \
-        } \
-    } while (0)
-
-#define MEMMOVE_OR_DIE(dest, src, n) \
-    do { \
-        if (memmove((src), (dest), (n))) { \
-            perror("memmove failed"); \
-            abort(); \
-        } \
-    } while (0)
-
-#define MEMSET_OR_DIE(ptr, val, n) \
-    do { \
-        if (memset((ptr), (val), (n))) { \
-            perror("memset failed"); \
-            abort(); \
-        } \
-    } while (0)
-
-#define BZERO_OR_DIE(ptr, n) \
-    do { \
-        if (bzero((ptr), (n))) { \
-            perror("bzero failed"); \
-            abort(); \
-        } \
-    } while (0)
-
-#define QSORT_OR_DIE(base, num, size, compar) \
-    do { \
-        if (qsort((base), (num), (size), (compar))) { \
-            perror("qsort failed"); \
-            abort(); \
-        } \
-    } while (0)
+// Check whether input vector has duplicates
+bool util::hasDuplicates(const vector<int>& vec) {
+    auto sortedVec = vec;
+    sort(sortedVec.begin(), sortedVec.end());
+    return adjacent_find(sortedVec.begin(), sortedVec.end()) != sortedVec.end();
+}
