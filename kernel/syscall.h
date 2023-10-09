f085bafa4569c5d8abd4f094653e2867a1583f8a
Asphorr <144871094+Asphorr@users.noreply.github.com>
2023-10-09 14:24:44 +0700

    Update syscall.h

--- kernel/syscall.h
+++ kernel/syscall.h
@@ -1,33 +1,51 @@
-#ifndef SYSCall_H
-#define SYSCall_H
+#pragma once
+
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
 
 // System call numbers
-#define SYSCALL_NUMBER_exit 1
-#define SYSCALL_NUMBER_read 2
-#define SYSCALL_NUMBER_write 3
-#define SYSCALL_NUMBER_open 4
-#define SYSCALL_NUMBER_close 5
-#define SYSCALL_NUMBER_create 6
-#define SYSCALL_NUMBER_delete 7
-#define SYSCALL_NUMBER_getpid 8
-#define SYSCALL_NUMBER_sleep 9
+enum {
+    SYSCALL_EXIT,
+    SYSCALL_READ,
+    SYSCALL_WRITE,
+    SYSCALL_OPEN,
+    SYSCALL_CLOSE,
+    SYSCALL_CREATE,
+    SYSCALL_DELETE,
+    SYSCALL_GETPID,
+    SYSCALL_SLEEP,
+};
 
 // Structure for storing system call information
-typedef struct {
-    int number;   // System call number
-    void (*function)(void); // Function to execute for the system call
-} syscall_t;
+struct syscall {
+    enum {
+        EXIT,
+        READ,
+        WRITE,
+        OPEN,
+        CLOSE,
+        CREATE,
+        DELETE,
+        GETPID,
+        SLEEP,
+    };
+    union {
+        int number;       // System call number
+        void (*function)(void); // Function to execute for the system call
+    };
+};
 
-// Declare external variables and functions
-extern int syscall(syscall_t *sc);
+// Define external variables and functions
+extern int syscall(struct syscall *sc);
 extern int exit(int status);
-extern int read(int fd, char *buf, int len);
-extern int write(int fd, const char *buf, int len);
-extern int open(const char *filename, int flags);
+extern ssize_t read(int fd, void *buf, size_t count);
+extern ssize_t write(int fd, const void *buf, size_t count);
+extern int open(const char *pathname, int flags);
 extern int close(int fd);
-extern int create(const char *filename, int mode);
-extern int delete(const char *filename);
-extern int getpid();
-extern int sleep(int seconds);
+extern int creat(const char *pathname, int mode);
+extern int unlink(const char *pathname);
+extern pid_t getpid(void);
+extern unsigned int sleep(unsigned int seconds);
 
-#endif // SYSCall_H
+#endif /* SYSCall_H */
