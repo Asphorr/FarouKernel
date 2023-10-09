ff6f22f48675c5704ef12c6f48213d2ff69a6e5d
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-10-09 17:03:13 +0700

    Update x86.c

--- arch/x86/x86.c
+++ arch/x86/x86.c
@@ -4,33 +4,35 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-/* Instruction sets */
-#define X86_INSTR_SET_SSE   (1 << 0)
-#define X86_INSTR_SET_SSE2  (1 << 1)
-#define X86_INSTR_SET_SSE3  (1 << 2)
-#define X86_INSTR_SET_SSSE3 (1 << 3)
-#define X86_INSTR_SET_SSE4_1 (1 << 4)
-#define X86_INSTR_SET_SSE4_2 (1 << 5)
-#define X86_INSTR_SET_AVX   (1 << 6)
-#define X86_INSTR_SET_AVX2  (1 << 7)
-#define X86_INSTR_SET_FMA   (1 << 8)
-#define X86_INSTR_SET_FMA4  (1 << 9)
-#define X86_INSTR_SET_FMA3  (1 << 10)
-#define X86_INSTR_SET_AVX512F (1 << 11)
-#define X86_INSTR_SET_AVX512CD (1 << 12)
-#define X86_INSTR_SET_AVX512ER (1 << 13)
-#define X86_INSTR_SET_AVX512PF (1 << 14)
-#define X86_INSTR_SET_AVX512EF (1 << 15)
+typedef enum {
+    SSE = 1 << 0,
+    SSE2 = 1 << 1,
+    SSE3 = 1 << 2,
+    SSSE3 = 1 << 3,
+    SSE4_1 = 1 << 4,
+    SSE4_2 = 1 << 5,
+    AVX = 1 << 6,
+    AVX2 = 1 << 7,
+    FMA = 1 << 8,
+    FMA4 = 1 << 9,
+    FMA3 = 1 << 10,
+    AVX512F = 1 << 11,
+    AVX512CD = 1 << 12,
+    AVX512ER = 1 << 13,
+    AVX512PF = 1 << 14,
+    AVX512EF = 1 << 15,
+} x86_instruction_set;
 
-/* Features */
-#define X86_FEATURE_FP     (1 << 0)
-#define X86_FEATURE_ASM    (1 << 1)
-#define X86_FEATURE_AVX    (1 << 2)
-#define X86_FEATURE_AES    (1 << 3)
-#define X86_FEATURE_RDRND  (1 << 4)
-#define X86_FEATURE_FMA    (1 << 5)
-#define X86_FEATURE_CVT16  (1 << 6)
-#define X86_FEATURE_MOVBE  (1 << 7)
+typedef enum {
+    FP = 1 << 0,
+    ASM = 1 << 1,
+    AVX = 1 << 2,
+    AES = 1 << 3,
+    RDRND = 1 << 4,
+    FMA = 1 << 5,
+    CVT16 = 1 << 6,
+    MOVBE = 1 << 7,
+} x86_feature;
 
 struct x86_cpuid {
     uint32_t vendor_id;
@@ -40,6 +42,14 @@ struct x86_cpuid {
     uint32_t instruction_sets[4];
 };
 
-extern struct x86_cpuid x86_cpuid;
+extern const struct x86_cpuid x86_cpuid;
+
+static inline bool has_feature(x86_feature feature) {
+    return (x86_cpuid.features & feature) != 0;
+}
+
+static inline bool has_instruction_set(x86_instruction_set set) {
+    return (x86_cpuid.instruction_sets[set / 32] & (1U << (set % 32))) != 0;
+}
 
 #endif /* X86_H */
