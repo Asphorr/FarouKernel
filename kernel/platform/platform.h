249b541e449375dce8c7754cdf076b1356051733
Asphorr <144871094+Asphorr@users.noreply.github.com>
2023-10-09 10:12:56 +0700

    Update platform.h

--- kernel/platform/platform.h
+++ kernel/platform/platform.h
@@ -1,36 +1,31 @@
-#ifndef PLATFORM_H
-#define PLATFORM_H
+#pragma once
 
-// Platform-specific definitions
+// Target Architecture
+#define TARGET_ARCHITECTURE x86_64
 
-// Define the target architecture
-#define TARGET_ARCHITECTURE "x86_64"
+// Operating System
+#define OPERATING_SYSTEM Linux
 
-// Define the operating system
-#define OPERATING_SYSTEM "Linux"
+// Compiler
+#define COMPILER GCC
 
-// Define the compiler being used
-#define COMPILER "GCC"
+// C Standard
+#define C_STANDARD C11
 
-// Define the version of the C standard being used
-#define C_STANDARD "C11"
+// C++ Standard
+#define CPP_STANDARD CPP17
 
-// Define the version of the C++ standard being used
-#define CPP_STANDARD "CPP17"
-
-// Define the maximum number of CPU cores to use
+// Maximum Number of CPU Cores
 #define MAX_CPU_CORES 8
 
-// Define the maximum amount of memory to use
+// Maximum Amount of Memory
 #define MAX_MEMORY 16384
 
-// Define the page size
+// Page Size
 #define PAGE_SIZE 4096
 
-// Define the directory where files will be stored
+// File Directory
 #define FILE_DIRECTORY "/home/user/files"
 
-// Define the executable name
+// Executable Name
 #define EXECUTABLE_NAME "my_program"
-
-#endif // PLATFORM_H
