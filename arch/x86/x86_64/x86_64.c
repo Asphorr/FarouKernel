c9b28fcce281bb9701d4deccf73653a1cdca8365
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-10-09 17:00:03 +0700

    Update x86_64.c

--- arch/x86/x86_64/x86_64.c
+++ arch/x86/x86_64/x86_64.c
@@ -4,35 +4,25 @@
 #include "x86_64.h"
 
 // CPUID information
-struct x86_64_cpuid x86_64_cpuid = {
-    .vendor_id = 0x12345678, // Intel or AMD vendor ID
-    .device_id = 0x23456789, // Device ID for the CPU
-    .revision = 0x01234567, // Revision number for the CPU
-    .features = X86_64_FEATURE_FP | X86_64_FEATURE_ASM | X86_64_FEATURE_AVX | X86_64_FEATURE_AES | X86_64_FEATURE_RDRND | X86_64_FEATURE_FMA | X86_64_FEATURE_CVT16 | X86_64_FEATURE_MOVBE,
-    .instruction_sets = {
-        [0] = X86_64_INSTR_SET_SSE,
-        [1] = X86_64_INSTR_SET_SSE2,
-        [2] = X86_64_INSTR_SET_SSE3,
-        [3] = X86_64_INSTR_SET_SSSE3,
-        [4] = X86_64_INSTR_SET_SSE4_1,
-        [5] = X86_64_INSTR_SET_SSE4_2,
-        [6] = X86_64_INSTR_SET_AVX,
-        [7] = X86_64_INSTR_SET_AVX2,
-        [8] = X86_64_INSTR_SET_FMA,
-        [9] = X86_64_INSTR_SET_FMA4,
-        [10] = X86_64_INSTR_SET_FMA3,
-        [11] = X86_64_INSTR_SET_AVX512F,
-        [12] = X86_64_INSTR_SET_AVX512CD,
-        [13] = X86_64_INSTR_SET_AVX512ER,
-        [14] = X86_64_INSTR_SET_AVX512PF,
-        [15] = X86_64_INSTR_SET_AVX512EF,
-    },
-};
+static const uint32_t vendor_id = 0x12345678; // Intel or AMD vendor ID
+static const uint32_t device_id = 0x23456789; // Device ID for the CPU
+static const uint32_t revision = 0x01234567; // Revision number for the CPU
+static const uint32_t features = X86_64_FEATURE_FP | X86_64_FEATURE_ASM | X86_64_FEATURE_AVX | X86_64_FEATURE_AES | X86_64_FEATURE_RDRND | X86_64_FEATURE_FMA | X86_64_FEATURE_CVT16 | X86_64_FEATURE_MOVBE;
+static const char* instruction_set_names[] = {"SSE", "SSE2", "SSE3", "SSSE3", "SSE4_1", "SSE4_2", "AVX", "AVX2", "FMA", "FMA4", "FMA3", "AVX512F", "AVX512CD", "AVX512ER", "AVX512PF", "AVX512EF"};
 
-void get_cpuid(struct x86_64_cpuid *cpuid) {
-    // Retrieve CPUID information from the processor
-    // This function would typically be implemented using assembly language
-    // and would retrieve the CPUID information using the CPUID instruction
+void print_cpuid_info(const struct x86_64_cpuid* cpuid) {
+    printf("CPUID Information:\n");
+    printf("  Vendor ID: %08x\n", cpuid->vendor_id);
+    printf("  Device ID: %08x\n", cpuid->device_id);
+    printf("  Revision: %08x\n", cpuid->revision);
+    printf("  Features: %08x\n", cpuid->features);
+    printf("  Instruction Sets: ");
+    for (size_t i = 0; i < sizeof(cpuid->instruction_sets) / sizeof(cpuid->instruction_sets[0]); ++i) {
+        if (cpuid->instruction_sets[i]) {
+            printf("%s ", instruction_set_names[i]);
+        }
+    }
+    putchar('\n');
 }
 
 int main() {
@@ -42,18 +32,7 @@ int main() {
     get_cpuid(&cpuid);
 
     // Print out the CPUID information
-    printf("CPUID Information:\n");
-    printf("  Vendor ID: %08x\n", cpuid.vendor_id);
-    printf("  Device ID: %08x\n", cpuid.device_id);
-    printf("  Revision: %08x\n", cpuid.revision);
-    printf("  Features: %08x\n", cpuid.features);
-    printf("  Instruction Sets: ");
-    for (int i = 0; i < sizeof(cpuid.instruction_sets) / sizeof(cpuid.instruction_sets[0]); i++) {
-        if (cpuid.instruction_sets[i]) {
-            printf("%s ", x86_64_instruction_set_names[i]);
-        }
-    }
-    printf("\n");
+    print_cpuid_info(&cpuid);
 
     return 0;
 }
