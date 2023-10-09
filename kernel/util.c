ef57bc4850b4a35e31639ed47547b5397e4e7727
Asphorr <144871094+Asphorr@users.noreply.github.com>
2023-10-09 10:16:38 +0700

    Update util.c

--- kernel/util.c
+++ kernel/util.c
@@ -1,23 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "util.h"
 
 void* malloc_or_die(size_t size) {
     void* ptr = malloc(size);
     if (ptr == NULL) {
         fprintf(stderr, "Failed to allocate %zu bytes\n", size);
-        exit(1);
+        exit(EXIT_FAILURE);
     }
     return ptr;
 }
 
 void free_or_die(void* ptr) {
-    if (ptr == NULL) {
+    if (ptr != NULL) {
+        free(ptr);
+    } else {
         fprintf(stderr, "Attempted to free null pointer\n");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
-    free(ptr);
 }
 
 void print_error_and_exit(const char* message) {
     fprintf(stderr, "%s\n", message);
-    exit(1);
+    exit(EXIT_FAILURE);
 }
