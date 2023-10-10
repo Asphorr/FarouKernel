909501b2415c564301791a62737d3c4b4f4b0697
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-10-10 13:36:36 +0700

    Update command_parser.hpp

--- kernel/command_parser.hpp
+++ kernel/command_parser.hpp
@@ -1,7 +1,100 @@
-#pragma once
-
+#include <iostream>
+#include <memory>
+#include <stdexcept>
 #include <string>
-#include <regex>
+#include <utility>
+#include <vector>
+
+// Forward declarations
+struct ParsedCommand;
+class CommandParser;
+class CommandExecutor;
+class ErrorHandler;
+
+using namespace std::literals;
+
+int main() {
+    // Create objects
+    auto parser = std::make_unique<CommandParser>();
+    auto executor = std::make_unique<CommandExecutor>();
+    auto handler = std::make_unique<ErrorHandler>();
+
+    // Configure console
+    Console console(std::move(parser), std::move(executor), std::move(handler));
+
+    // Start console loop
+    console.run();
+
+    return 0;
+}
+
+// Class definitions
+class Console {
+public:
+    // Constructor
+    Console(std::unique_ptr<CommandParser> parser, std::unique_ptr<CommandExecutor> executor, std::unique_ptr<ErrorHandler> handler)
+      : _parser{std::move(parser)}, _executor{std::move(executor)}, _handler{std::move(handler)} {}
+
+    // Copy constructor
+    Console(const Console& other) = delete;
+
+    // Move constructor
+    Console(Console&& other) noexcept = default;
+
+    // Copy assignment operator
+    Console& operator=(const Console& other) = delete;
+
+    // Move assignment operator
+    Console& operator=(Console&& other) noexcept = default;
+
+    // Destructor
+    ~Console() override = default;
+
+    // Run method
+    void run() override {
+        while (!isEof()) {
+            try {
+                auto command = readLine();
+                executeCommand(command);
+            } catch (const std::exception& e) {
+                handleError(e);
+            }
+        }
+    }
+
+private:
+    // Private constructor
+    Console(std::unique_ptr<CommandParser> parser, std::unique_ptr<CommandExecutor> executor, std::unique_ptr<ErrorHandler> handler)
+      : _parser{std::move(parser)}, _executor{std::move(executor)}, _handler{std::move(handler)} {}
+
+    // Check if EOF has been reached
+    [[nodiscard]] bool isEof() const {
+        return !std::cin.good();
+    }
+
+    // Read a line from standard input
+    [[noreturn]] std::string readLine() {
+        std::string line;
+        getline(std::cin, line);
+        return line;
+    }
+
+    // Parse and execute a command
+    void executeCommand(const std::string& command) {
+        auto parsedCommand = _parser->parse(command);
+        _executor->execute(parsedCommand);
+    }
+
+    // Handle an exception thrown during execution
+    void handleError(const std::exception& e) {
+        _handler->handle(e);
+    }
+
+    // Member variables
+    std::unique_ptr<CommandParser> _parser;
+    std::unique_ptr<CommandExecutor> _executor;
+    std::unique_ptr<ErrorHandler> _handler;
+};
 
 class CommandParser {
 public:
@@ -32,4 +125,14 @@ private:
     }
 
     std::regex _pattern;
+};
+
+class CommandExecutor {
+public:
+    virtual void execute(const CommandParser::ParsedCommand& command) = 0;
+};
+
+class ErrorHandler {
+public:
+    virtual void handle(const std::exception& error) = 0;
 };
