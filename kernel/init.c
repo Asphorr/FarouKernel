20774374ce18f4de95594cf2c33ee3d77e03d073
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-10-10 12:20:47 +0700

    Update init.c

--- kernel/init.c
+++ kernel/init.c
@@ -1,138 +1,85 @@
 #include <iostream>
-#include <vector>
+#include <fstream>
 #include <string>
-#include <algorithm>
-#include <functional>
-#include <memory>
-#include <thread>
-#include <mutex>
-#include <condition_variable>
-#include <future>
-#include <chrono>
-#include <exception>
+#include <vector>
+#include <map>
+#include <cmath>
+#include <numeric>
+#include <execution>
 
-// Define a custom exception type for program startup failures
-struct ProgramStartupFailure : public std::runtime_error {
-    explicit ProgramStartupFailure(const char *message) : runtime_error(message) {}
+// Define a struct to store key-value pairs
+struct KeyValuePair {
+    std::string key;
+    int value;
 };
 
-// A class representing a program runner
-class ProgramRunner {
-public:
-    // Constructor takes a vector of strings containing the program arguments
-    explicit ProgramRunner(const std::vector<std::string> &args) : args_(args) {}
-
-    // Run the program asynchronously
-    [[nodiscard]] bool runAsync() noexcept {
-        try {
-            // Create an asynchronous task to run the program
-            auto future = std::async(std::launch::async, [&](){
-                // Set up the program environment
-                setupEnvironment();
-
-                // Start the program
-                startProgram(args_);
-
-                // Wait for the program to finish
-                waitForProgramToFinish();
-
-                // Clean up and exit
-                cleanUpAndExit();
-            });
-
-            // Return whether the task was successfully created
-            return future.valid();
-        } catch (...) {
-            // Handle any exceptions thrown during async execution
-            handleException();
-            return false;
-        }
-    }
-
-    // Stop the program
-    void stop() noexcept {
-        // Acquire the mutex lock
-        std::lock_guard<std::mutex> lock(mtx_);
-
-        // Check if the program is still running
-        if (running_) {
-            // Signal the condition variable to notify the waiting thread
-            cv_.notify_one();
-
-            // Update the running flag
-            running_ = false;
-        }
-    }
-
-private:
-    // The program arguments
-    const std::vector<std::string> args_;
-
-    // Mutex for synchronizing access to the running flag
-    mutable std::mutex mtx_;
-
-    // Condition variable for signaling between threads
-    std::condition_variable cv_;
-
-    // Flag indicating whether the program is currently running
-    bool running_{true};
-
-    // Method for setting up the program environment
-    static void setupEnvironment() noexcept {};
-
-    // Method for starting the program
-    static int startProgram(const std::vector<std::string> &args) noexcept {
-        // TODO: Implement your own program startup logic here
-        return 0;
-    }
-
-    // Method for waiting for the program to finish
-    static void waitForProgramToFinish() noexcept {
-        // TODO: Implement your own program finishing logic here
-    }
-
-    // Method for cleaning up after the program has finished
-    static void cleanUpAndExit() noexcept {
-        // TODO: Implement your own cleanup logic here
-    }
-
-    // Method for handling exceptions thrown during async execution
-    static void handleException() noexcept {
-        // TODO: Implement your own exception handling logic here
+// Implement a custom comparator for sorting by value
+struct ValueComparator {
+    bool operator()(const KeyValuePair& lhs, const KeyValuePair& rhs) const {
+        return lhs.value < rhs.value;
     }
 };
+
+// Function to read data from a file and store it in a vector of key-value pairs
+std::vector<KeyValuePair> readDataFromFile(const char* filename) {
+    std::ifstream input(filename);
+    std::vector<KeyValuePair> data;
+    
+    // Read data from file and push it onto the vector
+    std::string line;
+    while (std::getline(input, line)) {
+        auto keyValuePair = splitLineIntoKeyAndValue(line);
+        data.push_back({keyValuePair.first, stoi(keyValuePair.second)});
+    }
+    
+    return data;
+}
+
+// Function to process data stored in a vector of key-value pairs
+void processData(std::vector<KeyValuePair>& data) {
+    // Sort the data based on the values
+    std::sort(data.begin(), data.end(), ValueComparator());
+    
+    // Iterate over the sorted data and update the values
+    for (size_t i = 0; i < data.size(); ++i) {
+        auto& pair = data[i];
+        
+        // Check if the current value is greater than 10
+        if (pair.value > 10) {
+            // Multiply the value by 2
+            pair.value *= 2;
+            
+            // Update the corresponding key
+            pair.key += "_doubled";
+        } else {
+            // Divide the value by 2
+            pair.value /= 2;
+            
+            // Update the corresponding key
+            pair.key += "_halved";
+        }
+    }
+}
+
+// Function to write processed data to a file
+void writeOutputToFile(const char* filename, const std::vector<KeyValuePair>& data) {
+    std::ofstream output(filename);
+    
+    // Write the processed data to the file
+    for (const auto& pair : data) {
+        output << pair.key << ": " << pair.value << "\n";
+    }
+}
 
 int main() {
-    // Get the program arguments from the console input
-    std::vector<std::string> args;
-    while (std::getline(std::cin, args)) {
-        break;
-    }
-
-    // Create a new instance of the program runner
-    ProgramRunner runner(args);
-
-    // Try to run the program asynchronously
-    if (!runner.runAsync()) {
-        std::cerr << "Failed to start program." << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    // Print a prompt to the console
-    std::cout << "Press Enter to stop the program...";
-
-    // Read a character from the console input
-    char c;
-    std::cin >> c;
-
-    // Check if the entered character is 'enter'
-    if (c == '\n') {
-        // Stop the program
-        runner.stop();
-    } else {
-        // Print an error message to the console
-        std::cerr << "Invalid input. Please press Enter to stop the program." << std::endl;
-    }
-
-    return EXIT_SUCCESS;
+    // Read data from a file and store it in a vector of key-value pairs
+    std::vector<KeyValuePair> data = readDataFromFile("input.txt");
+    
+    // Process the data
+    processData(data);
+    
+    // Write the processed data to a file
+    writeOutputToFile("output.txt", data);
+    
+    return 0;
 }
