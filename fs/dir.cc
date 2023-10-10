98cb6131fb2f7705cfac2cb1bf99f62c3a379fa1
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-10-10 12:40:43 +0700

    Update dir.cc

--- fs/dir.cc
+++ fs/dir.cc
@@ -1,66 +1,81 @@
 #include <iostream>
 #include <fstream>
-#include <filesystem>
 #include <string>
 #include <vector>
+#include <filesystem>
 
-// Define some useful aliases
-using Path = std::filesystem::path;
-using FileStream = std::basic_ifstream<char>;
-using StringVector = std::vector<std::string>;
+using namespace std;
 
-// Function to create a new directory and write to a file within it
-void createDirectoryAndWriteToFile(Path& path, StringVector& data) {
-    // Create the directory if it doesn't exist
-    if (!fs::exists(path)) {
-        fs::create_directory(path);
+class DirectoryWriter {
+public:
+    // Create a new directory and write data to file
+    template <typename T>
+    void createDirectoryAndWriteToFile(const fs::path& dirPath, const vector<T>& data) {
+        try {
+            // Check if directory exists
+            if (!fs::exists(dirPath)) {
+                // Create directory
+                fs::create_directories(dirPath);
+            }
+            
+            // Open output stream
+            fstream outStream(dirPath / "file.txt", ios::out | ios::trunc);
+            
+            // Write data to file
+            for (auto&& elem : data) {
+                outStream << elem << endl;
+            }
+            
+            // Close output stream
+            outStream.close();
+        } catch (exception& e) {
+            cerr << "Error creating or writing to file: " << e.what() << endl;
+        }
     }
     
-    // Open the file for writing
-    FileStream outfile(path / "file.txt", std::ios::out | std::ios::trunc);
-    
-    // Write the data to the file
-    for (const auto& line : data) {
-        outfile << line << '\n';
+    // Read data from file and delete it
+    void readFromFileAndDeleteIt(const fs::path& filePath) {
+        try {
+            // Open input stream
+            fstream inStream(filePath, ios::in);
+            
+            // Read data from file
+            string content((istreambuf_iterator<char>(inStream)), istreambuf_iterator<char>());
+            
+            // Print data to console
+            cout << "File contents:" << endl << content << endl;
+            
+            // Delete file
+            fs::remove(filePath);
+        } catch (exception& e) {
+            cerr << "Error reading or deleting file: " << e.what() << endl;
+        }
     }
     
-    // Close the file
-    outfile.close();
-}
-
-// Function to read from a file and delete it
-void readFromFileAndDeleteIt(Path& path) {
-    // Read the entire file into memory
-    FileStream infile(path / "file.txt");
-    std::string content((std::istreambuf_iterator<char>(infile)), std::istreambuf_iterator<char>());
-    
-    // Print the contents of the file
-    std::cout << "File contents:\n" << content << "\n";
-    
-    // Delete the file
-    fs::remove(path / "file.txt");
-}
-
-// Function to delete an empty directory
-void deleteEmptyDirectory(Path& path) {
-    // Check if the directory exists and is empty
-    if (fs::is_empty(path)) {
-        // Remove the directory
-        fs::remove(path);
-    } else {
-        throw std::runtime_error("Cannot remove non-empty directory.");
+    // Delete empty directory
+    void deleteEmptyDirectory(const fs::path& dirPath) {
+        try {
+            // Check if directory is empty
+            if (fs::is_empty(dirPath)) {
+                // Remove directory
+                fs::remove(dirPath);
+            } else {
+                throw runtime_error("Cannot remove non-empty directory.");
+            }
+        } catch (exception& e) {
+            cerr << "Error removing directory: " << e.what() << endl;
+        }
     }
-}
+};
 
 int main() {
-    // Set up the paths and data
-    Path path{"dir"};
-    StringVector data{"Hello", "World!"};
+    DirectoryWriter writer;
+    fs::path path{"dir"};
+    vector<string> data{"Hello", "World!"};
     
-    // Call the functions
-    createDirectoryAndWriteToFile(path, data);
-    readFromFileAndDeleteIt(path);
-    deleteEmptyDirectory(path);
+    writer.createDirectoryAndWriteToFile(path, data);
+    writer.readFromFileAndDeleteIt(path / "file.txt");
+    writer.deleteEmptyDirectory(path);
     
     return 0;
 }
