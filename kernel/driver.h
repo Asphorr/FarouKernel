ba37125e76f371c0572d331b6353529b50548033
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-10-10 12:24:56 +0700

    Update driver.h

--- kernel/driver.h
+++ kernel/driver.h
@@ -1,69 +1,171 @@
-#pragma once
+#include <cassert>
+#include <chrono>
+#include <condition_variable>
+#include <exception>
+#include <future>
+#include <iostream>
+#include <memory>
+#include <mutex>
+#include <optional>
+#include <string>
+#include <system_error>
+#include <tuple>
+#include <type_traits>
+#include <vector>
 
-class Driver {
-public:
-    std::string name;
-    std::function<void()> entryPoint;
-    std::atomic<bool> initialized{false};
-    std::atomic<uint8_t> status{0x00};
+// A simple mutex wrapper around std::mutex
+struct Mutex {
+    std::mutex mtx_;
 
-    static void init(Driver& drv) {
-        drv.initialized = true;
-        drv.status = 0x01;
+    void lock() { mtx_.lock(); }
+    void unlock() { mtx_.unlock(); }
+};
+
+// A simple condition variable wrapper around std::condition_variable
+struct ConditionVariable {
+    std::condition_variable cv_;
+
+    void wait(std::unique_lock<Mutex>& lk) { cv_.wait(lk); }
+    void notify_one() { cv_.notify_one(); }
+    void notify_all() { cv_.notify_all(); }
+};
+
+// A simple semaphore implementation based on std::atomic and std::condition_variable
+struct Semaphore {
+    std::atomic<int> count_{0};
+    ConditionVariable cv_;
+
+    void signal() {
+        int oldCount = count_.fetch_add(1);
+        if (oldCount <= 0) {
+            cv_.notify_one();
+        }
     }
 
-    static void load(Driver& drv) {
-        if (!drv.initialized || drv.status != 0x01) {
-            throw std::runtime_error("Cannot load driver");
+    void wait() {
+        int oldCount = count_.fetch_sub(1);
+        if (oldCount > 0) {
+            cv_.wait([this]{return count_.load() >= 0;});
+        } else {
+            cv_.wait([this]{return count_.load() > 0;});
         }
-        drv.status = 0x02;
-        drv.entryPoint();
-    }
-
-    static void unload(Driver& drv) {
-        if (!drv.initialized || drv.status == 0x03) {
-            throw std::runtime_error("Cannot unload driver");
-        }
-        drv.status = 0x03;
-    }
-
-    template <typename T>
-    static void sendCommand(Driver& drv, T command) {
-        if (!drv.initialized || drv.status != 0x02) {
-            throw std::runtime_error("Cannot send command to driver");
-        }
-        // Send command to driver
-    }
-
-    template <typename T>
-    static T receiveData(Driver& drv) {
-        if (!drv.initialized || drv.status != 0x02) {
-            throw std::runtime_error("Cannot receive data from driver");
-        }
-        // Receive data from driver
-        return {};
     }
 };
 
-#define DriverInit(drv) \
-    do { \
-        drv.initialized = true; \
-        drv.status = 0x01; \
-    } while (0)
+// A simple thread pool implementation based on std::async and std::packaged_task
+struct ThreadPool {
+    std::vector<std::thread> threads_;
+    std::queue<std::packaged_task<void()>> tasks_;
+    Semaphore semaphore_;
 
-#define DriverLoad(drv) \
-    do { \
-        if (!drv.initialized || drv.status != 0x01) { \
-            throw std::runtime_error("Cannot load driver"); \
-        } \
-        drv.status = 0x02; \
-        drv.entryPoint(); \
-    } while (0)
+    ThreadPool(size_t numThreads) {
+        assert(numThreads > 0 && "Invalid number of threads");
+        threads_.reserve(numThreads);
+        for (size_t i = 0; i < numThreads; ++i) {
+            threads_.emplace_back(&ThreadPool::worker, this);
+        }
+    }
 
-#define DriverUnload(drv) \
-    do { \
-        if (!drv.initialized || drv.status == 0x03) { \
-            throw std::runtime_error("Cannot unload driver"); \
-        } \
-        drv.status = 0x03; \
-    } while (0)
+    ~ThreadPool() {
+        stop();
+    }
+
+    void submitTask(std::packaged_task<void()> task) {
+        {
+            std::scoped_lock lock{semaphore_};
+            tasks_.push(std::move(task));
+        }
+        semaphore_.signal();
+    }
+
+    void worker() {
+        while (true) {
+            std::packaged_task<void()> task;
+            {
+                std::scoped_lock lock{semaphore_};
+                if (tasks_.empty()) {
+                    break;
+                }
+                task = std::move(tasks_.front());
+                tasks_.pop();
+            }
+            try {
+                task();
+            } catch (...) {
+                std::cerr << "Exception thrown by task" << std::endl;
+            }
+        }
+    }
+
+    void stop() {
+        {
+            std::scoped_lock lock{semaphore_};
+            tasks_.clear();
+        }
+        for (auto& thread : threads_) {
+            thread.join();
+        }
+    }
+};
+
+// A simple driver interface
+struct IDriver {
+    virtual ~IDriver() = default;
+
+    virtual void initialize() = 0;
+    virtual void load() = 0;
+    virtual void unload() = 0;
+    virtual void sendCommand(std::function<void()>) = 0;
+    virtual std::optional<std::any> receiveData() = 0;
+};
+
+// A concrete driver implementation
+class MyDriver : public IDriver {
+public:
+    MyDriver(const std::string& name) : name_(name) {}
+
+    void initialize() override {
+        std::cout << "MyDriver::initialize()" << std::endl;
+    }
+
+    void load() override {
+        std::cout << "MyDriver::load()" << std::endl;
+    }
+
+    void unload() override {
+        std::cout << "MyDriver::unload()" << std::endl;
+    }
+
+    void sendCommand(std::function<void()> cmd) override {
+        std::cout << "MyDriver::sendCommand()" << std::endl;
+    }
+
+    std::optional<std::any> receiveData() override {
+        std::cout << "MyDriver::receiveData()" << std::endl;
+        return {};
+    }
+
+private:
+    std::string name_;
+};
+
+// A factory function to create drivers
+std::shared_ptr<IDriver> createDriver(const std::string& name) {
+    auto driver = std::make_shared<MyDriver>(name);
+    driver->initialize();
+    return driver;
+}
+
+int main() {
+    // Create a thread pool with 4 threads
+    ThreadPool pool(4);
+
+    // Submit some tasks to the thread pool
+    for (int i = 0; i < 16; ++i) {
+        pool.submitTask([](){
+            std::cout << "Hello from thread pool!" << std::endl;
+        });
+    }
+
+    // Wait for all tasks to complete
+    pool.stop();
