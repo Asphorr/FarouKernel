da3655e3b2c1c09d9e13e393d1d7c435496cb5e6
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-10-11 01:24:33 +0700

    Update setup.c

--- kernel/setup.c
+++ kernel/setup.c
@@ -1,64 +1,80 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <algorithm>
-#include <functional>
 #include <memory>
 #include <utility>
 #include <type_traits>
 
-using namespace std::literals;
+// Define a type alias for a unique pointer to a vector of strings
+using StringVectorPtr = std::unique_ptr<std::vector<std::string>>;
 
-class Program {
-public:
-    explicit Program(const std::vector<std::string>& args) : _args{args}, _env{} {}
-    
-    void run() {
-        auto syscallTable = createSyscallTable();
-        
-        // Set up the program environment
-        setupEnv(_env);
-        
-        // Start the program
-        startProg(_args, _env, syscallTable);
-        
-        // Wait for the program to finish
-        waitForProgToFinish();
-        
-        // Clean up and exit
-        cleanupAndExit();
-    }
-private:
-    static std::unique_ptr<SyscallTable> createSyscallTable() {
-        // Create the system call table
-        // ...
-    }
-    
-    static void setupEnv(const Env& env) {
-        // Set up the program environment
-        // ...
-    }
-    
-    static void startProg(const std::vector<std::string>& args, const Env& env, SyscallTable& syscallTable) {
-        // Start the program
-        // ...
-    }
-    
-    static void waitForProgToFinish() {
-        // Wait for the program to finish
-        // ...
-    }
-    
-    static void cleanupAndExit() {
-        // Clean up and exit
-        // ...
-    }
+// Define a function template to convert a string literal to a vector of strings
+template <typename T>
+StringVectorPtr split(T&& str) {
+    // Split the input string into words
+    auto words = std::make_unique<std::vector<std::string>>();
+    std::istringstream iss(str);
+    std::copy(std::istream_iterator<std::string>(iss),
+              std::istream_iterator<std::string>(),
+              std::back_inserter(*words));
+    return words;
+}
+
+// Define a struct to represent a system call
+struct SystemCall {
+    int number;
+    std::string name;
 };
+
+// Define a function to retrieve the system call table
+SystemCall* getSystemCalls() {
+    // Implement this function to retrieve the system call table
+    throw std::runtime_error("Not implemented");
+}
+
+// Define a function to set up the program environment
+void setupEnvironment(const std::vector<std::string>& args) {
+    // Implement this function to set up the program environment
+    throw std::runtime_error("Not implemented");
+}
+
+// Define a function to start the program
+void startProgram(const std::vector<std::string>& args, const std::vector<SystemCall>& syscalls) {
+    // Implement this function to start the program
+    throw std::runtime_error("Not implemented");
+}
+
+// Define a function to wait for the program to finish
+void waitForProgramToFinish() {
+    // Implement this function to wait for the program to finish
+    throw std::runtime_error("Not implemented");
+}
+
+// Define a function to clean up and exit
+void cleanupAndExit() {
+    // Implement this function to clean up and exit
+    throw std::runtime_error("Not implemented");
+}
 
 int main(int argc, char** argv) {
     try {
-        Program p{argv};
-        p.run();
+        // Parse command line arguments
+        auto args = split(argc > 0 ? argv[0] : ""sv);
+
+        // Retrieve the system call table
+        auto syscalls = getSystemCalls();
+
+        // Set up the program environment
+        setupEnvironment(args);
+
+        // Start the program
+        startProgram(args, *syscalls);
+
+        // Wait for the program to finish
+        waitForProgramToFinish();
+
+        // Clean up and exit
+        cleanupAndExit();
     } catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << '\n';
         return EXIT_FAILURE;
