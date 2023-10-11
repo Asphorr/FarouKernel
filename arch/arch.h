280d585608536a39661300bafddb10ca57358996
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-10-11 21:52:21 +0700

    Update arch.h

--- arch/arch.h
+++ arch/arch.h
@@ -1,77 +1,147 @@
-#pragma once
+#include <concepts>
+#include <iostream>
+#include <string>
+#include <vector>
 
-#include <cstdint>
-#include <type_traits>
-
-namespace kernel {
-
-enum class Isa : uint8_t { X86_64 };
-enum class Endianness : uint8_t { Little };
-
-struct ArchitectureDefinitions {
-    static constexpr Isaa kIsa = Isa::X86_64;
-    static constexpr Endianness kEndianness = Endianness::Little;
-    static constexpr size_t kWordSize = sizeof(uint64_t);
+// Define a concept for a platform-specific data structure
+template<typename T>
+concept PlatformSpecificData = requires() {
+    typename T::foo;
+    typename T::bar;
 };
 
-struct AddressSpaceLayout {
-    static constexpr bool kAslr = true;
+// Define a concept for a platform-specific instruction set
+template<typename T>
+concept PlatformSpecificInstructionSet = requires() {
+    std::is_same_v<T, "platform specific instructions">;
 };
 
-struct StackLayout {
-    static constexpr int kStackGrowthDirection = 1;
+// Define a concept for a platform-specific function
+template<typename T>
+concept PlatformSpecificFunction = requires() {
+    std::is_same_v<T, "platform specific functions">;
 };
 
-struct PageTableLayout {
-    static constexpr size_t kPageTableEntries = 1024;
-    static constexpr size_t kPageTableShift = 12;
+// Implement the architecture definitions
+class ArchitectureDefinitions {
+public:
+    static constexpr auto kIsa = Isa::X86_64;
+    static constexpr auto kEndianness = Endianness::Little;
+    static constexpr auto kWordSize = sizeof(uint64_t);
 };
 
-struct VirtualMemoryLayout {
-    static constexpr size_t kVirtualMemoryStart = 0x10000000;
-    static constexpr size_t kVirtualMemoryEnd = 0x20000000;
+// Implement the address space layout
+class AddressSpaceLayout {
+public:
+    static constexpr auto kAslr = true;
 };
 
-struct InterruptController {
-    static constexpr bool kInterruptControllerEnabled = false;
+// Implement the stack layout
+class StackLayout {
+public:
+    static constexpr auto kStackGrowthDirection = 1;
 };
 
-struct Timekeeping {
-    static constexpr size_t kTimekeepingFrequency = 1000;
+// Implement the page table layout
+class PageTableLayout {
+public:
+    static constexpr auto kPageTableEntries = 1024;
+    static constexpr auto kPageTableShift = 12;
 };
 
-struct ConsoleOutput {
-    static constexpr size_t kConsoleOutputBufferSize = 4096;
+// Implement the virtual memory layout
+class VirtualMemoryLayout {
+public:
+    static constexpr auto kVirtualMemoryStart = 0x10000000;
+    static constexpr auto kVirtualMemoryEnd = 0x20000000;
 };
 
-struct Networking {
-    static constexpr bool kNetworkingEnabled = true;
+// Implement the interrupt controller
+class InterruptController {
+public:
+    static constexpr auto kInterruptControllerEnabled = false;
 };
 
-struct FileSystem {
+// Implement the timekeeping
+class Timekeeping {
+public:
+    static constexpr auto kTimekeepingFrequency = 1000;
+};
+
+// Implement the console output
+class ConsoleOutput {
+public:
+    static constexpr auto kConsoleOutputBufferSize = 4096;
+};
+
+// Implement the networking
+class Networking {
+public:
+    static constexpr auto kNetworkingEnabled = true;
+};
+
+// Implement the file system
+class FileSystem {
+public:
     enum class Type : uint8_t { FAT32 };
-    static constexpr Type kFileSystemType = Type::FAT32;
+    static constexpr auto kFileSystemType = Type::FAT32;
 };
 
-struct MemoryManagement {
-    static constexpr bool kMemoryManagementEnabled = true;
+// Implement the memory management
+class MemoryManagement {
+public:
+    static constexpr auto kMemoryManagementEnabled = true;
 };
 
-struct KernelDebugging {
-    static constexpr bool kKernelDebuggingEnabled = false;
+// Implement the kernel debugging
+class KernelDebugging {
+public:
+    static constexpr auto kKernelDebuggingEnabled = false;
 };
 
-struct PlatformSpecificData {
+// Implement the platform-specific data structure
+class PlatformSpecificDataImpl {
+public:
     uint32_t foo;
     uint64_t bar;
 };
 
-struct PlatformSpecificInstructions {
-    static constexpr char kPlatformSpecificInstructions[] = "\x0f\xc7\xc0";
-};
+// Implement the platform-specific instruction set
+constexpr char kPlatformSpecificInstructions[] = "\x0f\xc7\xc0";
 
-struct PlatformSpecificFunctions {
-    inline void platform_specific_function();
-};
+// Implement the platform-specific function
+inline void platform_specific_function() {}
 
-} // namespace kernel
+// Create instances of each component
+ArchitectureDefinitions arch{};
+AddressSpaceLayout addrspace{};
+StackLayout stack{};
+PageTableLayout pagetable{};
+VirtualMemoryLayout virtmem{};
+InterruptController intrctrl{};
+Timekeeping timekeeping{};
+ConsoleOutput consoleout{};
+Networking network{};
+FileSystem filesystem{};
+MemoryManagement memmanage{};
+KernelDebugging debug{};
+PlatformSpecificData psd{};
+PlatformSpecificInstructionSet pis{kPlatformSpecificInstructions};
+PlatformSpecificFunction psdfn{platform_specific_function};
+
+// Test the components
+static_assert(arch.isa == Isa::X86_64);
+static_assert(addrspace.aslr == true);
+static_assert(stack.growth_direction == 1);
+static_assert(pagetable.entries == 1024 && pagetable.shift == 12);
+static_assert(virtmem.start == 0x10000000 && virtmem.end == 0x20000000);
+static_assert(!intrctrl.enabled());
+static_assert(timekeeping.frequency == 1000);
+static_assert(consoleout.buffer_size == 4096);
+static_assert(network.enabled());
+static_assert(filesystem.type == FileSystem::Type::FAT32);
+static_assert(memmanage.enabled());
+static_assert(!debug.enabled());
+static_assert(psd.foo == 0 && psd.bar == 0);
+static_assert(pis.instructions[0] == '\x0f');
+static_assert(psdfn.fn() == nullptr);
