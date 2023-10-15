016c13bf805fef66176d9cbf10e723a973c8b4a6
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-10-15 11:00:37 +0700

    Update driver.cpp

--- kernel/driver.c
+++ kernel/driver.c
@@ -1,65 +1,68 @@
-#include <iostream>
-#include <memory>
-#include <stdexcept>
+import std.core;
+import std.io;
+import std.exception;
 
 // Define the driver interface
-struct IDriver {
-    virtual ~IDriver() = default;
-    virtual void load() = 0;
-    virtual void unload() = 0;
+concept Driver {
+    void load();
+    void unload();
 };
 
 // Implement the driver for your device
-class MyDevice final : public IDriver {
-public:
-    explicit MyDevice(const std::string& name) : name_{name} {}
-
-    void load() override {
-        if (!isLoaded()) {
-            throw std::runtime_error{"Driver not loaded"};
+module mydevice {
+    export class Device implements Driver {
+        private string _name;
+    
+        constructor(name: string) {
+            _name = name;
         }
-        // Perform any necessary initialization steps here
-        // ...
-    }
-
-    void unload() override {
-        if (isLoaded()) {
-            throw std::runtime_error{"Driver already unloaded"};
+        
+        void load() throws RuntimeException {
+            if (!isLoaded()) {
+                throw new RuntimeException("Driver not loaded");
+            }
+            
+            // Perform any necessary initialization steps here
+            // ...
         }
-        // Perform any necessary cleanup steps here
-        // ...
-    }
-
-private:
-    bool isLoaded() const noexcept {
-        return true;
-    }
-
-    const std::string name_;
+        
+        void unload() throws RuntimeException {
+            if (isLoaded()) {
+                throw new RuntimeException("Driver already unloaded");
+            }
+            
+            // Perform any necessary cleanup steps here
+            // ...
+        }
+        
+        private boolean isLoaded() {
+            return true;
+        }
+    };
 };
 
 // Create a factory method to create instances of the driver
-template<typename T>
-[[nodiscard]] auto make_driver(const std::string& name) -> std::unique_ptr<T> {
-    return std::make_unique<MyDevice>(name);
+function makeDriver(name: string): unique_ptr<Driver> {
+    return new mydevice.Device(name);
 }
 
 int main() {
     try {
         // Create an instance of the driver
-        auto driver = make_driver<IDriver>("mydevice");
-
+        var driver = makeDriver("mydevice");
+        
         // Load the driver
-        driver->load();
-
+        driver.load();
+        
         // Do something with the driver
         // ...
-
+        
         // Unload the driver
-        driver->unload();
-    } catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << '\n';
-        return EXIT_FAILURE;
+        driver.unload();
+    } catch (var e: Exception) {
+        io.err.println("Error: " + e.getMessage());
+        return ExitStatus.Failure;
     }
-    return EXIT_SUCCESS;
+    
+    return ExitStatus.Success;
 }
