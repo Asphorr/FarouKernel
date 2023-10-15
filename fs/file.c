caa641a6934a975a0c29399a5d0603b557f5884f
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-10-15 22:09:30 +0700

    Update file.c

--- fs/file.c
+++ fs/file.c
@@ -1,110 +1,95 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <iterator>
-#include <functional>
+#include <fstream>
+#include <type_traits>
+
+template <typename T>
+concept WritableType = requires(T t) {
+    { t.write(std::declval<char[]>(), std::declval<size_t>())} -> std::same_as<void>;
+};
+
+template <typename T>
+concept ReadableType = requires(T t) {
+    { t.read(std::declval<char[]>(), std::declval<size_t>())} -> std::same_as<void>;
+};
 
 class File {
 public:
-    explicit File(const std::string& filename): _filename{filename}, _isOpen{false} {}
-    
-    ~File() { close(); }
-    
+    explicit File(const std::string& filename) : _filename(filename), _isOpen(false) {}
+
+    virtual ~File() { close(); }
+
     bool open() {
         if (_isOpen) {
             return true;
         }
-        
-        // Try to open the file
-        _fileStream.open(_filename, std::ios::in | std::ios::out | std::ios::binary);
-        if (!_fileStream.good()) {
+
+        try {
+            _fileStream.open(_filename, std::ios::in | std::ios::out | std::ios::binary);
+            if (!_fileStream.good()) {
+                throw std::runtime_error("Failed to open file");
+            }
+        } catch (const std::exception& e) {
+            std::cerr << "Error opening file: " << e.what() << '\n';
             return false;
         }
-        
+
         _isOpen = true;
         return true;
     }
-    
+
     void close() {
         if (_isOpen) {
             _fileStream.close();
             _isOpen = false;
         }
     }
-    
-    template<typename T>
-    bool read(T* value) {
-        if (!_isOpen || !value) {
+
+    template <ReadableType T>
+    bool read(T& value) {
+        if (!_isOpen) {
             return false;
         }
-        
-        // Read the next byte from the file
-        unsigned char byte;
-        _fileStream >> byte;
-        if (!_fileStream.good()) {
+
+        size_t numBytes = sizeof(T);
+        char buffer[numBytes];
+
+        try {
+            _fileStream.read(buffer, numBytes);
+            if (!_fileStream.good()) {
+                throw std::runtime_error("Failed to read from file");
+            }
+        } catch (const std::exception& e) {
+            std::cerr << "Error reading from file: " << e.what() << '\n';
             return false;
         }
-        
-        // Convert the byte to the desired type
-        *value = static_cast<T>(byte);
+
+        memcpy(&value, buffer, numBytes);
         return true;
     }
-    
-    template<typename T>
+
+    template <WritableType T>
     bool write(const T& value) {
         if (!_isOpen) {
             return false;
         }
-        
-        // Convert the value to a byte array
-        std::array<unsigned char, sizeof(T)> bytes;
-        std::memcpy(&bytes[0], &value, sizeof(T));
-        
-        // Write the byte array to the file
-        _fileStream << bytes;
-        if (!_fileStream.good()) {
+
+        size_t numBytes = sizeof(T);
+        char buffer[numBytes];
+
+        try {
+            _fileStream.write(buffer, numBytes);
+            if (!_fileStream.good()) {
+                throw std::runtime_error("Failed to write to file");
+            }
+        } catch (const std::exception& e) {
+            std::cerr << "Error writing to file: " << e.what() << '\n';
             return false;
         }
-        
+
         return true;
     }
+
 private:
     std::string _filename;
     std::ifstream _fileStream;
     bool _isOpen;
 };
-
-int main() {
-    File file{"example.txt"};
-    if (!file.open()) {
-        std::cerr << "Failed to open file\n";
-        return 1;
-    }
-    
-    // Read some values from the file
-    int x;
-    float y;
-    double z;
-    if (!file.read(&x)) {
-        std::cerr << "Failed to read integer from file\n";
-        return 1;
-    }
-    if (!file.read(&y)) {
-        std::cerr << "Failed to read floating-point number from file\n";
-        return 1;
-    }
-    if (!file.read(&z)) {
-        std::cerr << "Failed to read double precision floating-point number from file\n";
-        return 1;
-    }
-    
-    // Print the values
-    std::cout << "Integer: " << x << "\n";
-    std::cout << "Float: " << y << "\n";
-    std::cout << "Double: " << z << "\n";
-    
-    // Close the file
-    file.close();
-    return 0;
-}
