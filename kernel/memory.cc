3da703360c75f9937c6c2039f19eb5d635b6ea8f
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-11-16 00:11:19 +0700

    Update memory.cc

--- kernel/memory.cc
+++ kernel/memory.cc
@@ -1,19 +1,10 @@
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <functional>
-#include <initializer_list>
-#include <iterator>
-#include <limits>
-#include <new>
-#include <type_traits>
-#include <utility>
-#include <vector>
+#include <memory>
+#include <algorithm>
 
 template <typename T>
 struct MemoryBlock {
-    T* startAddress;
-    T* endAddress;
+  std::unique_ptr<T[]> data;
+  std::size_t size;
 };
 
 template <typename T>
@@ -21,48 +12,62 @@ using BlockList = std::vector<MemoryBlock<T>>;
 
 template <typename T>
 void allocate(BlockList<T>& list, std::size_t size) {
-    auto ptr = new MemoryBlock<T>;
-    ptr->startAddress = reinterpret_cast<T*>(malloc(size));
-    ptr->endAddress = ptr->startAddress + size / sizeof(T);
-    list.push_back(*ptr);
+  list.emplace_back();
+  auto& block = list.back();
+  block.data = std::make_unique<T[]>(size);
+  block.size = size;
 }
 
 template <typename T>
 void deallocate(BlockList<T>& list, T* address) {
-    auto iter = std::find_if(list.begin(), list.end(), [address](const MemoryBlock<T>& block) {
-        return block.startAddress <= address && address < block.endAddress;
-    });
-    if (iter != list.end()) {
-        free(iter->startAddress);
-        list.erase(iter);
-    }
+  auto iter = std::find_if(list.begin(), list.end(), [address](const MemoryBlock<T>& block) {
+      return block.data.get() <= address && address < block.data.get() + block.size;
+  });
+  if (iter != list.end()) {
+      list.erase(iter);
+  }
 }
 
 template <typename T>
 void printFreeMemory(const BlockList<T>& list) {
-    std::printf("Total Free Memory: %zu\n", std::accumulate(list.begin(), list.end(), 0, [](auto acc, const MemoryBlock<T>& block) {
-        return acc + (block.endAddress - block.startAddress);
-    }));
+  std::printf("Total Free Memory: %zu\n", std::accumulate(list.begin(), list.end(), 0, [](auto acc, const MemoryBlock<T>& block) {
+      return acc + block.size;
+  }));
 }
 
-int main() {
-    BlockList<int> intBlocks;
-    BlockList<double> doubleBlocks;
-    BlockList<char> charBlocks;
+template <typename T>
+void reallocate(BlockList<T>& list, T* address, std::size_t newSize) {
+  auto iter = std::find_if(list.begin(), list.end(), [address](const MemoryBlock<T>& block) {
+      return block.data.get() <= address && address < block.data.get() + block.size;
+  });
+  if (iter != list.end()) {
+      iter->data = std::make_unique<T[]>(newSize);
+      iter->size = newSize;
+  }
+}
 
-    // Allocate some memory blocks
-    allocate(intBlocks, 4 * sizeof(int));
-    allocate(doubleBlocks, 8 * sizeof(double));
-    allocate(charBlocks, 16 * sizeof(char));
+template <typename T>
+void resize(BlockList<T>& list, T* address, std::size_t newSize) {
+  auto iter = std::find_if(list.begin(), list.end(), [address](const MemoryBlock<T>& block) {
+      return block.data.get() <= address && address < block.data.get() + block.size;
+  });
+  if (iter != list.end()) {
+      auto newData = std::make_unique<T[]>(newSize);
+      std::copy(iter->data.get(), iter->data.get() + std::min(iter->size, newSize), newData.get());
+      iter->data = std::move(newData);
+      iter->size = newSize;
+  }
+}
 
-    // Deallocate some memory blocks
-    deallocate(intBlocks, intBlocks[0].startAddress);
-    deallocate(doubleBlocks, doubleBlocks[0].startAddress);
-
-    // Print free memory
-    printFreeMemory(intBlocks);
-    printFreeMemory(doubleBlocks);
-    printFreeMemory(charBlocks);
-
-    return 0;
+template <typename T>
+void copy(BlockList<T>& list, T* sourceAddress, T* destinationAddress, std::size_t size) {
+  auto sourceIter = std::find_if(list.begin(), list.end(), [sourceAddress](const MemoryBlock<T>& block) {
+      return block.data.get() <= sourceAddress && sourceAddress < block.data.get() + block.size;
+  });
+  auto destinationIter = std::find_if(list.begin(), list.end(), [destinationAddress](const MemoryBlock<T>& block) {
+      return block.data.get() <= destinationAddress && destinationAddress < block.data.get() + block.size;
+  });
+  if (sourceIter != list.end() && destinationIter != list.end()) {
+      std::copy(sourceIter->data.get(), sourceIter->data.get() + std::min(sourceIter->size, size), destinationIter->data.get());
+  }
 }
