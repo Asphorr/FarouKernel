457b11ce38602d5d65667b453cbb2dad39575ba4
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-11-16 18:08:38 +0700

    Update dir_functions.h

--- fs/dir_functions.h
+++ fs/dir_functions.h
@@ -5,9 +5,16 @@
 #include <vector>
 #include <filesystem>
 
+namespace DirFunctions {
+
 void createDirectoryAndWriteToFile(const std::filesystem::path& dirPath, const std::vector<std::string>& data);
 void readFromFileAndDeleteIt(const std::filesystem::path& filePath);
 void deleteEmptyDirectory(const std::filesystem::path& dirPath);
 bool hasFilesInDir(const std::filesystem::path& dirPath);
+void copyDirectory(const std::filesystem::path& sourcePath, const std::filesystem::path& destinationPath);
+std::vector<std::filesystem::path> getAllFilesInDir(const std::filesystem::path& dirPath);
+std::vector<std::filesystem::path> getAllDirectoriesInDir(const std::filesystem::path& dirPath);
 
-#endif
+} // namespace DirFunctions
+
+#endif // DIR_FUNCTIONS_H
