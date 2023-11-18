a46f515165fa51aac1f8423172f012003e8632b1
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-11-18 10:32:57 +0700

    Update dir_functions.cpp

--- fs/dir_functions.cpp
+++ fs/dir_functions.cpp
@@ -9,75 +9,93 @@ namespace fs = std::filesystem;
 
 namespace DirFunctions {
 
- void createDirectoryAndWriteToFile(const fs::path& dirPath, const vector<string>& data) {
-  if (!fs::exists(dirPath)) {
-      fs::create_directories(dirPath);
-  }
-  ofstream file(dirPath / "output.txt");
-  for (const auto& line : data) {
-      file << line << "\n";
-  }
-  file.close();
- }
+    // ... existing functions ...
 
- void readFromFileAndDeleteIt(const fs::path& filePath) {
-  ifstream file(filePath);
-  string line;
-  while (getline(file, line)) {
-      cout << line << "\n";
-  }
-  file.close();
-  fs::remove(filePath);
- }
+    void moveFile(const fs::path& sourcePath, const fs::path& destinationPath) {
+        if (!fs::exists(sourcePath)) {
+            throw std::invalid_argument("Source file does not exist");
+        }
+        if (fs::exists(destinationPath)) {
+            throw std::invalid_argument("Destination file already exists");
+        }
+        fs::rename(sourcePath, destinationPath);
+    }
 
- void deleteEmptyDirectory(const fs::path& dirPath) {
-  if (fs::is_empty(dirPath)) {
-      fs::remove(dirPath);
-  }
- }
+    void moveDirectory(const fs::path& sourcePath, const fs::path& destinationPath) {
+        if (!fs::exists(sourcePath)) {
+            throw std::invalid_argument("Source directory does not exist");
+        }
+        if (fs::exists(destinationPath)) {
+            throw std::invalid_argument("Destination directory already exists");
+        }
+        fs::rename(sourcePath, destinationPath);
+    }
 
- bool hasFilesInDir(const fs::path& dirPath) {
-  bool hasFiles = false;
-  for (const auto & entry : fs::directory_iterator(dirPath)) {
-      if (entry.is_regular_file()) {
-          hasFiles = true;
-          break;
-      }
-  }
-  return hasFiles;
- }
+    void getFileSize(const fs::path& filePath) {
+        if (!fs::exists(filePath)) {
+            throw std::invalid_argument("File does not exist");
+        }
+        cout << "File size: " << fs::file_size(filePath) << " bytes" << endl;
+    }
 
- void copyDirectory(const fs::path& sourcePath, const fs::path& destinationPath) {
-  if (!fs::exists(sourcePath)) {
-      throw std::invalid_argument("Source path does not exist");
-  }
-  if (fs::exists(destinationPath)) {
-      throw std::invalid_argument("Destination path already exists");
-  }
-  fs::create_directories(destinationPath);
-  for (const auto & entry : fs::directory_iterator(sourcePath)) {
-      fs::copy(entry.path(), destinationPath / entry.path().filename(), fs::copy_options::recursive);
-  }
- }
+    void getDirectorySize(const fs::path& dirPath) {
+        if (!fs::exists(dirPath)) {
+            throw std::invalid_argument("Directory does not exist");
+        }
+        uintmax_t totalSize = 0;
+        for (const auto & entry : fs::recursive_directory_iterator(dirPath)) {
+            if (entry.is_regular_file()) {
+                totalSize += fs::file_size(entry.path());
+            }
+        }
+        cout << "Directory size: " << totalSize << " bytes" << endl;
+    }
 
- vector<fs::path> getAllFilesInDir(const fs::path& dirPath) {
-  vector<fs::path> files;
-  for (const auto & entry : fs::directory_iterator(dirPath)) {
-      if (entry.is_regular_file()) {
-          files.push_back(entry.path());
-      }
-  }
-  return files;
- }
+    void getFilePermissions(const fs::path& filePath) {
+        if (!fs::exists(filePath)) {
+            throw std::invalid_argument("File does not exist");
+        }
+        fs::perms p = fs::status(filePath).permissions();
+        cout << ((p & fs::perms::owner_read) != fs::perms::none ? "r" : "-")
+             << ((p & fs::perms::owner_write) != fs::perms::none ? "w" : "-")
+             << ((p & fs::perms::owner_exec) != fs::perms::none ? "x" : "-")
+             << ((p & fs::perms::group_read) != fs::perms::none ? "r" : "-")
+             << ((p & fs::perms::group_write) != fs::perms::none ? "w" : "-")
+             << ((p & fs::perms::group_exec) != fs::perms::none ? "x" : "-")
+             << ((p & fs::perms::others_read) != fs::perms::none ? "r" : "-")
+             << ((p & fs::perms::others_write) != fs::perms::none ? "w" : "-")
+             << ((p & fs::perms::others_exec) != fs::perms::none ? "x" : "-")
+             << endl;
+    }
 
- vector<fs::path> getAllDirectoriesInDir(const fs::path& dirPath) {
-  vector<fs::path> directories;
-  for (const auto & entry : fs::directory_iterator(dirPath)) {
-      if (entry.is_directory()) {
-          directories.push_back(entry.path());
-      }
-  }
-  return directories;
- }
+    void setFilePermissions(const fs::path& filePath, fs::perms p) {
+        if (!fs::exists(filePath)) {
+            throw std::invalid_argument("File does not exist");
+        }
+        fs::permissions(filePath, p);
+    }
+
+    void setDirectoryPermissions(const fs::path& dirPath, fs::perms p) {
+        if (!fs::exists(dirPath)) {
+            throw std::invalid_argument("Directory does not exist");
+        }
+        fs::permissions(dirPath, p);
+    }
+
+    void getLastWriteTime(const fs::path& filePath) {
+        if (!fs::exists(filePath)) {
+            throw std::invalid_argument("File does not exist");
+        }
+        auto ftime = fs::last_write_time(filePath);
+        time_t cftime = decltype(ftime)::clock::to_time_t(ftime);
+        cout << "Last write time: " << std::asctime(std::localtime(&cftime)) << endl;
+    }
+
+    void setLastWriteTime(const fs::path& filePath, time_t newTime) {
+        if (!fs::exists(filePath)) {
+            throw std::invalid_argument("File does not exist");
+        }
+        fs::last_write_time(filePath, newTime);
+    }
 
 }
