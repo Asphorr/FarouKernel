20aa2eb26b2089a8be9ca09bcc5dff2d5a77a564
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-11-19 22:14:24 +0700

    Update system.c

--- kernel/system.c
+++ kernel/system.c
@@ -1,41 +1,33 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <functional>
+#include <stdio.h>
+#include <stdlib.h>
 
-// Using C++23 features
-using namespace std::literals; // Enable string literals
+// Define a structure for the system
+typedef struct {
+    int system_id;
+    char system_name[20];
+} System;
 
-struct User {
-    std::string name;
-    std::string email;
-};
+// Function to initialize the system
+void system_init(System *system, int id, const char *name) {
+    system->system_id = id;
+    strcpy(system->system_name, name);
+}
 
-class System {
-public:
-    void addUser(std::string&& name, std::string&& email) {
-        users.push_back({std::move(name), std::move(email)});
-    }
-
-    [[nodiscard]] bool hasUserWithEmail(const std::string& email) const noexcept {
-        return std::any_of(users.begin(), users.end(), [&email](const auto& user) {
-            return user.email == email;
-        });
-    }
-
-private:
-    std::vector<User> users;
-};
+// Function to print system information
+void system_print(System *system) {
+    printf("System ID: %d\n", system->system_id);
+    printf("System Name: %s\n", system->system_name);
+}
 
 int main() {
+    // Create a system
     System system;
 
-    system.addUser("John Doe"s, "johndoe@example.com"s);
-    system.addUser("Jane Doe"s, "janedoe@example.com"s);
+    // Initialize the system
+    system_init(&system, 1, "My System");
 
-    std::cout << "Has John Doe? "sv << system.hasUserWithEmail("johndoe@example.com"s) << '\n';
-    std::cout << "Has Jane Doe? "sv << system.hasUserWithEmail("janedoe@example.com"s) << '\n';
+    // Print the system information
+    system_print(&system);
 
     return 0;
 }
