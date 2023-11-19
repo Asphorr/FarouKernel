60704d30161692cae8de615693880434d7c18aa9
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-11-19 08:30:22 +0700

    Update cpuid.h

--- arch/x86/cpuid.h
+++ arch/x86/cpuid.h
@@ -1,27 +1,38 @@
 #ifndef CPUID_H
 #define CPUID_H
 
+#include <stdint.h>
+
 typedef struct {
-  unsigned int eax;
-  unsigned int ebx;
-  unsigned int ecx;
-  unsigned int edx;
+  uint32_t eax;
+  uint32_t ebx;
+  uint32_t ecx;
+  uint32_t edx;
 } cpuid_t;
 
-void cpuid(cpuid_t *info, unsigned int function) {
+void cpuid(cpuid_t *info, uint32_t function) {
   __asm__ __volatile__ (
       "cpuid;"
       : "=a"(info->eax), "=b"(info->ebx), "=c"(info->ecx), "=d"(info->edx)
-      : "a"(function)
+      : "a"(function), "c"(0)
   );
 }
 
-void cpuid_ex(cpuid_t *info, unsigned int function, unsigned int subfunction) {
+void cpuid_ex(cpuid_t *info, uint32_t function, uint32_t subfunction) {
   __asm__ __volatile__ (
       "cpuid;"
       : "=a"(info->eax), "=b"(info->ebx), "=c"(info->ecx), "=d"(info->edx)
       : "a"(function), "c"(subfunction)
   );
+}
+
+void cpuid_vendor(char vendor[13]) {
+  cpuid_t info;
+  cpuid(&info, 0);
+  *(uint32_t*)(vendor) = info.ebx;
+  *(uint32_t*)(vendor+4) = info.edx;
+  *(uint32_t*)(vendor+8) = info.ecx;
+  vendor[12] = '\0';
 }
 
 #endif // CPUID_H
