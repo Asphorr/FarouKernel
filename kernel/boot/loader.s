94ec85232e3ca1181cbdf172228e32d5d6e44668
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-11-24 00:48:54 +0700

    Delete kernel directory

--- kernel/boot/loader.s
+++
@@ -1,8 +0,0 @@
-[bits 32]
-[extern main] ; Declare that we will be referencing the external symbol 'main',
-             ; so the linker can substitute the final address
-
-call main ; Invoke main() in our C kernel
-jmp $ ; Hang forever when we're done
-
-%include "print_string.asm"
