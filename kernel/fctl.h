bfe2fa0bd9d56ae99d814ebb5873f8c66144285f
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-12-08 01:06:46 +0700

    Update fctl.h

--- kernel/fctl.h
+++ kernel/fctl.h
@@ -1,167 +1,78 @@
-#ifndef FCTL_H
-#define FCTL_H
-
-// Include necessary headers
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <linux/slab.h>
 
-// Define structure for our file control block
 struct fcb {
-    unsigned int magic;   // Magic number to identify FCB
-    unsigned int flags;   // Flags for file status
-    unsigned int mode;    // File access mode
-    unsigned int offset; // Current read/write position
+   unsigned int magic;
+   unsigned int flags;
+   unsigned int mode;
+   unsigned int offset;
 };
 
-// Function prototypes
-static struct fcb *alloc_fcb(void);
-static void free_fcb(struct fcb *fcb);
-static int fcb_open(struct inode *inode, struct file *filp);
-static int fcb_release(struct inode *inode, struct file *filp);
-static ssize_t fcb_read(struct file *filp, char __user *buf, size_t count, loff_t *ppos);
-static ssize_t fcb_write(struct file *filp, const char __user *buf, size_t count, loff_t *ppos);
-static int fcb_ioctl(struct file *filp, unsigned int cmd, unsigned long arg);
+static struct kmem_cache *fcb_cache;
 
-// Module initialization and cleanup functions
-static int __init init_fctl(void);
-static void __exit exit_fctl(void);
+static struct fcb *alloc_fcb(void)
+{
+   struct fcb *fcb;
+
+   fcb = kmem_cache_zalloc(fcb_cache, GFP_KERNEL);
+   if (!fcb)
+       return ERR_PTR(-ENOMEM);
+
+   fcb->magic = FCB_MAGIC;
+   fcb->flags = 0;
+   fcb->mode = 0;
+   fcb->offset = 0;
+
+   return fcb;
+}
+
+static void free_fcb(struct fcb *fcb)
+{
+   kmem_cache_free(fcb_cache, fcb);
+}
+
+static int fcb_open(struct inode *inode, struct file *filp)
+{
+   struct fcb *fcb;
+
+   fcb = alloc_fcb();
+   if (IS_ERR(fcb))
+       return PTR_ERR(fcb);
+
+   filp->private_data = fcb;
+
+   return 0;
+}
+
+static int fcb_release(struct inode *inode, struct file *filp)
+{
+   struct fcb *fcb = filp->private_data;
+
+   free_fcb(fcb);
+
+   return 0;
+}
+
+static int __init init_fctl(void)
+{
+   fcb_cache = kmem_cache_create("fcb_cache", sizeof(struct fcb), 0, SLAB_HWCACHE_ALIGN, NULL);
+   if (!fcb_cache)
+       return -ENOMEM;
+
+   printk(KERN_INFO "FCTL: initialized\n");
+   return 0;
+}
+
+static void __exit exit_fctl(void)
+{
+   kmem_cache_destroy(fcb_cache);
+   printk(KERN_INFO "FCTL: unloaded\n");
+}
+
+module_init(init_fctl);
+module_exit(exit_fctl);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Your Name");
 MODULE_DESCRIPTION("File Control Block driver");
-
-// Global variables
-static struct kmem_cache *fcb_cache;
-
-// Initialize the file control block cache
-static int __init init_fctl(void)
-{
-    fcb_cache = kmem_cache_create("fcb_cache", sizeof(struct fcb), 0, SLAB_HWCACHE_ALIGN, NULL);
-    if (!fcb_cache)
-        return -ENOMEM;
-
-    printk(KERN_INFO "FCTL: initialized\n");
-    return 0;
-}
-
-// Clean up the file control block cache
-static void __exit exit_fctl(void)
-{
-    kmem_cache_destroy(fcb_cache);
-    printk(KERN_INFO "FCTL: unloaded\n");
-}
-
-// Allocate a new file control block
-static struct fcb *alloc_fcb(void)
-{
-    struct fcb *fcb;
-
-    fcb = kmem_cache_zalloc(fcb_cache, GFP_KERNEL);
-    if (!fcb)
-        return ERR_PTR(-ENOMEM);
-
-    fcb->magic = FCB_MAGIC;
-    fcb->flags = 0;
-    fcb->mode = 0;
-    fcb->offset = 0;
-
-    return fcb;
-}
-
-// Free a file control block
-static void free_fcb(struct fcb *fcb)
-{
-    kmem_cache_free(fcb_cache, fcb);
-}
-
-// Open a file associated with a file control block
-static int fcb_open(struct inode *inode, struct file *filp)
-{
-    struct fcb *fcb;
-
-    fcb = alloc_fcb();
-    if (IS_ERR(fcb))
-        return PTR_ERR(fcb);
-
-    filp->private_data = fcb;
-
-    return 0;
-}
-
-// Release a file associated with a file control block
-static int fcb_release(struct inode *inode, struct file *filp)
-{
-    struct fcb *fcb = filp->private_data;
-
-    free_fcb(fcb);
-
-    return 0;
-}
-
-// Read data from a file associated with a file control block
-static ssize_t fcb_read(struct file *filp,;
-char __user *buf, size_t count, loff_t *ppos)
-{
-struct fcb *fcb = filp->private_data;
-unsigned int len;
-
-// Check that the file is open for reading
-if ((fcb->flags & O_RDONLY) == 0)
-    return -EBADF;
-
-// Calculate the length of the read operation
-len = min_t(unsigned int, count, i_size_read(&file_inode(filp)->i_size) - *ppos);
-if (len == 0)
-    return 0;
-
-// Perform the read operation
-if (copy_to_user(buf, fcb + *ppos, len))
-    return -EFAULT;
-
-// Update the current read position
-*ppos += len;
-
-return len;
-}
-
-// Write data to a file associated with a file control block
-static ssize_t fcb_write(struct file *filp, const char __user *buf, size_t count, loff_t *ppos)
-{
-struct fcb *fcb = filp->private_data;
-unsigned int len;
-
-// Check that the file is open for writing
-if ((fcb->flags & O_WRONLY) == 0)
-    return -EBADF;
-
-// Calculate the length of the write operation
-len = min_t(unsigned int, count, i_size_read(&file_inode(filp)->i_size) - *ppos);
-if (len == 0)
-    return 0;
-
-// Perform the write operation
-if (copy_from_user(fcb + *ppos, buf, len))
-    return -EFAULT;
-
-// Update the current write position
-*ppos += len;
-
-return len;
-}
-
-// Handle an ioctl request on a file associated with a file control block
-static int fcb_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
-{
-struct fcb *fcb = filp->private_data;
-
-switch (cmd) {
-case FCB_IOCTL_GETFLAGS:
-    return put_user(fcb->flags, (int __user *)arg);
-case FCB_IOCTL_SETFLAGS:
-    get_user(fcb->flags, (int __user *)arg);
-    return 0;
-default:
-    return -ENOTTY;
-}
-}
