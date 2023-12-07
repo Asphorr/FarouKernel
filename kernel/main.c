111293e326ed1d360db5dc6360bbe6d1dfe197fd
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-12-08 01:08:41 +0700

    Update main.c

--- kernel/main.c
+++ kernel/main.c
@@ -15,62 +15,86 @@
 // Define a concept for the kernel class
 template <typename T>
 concept KernelConcept = requires(T k) {
-    // Requirements for the kernel class
-    { k.getName() } -> std::convertible_to<std::string>;
-    { k.getVersion() } -> std::convertible_to<std::pair<int, int>>;
-    { k.getReleaseDate() } -> std::convertible_to<std::string>;
-    { k.getBuildTime() } -> std::convertible_to<std::string>;
-    { k.getAuthor() } -> std::convertible_to<std::string>;
-    { k.getCopyright() } -> std::convertible_to<std::string>;
-    { k.getLicense() } -> std::convertible_to<std::string>;
-    { k.getDescription() } -> std::convertible_to<std::string>;
-    { k.isRunning() } -> std::same_as<bool>;
-    { k.isStopped() } -> std::same_as<bool>;
-    { k.requestStop() } -> std::same_as<void>;
-    { k.waitForStop() } -> std::same_as<void>;
+   // Requirements for the kernel class
+   { k.getName() } -> std::convertible_to<std::string>;
+   { k.getVersion() } -> std::convertible_to<std::pair<int, int>>;
+   { k.getReleaseDate() } -> std::convertible_to<std::string>;
+   { k.getBuildTime() } -> std::convertible_to<std::string>;
+   { k.getAuthor() } -> std::convertible_to<std::string>;
+   { k.getCopyright() } -> std::convertible_to<std::string>;
+   { k.getLicense() } -> std::convertible_to<std::string>;
+   { k.getDescription() } -> std::convertible_to<std::string>;
+   { k.isRunning() } -> std::same_as<bool>;
+   { k.isStopped() } -> std::same_as<bool>;
+   { k.requestStop() } -> std::same_as<void>;
+   { k.waitForStop() } -> std::same_as<void>;
+   { k.start() } -> std::same_as<void>;
 };
 
 // Implement the kernel class
 class Kernel final : public KernelConcept<Kernel> {
 public:
-    // Constructor
-    Kernel(const std::string& name, int major, int minor, const std::string& releaseDate, const std::string& buildTime, const std::string& author, const std::string& copyright, const std::string& license, const std::string& description) :
-        m_name{name}, m_major{major}, m_minor{minor}, m_releaseDate{releaseDate}, m_buildTime{buildTime}, m_author{author}, m_copyright{copyright}, m_license{license}, m_description{description} {}
+   // Constructor
+   Kernel(const std::string& name, int major, int minor, const std::string& releaseDate, const std::string& buildTime, const std::string& author, const std::string& copyright, const std::string& license, const std::string& description) :
+       m_name{name}, m_major{major}, m_minor{minor}, m_releaseDate{releaseDate}, m_buildTime{buildTime}, m_author{author}, m_copyright{copyright}, m_license{license}, m_description{description} {}
 
-    // Destructor
-    ~Kernel() noexcept override {
-        requestStop();
-        waitForStop();
-    }
+   // Destructor
+   ~Kernel() noexcept override {
+       requestStop();
+       waitForStop();
+   }
 
-    // Getters
-    [[nodiscard]] std::string getName() const override { return m_name; }
-    [[nodiscard]] std::pair<int, int> getVersion() const override { return {m_major, m_minor}; }
-    [[nodiscard]] std::string getReleaseDate() const override { return m_releaseDate; }
-    [[nodiscard]] std::string getBuildTime() const override { return m_buildTime; }
-    [[nodiscard]] std::string getAuthor() const override { return m_author; }
-    [[nodiscard]] std::string getCopyright() const override { return m_copyright; }
-    [[nodiscard]] std::string getLicense() const override { return m_license; }
-    [[nodiscard]] std::string getDescription() const override { return m_description; }
+   // Getters
+   [[nodiscard]] std::string getName() const override { return m_name; }
+   [[nodiscard]] std::pair<int, int> getVersion() const override { return {m_major, m_minor}; }
+   [[nodiscard]] std::string getReleaseDate() const override { return m_releaseDate; }
+   [[nodiscard]] std::string getBuildTime() const override { return m_buildTime; }
+   [[nodiscard]] std::string getAuthor() const override { return m_author; }
+   [[nodiscard]] std::string getCopyright() const override { return m_copyright; }
+   [[nodiscard]] std::string getLicense() const override { return m_license; }
+   [[nodiscard]] std::string getDescription() const override { return m_description; }
 
-    // Check running status
-    [[nodiscard]] bool isRunning() const override { return m_running; }
-    [[nodiscard]] bool isStopped() const override { return m_stopped; }
+   // Check running status
+   [[nodiscard]] bool isRunning() const override { return m_running; }
+   [[nodiscard]] bool isStopped() const override { return m_stopped; }
 
-    // Request stop
-    void requestStop() override {
-        m_stopRequested = true;
-        m_cv.notify_one();
-    }
+   // Request stop
+   void requestStop() override {
+       m_stopRequested = true;
+       m_cv.notify_one();
+   }
 
-    // Wait for stop
-    void waitForStop() override {
-        std::scoped_lock lock(m_mutex);
-        m_cv.wait(lock, [this]{ return m_stopRequested; });
-    }
+   // Wait for stop
+   void waitForStop() override {
+       std::scoped_lock lock(m_mutex);
+       m_cv.wait(lock, [this]{ return m_stopRequested; });
+   }
 
-    // Start the kernel
-    void start() {
-        m_running = true;
-        m_stopped = false;
+   // Start the kernel
+   void start() override {
+       if (isRunning()) {
+           throw std::runtime_error("Kernel is already running");
+       }
 
+       m_running = true;
+       m_stopped = false;
+
+       // Add code to start the kernel here
+   }
+
+private:
+   std::string m_name;
+   int m_major;
+   int m_minor;
+   std::string m_releaseDate;
+   std::string m_buildTime;
+   std::string m_author;
+   std::string m_copyright;
+   std::string m_license;
+   std::string m_description;
+   bool m_running = false;
+   bool m_stopped = true;
+   bool m_stopRequested = false;
+   std::mutex m_mutex;
+   std::condition_variable m_cv;
+};
