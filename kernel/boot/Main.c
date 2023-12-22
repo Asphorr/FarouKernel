8470285fa7f39ac1bb879a0afa0a927e7cdf1c44
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2023-12-22 16:36:59 +0700

    Update Main.c

--- kernel/boot/Main.c
+++ kernel/boot/Main.c
@@ -1,5 +1,7 @@
 #include <stdint.h>
 #include <stddef.h>
+#include <iostream>
+#include <cstdlib>
 
 #define VIDEO_MEMORY 0xb8000
 #define WHITE_ON_BLACK 0x0f
@@ -52,7 +54,83 @@ int main() {
 }
 
 void kernel_main(void) {
-    // Initialize memory manager
+    
+struct Block {
+  size_t size;
+  Block* next;
+};
+
+Block* head = nullptr;
+
+bool init_memory_manager(size_t totalSize) {
+  // Allocate memory for the head block
+  head = static_cast<Block*>(malloc(totalSize));
+  if (head == nullptr) {
+      // Failed to allocate memory
+      return false;
+  }
+
+  // Set the size of the head block to the total size
+  head->size = totalSize - sizeof(Block);
+
+  // There are no free blocks yet, so the next block is null
+  head->next = nullptr;
+
+  return true;
+}
+
+void* allocate_memory(size_t size) {
+  Block* current = head;
+  while (current != nullptr) {
+      if (current->size >= size) {
+          // Found a free block of sufficient size
+          void* ptr = static_cast<void*>(current + 1);
+
+          // Adjust the size of the remaining free space
+          current->size -= size;
+
+          // Move the head pointer forward
+          head = current->next;
+
+          return ptr;
+      }
+
+      // No suitable free block found, move to the next one
+      current = current->next;
+  }
+
+  // No suitable free block found
+  return nullptr;
+}
+
+void deallocate_memory(void* ptr) {
+  // In a real implementation, you would need to merge adjacent free blocks
+  // This is left as an exercise
+}
+
+void kernel_main(void) {
+  if (!init_memory_manager(1024)) {
+      std::cerr << "Error: Failed to initialize memory manager." << std::endl;
+      return;
+  }
+
+  void* ptr = allocate_memory(sizeof(int));
+  if (ptr == nullptr) {
+      std::cerr << "Error: Failed to allocate memory." << std::endl;
+      return;
+  }
+
+  int* intPtr = static_cast<int*>(ptr); {
+  *intPtr = 10;
+
+  std::cout << "Value: " << *intPtr << std::endl;
+
+  deallocate_memory(ptr);
+}
+
+
+
+    
     if (!init_memory_manager()) {
         // Handle error
     }
