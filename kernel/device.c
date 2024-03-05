3f51ab635f611cc4f98aaad3151b608eb06ac5db
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2024-03-05 19:07:45 +0300

    Update device.c

--- kernel/device.c
+++ kernel/device.c
@@ -6,6 +6,8 @@
 #include <linux/major.h>
 #include <linux/minor.h>
 #include <asm/ioctl.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Mikhail");
@@ -14,6 +16,7 @@ MODULE_DESCRIPTION("Optimized Character Device Driver");
 // Define constants for the module parameters
 #define MAJOR_NUM 245 // Major number for the character device
 #define MINOR_NUM 0   // Minor number for the character device
+#define BUFFER_SIZE 1024 // Size of the character device buffer
 
 // Structure representing the character device
 typedef struct _my_device {
@@ -21,6 +24,7 @@ typedef struct _my_device {
     struct class *class; // Pointer to the device's class
     dev_t devno;          // Device number
     atomic_t open_count; // Number of times the device has been opened
+    char *buffer;        // Buffer for storing data
 } my_device_t;
 
 // Function prototypes
@@ -31,7 +35,7 @@ ssize_t my_write(struct file *file, const char __user *buf, size_t count, loff_t
 long my_ioctl(struct file *file, unsigned int cmd, unsigned long arg);
 
 // Global variables
-static my_device_t my_device; // The character device object
+static my_device_t *my_device; // The character device object
 
 // Initialize the character device
 static int __init my_init(void)
@@ -39,41 +43,47 @@ static int __init my_init(void)
     int retval;
 
     // Allocate memory for the character device structure
-    my_device = kmalloc(sizeof(*my_device), GFP_KERNEL);
+    my_device = kmalloc(sizeof(my_device_t), GFP_KERNEL);
     if (!my_device) {
         pr_err("%s: Failed to allocate memory for the character device structure\n", __func__);
         return -ENOMEM;
     }
 
+    // Allocate memory for the character device buffer
+    my_device->buffer = kmalloc(BUFFER_SIZE, GFP_KERNEL);
+    if (!my_device->buffer) {
+        pr_err("%s: Failed to allocate memory for the character device buffer\n", __func__);
+        kfree(my_device);
+        return -ENOMEM;
+    }
+
     // Set up the character device structure
-    memset(my_device, 0, sizeof(*my_device));
+    memset(my_device, 0, sizeof(my_device_t));
     my_device->cdev.owner = THIS_MODULE;
-    my_device->cdev.ops = &my_fops;
+    cdev_init(&my_device->cdev, &my_fops);
     my_device->class = class_create(THIS_MODULE, "myclass");
     if (IS_ERR(my_device->class)) {
         pr_err("%s: Failed to create class\n", __func__);
+        kfree(my_device->buffer);
         kfree(my_device);
         return PTR_ERR(my_device->class);
     }
 
     // Register the character device
-    retval = alloc_chrdev_region(&my_device->devno, MAJOR_NUM, MINOR_NUM, "mychar");
+    retval = alloc_chrdev_region(&my_device->devno, MINOR_NUM, 1, "mychar");
     if (retval < 0) {
         pr_err("%s: Failed to register character device\n", __func__);
         class_destroy(my_device->class);
+        kfree(my_device->buffer);
         kfree(my_device);
         return retval;
     }
 
+    // Add the character device to the system
+    cdev_add(&my_device->cdev, my_device->devno, 1);
+
     // Create the character device node
-    retval = device_create(my_device->class, NULL, my_device->devno, NULL, "mychar%d", MINOR_NUM);
-    if (retval < 0) {
-        pr_err("%s: Failed to create character device node\n", __func__);
-        unregister_chrdev_region(my_device->devno, MINOR_NUM);
-        class_destroy(my_device->class);
-        kfree(my_device);
-        return retval;
-    }
+    device_create(my_device->class, NULL, my_device->devno, NULL, "mychar%d", MINOR_NUM);
 
     // Print a message indicating successful initialization
     pr_info("%s: Initialized character device %s\n", __func__, "/dev/mychar");
@@ -87,11 +97,17 @@ static void __exit my_cleanup(void)
     // Remove the character device node
     device_destroy(my_device->class, my_device->devno);
 
+    // Remove the character device from the system
+    cdev_del(&my_device->cdev);
+
     // Unregister the character device
-    unregister_chrdev_region(my_device->devno, MINOR_NUM);
+    unregister_chrdev_region(my_device->devno, 1);
 
     // Destroy the class
     class_destroy(my_device->class);
+
+    // Free the memory allocated for the character device buffer
+    kfree(my_device->buffer);
 
     // Free the memory allocated for the character device structure
     kfree(my_device);
@@ -137,52 +153,61 @@ int my_release(struct inode *inode, struct file *file)
 // Read method for the character device
 ssize_t my_read(struct file *filp, char __user *buf, size_t len, loff_t *off)
 {
-ssize_t retval;
+    ssize_t retval;
+    int count;
 
-// Check whether there are any characters left to read from the buffer
-if (*off >= BUFFER_SIZE || !atomic_read(&my_device->buffer[*off])) {
-    pr_warn("%s: No more data available to read\n", __func__);
-    return 0;
-}
+    // Check whether there are any characters left to read from the buffer
+    count = min(len, BUFFER_SIZE - (int)*off);
+    if (count <= 0) {
+        pr_warn("%s: No more data available to read\n", __func__);
+        return 0;
+    }
 
-// Copy the next character from the buffer into userspace
-get_user(retval, buf);
-put_user(atomic_read(&my_device->buffer[*off]), buf);
+    // Copy the data from the buffer into userspace
+    retval = copy_to_user(buf, my_device->buffer + *off, count);
+    if (retval) {
+        pr_err("%s: Failed to copy data to userspace\n", __func__);
+        return -EFAULT;
+    }
 
-// Update the offset and number of bytes remaining in the buffer
-++*off;
---len;
+    // Update the offset
+    *off += count;
 
-// Return the number of bytes copied
-return retval;
+    // Return the number of bytes copied
+    return count;
 }
 
 // Write method for the character device
 ssize_t my_write(struct file *filp, const char __user *buf, size_t len, loff_t *off)
 {
-ssize_t retval;
+    ssize_t retval;
+    int count;
 
-// Check whether we've reached the end of the buffer
-if (*off == BUFFER_SIZE) {
-    pr_warn("%s: Reached maximum capacity of buffer\n", __func__);
-    return 0;
-}
+    // Check whether we've reached the end of the buffer
+    count = min(len, BUFFER_SIZE - (int)*off);
+    if (count <= 0) {
+        pr_warn("%s: Reached maximum capacity of buffer\n", __func__);
+        return 0;
+    }
 
-// Copy the next character from userspace into the buffer
-put_user(get_user(retval, buf), &my_device->buffer[*off]);
+    // Copy the data from userspace into the buffer
+    retval = copy_from_user(my_device->buffer + *off, buf, count);
+    if (retval) {
+        pr_err("%s: Failed to copy data from userspace\n", __func__);
+        return -EFAULT;
+    }
 
-// Update the offset and number of bytes remaining in the buffer
-++*off;
---len;
+    // Update the offset
+    *off += count;
 
-// Return the number of bytes copied
-return retval;
+    // Return the number of bytes copied
+    return count;
 }
 
 // IOCTL method for the character device
 long my_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 {
-switch (cmd) {
+    switch (cmd) {
 case MYCHAR_IOC_MAGIC:
 // Handle custom ioctls here
 break;
@@ -191,5 +216,9 @@ default:
 return -ENOTTY;
 }
 
+
 return 0;
 }
+
+module_init(my_init);
+module_exit(my_cleanup);
