e5b25727aeb6c6b7c2e83a93e6b24baf344a2c46
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2024-05-06 18:01:12 +0700

    Update interrupts.c

--- kernel/interrupts.c
+++ kernel/interrupts.c
@@ -1,110 +1,130 @@
-#include <stdio.h>
 #include <stdint.h>
-#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/mman.h>
-#include <asm/io.h>
-#include <asm/interrupt.h>
+#include <pthread.h>
+#include <signal.h>
+#include <syslog.h>
 
 #define PAGE_SIZE 4096
-#define BUFFER_SIZE 1024
+#define IDT_SIZE 256
+#define MAX_PROCESSORS 4
 
-volatile uint8_t *idt;
-volatile uint8_t *gdt;
+struct idt_entry {
+    uint16_t offset_low;
+    uint16_t selector;
+    uint8_t zero;
+    uint8_t type_attr;
+    uint16_t offset_high;
+} __attribute__((packed));
 
-static inline void load_idt(uintptr_t address) {
-    asm volatile("lidt (%0)" : : "r"(address));
+struct idt_pointer {
+    uint16_t limit;
+    uintptr_t base;
+} __attribute__((packed));
+
+struct idt_entry idt[IDT_SIZE];
+struct idt_pointer idtp;
+
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+volatile bool keep_running = true;
+
+void load_idt(void *idt_ptr);
+void load_gdt(void *gdt_ptr);
+
+void set_idt_entry(int num, uintptr_t base, uint16_t sel, uint8_t flags) {
+    idt[num].offset_low = base & 0xFFFF;
+    idt[num].selector = sel;
+    idt[num].zero = 0;
+    idt[num].type_attr = flags;
+    idt[num].offset_high = (base >> 16) & 0xFFFF;
 }
 
-static inline void load_gdt(uintptr_t address) {
-    asm volatile("lgdt (%0)" : : "r"(address));
+void setup_idt() {
+    idtp.limit = sizeof(struct idt_entry) * IDT_SIZE - 1;
+    idtp.base = (uintptr_t)&idt;
+
+    memset(&idt, 0, sizeof(struct idt_entry) * IDT_SIZE);
+    // Setup default handler for all vectors
+    for (int i = 0; i < IDT_SIZE; i++) {
+        set_idt_entry(i, (uintptr_t)default_interrupt_handler, 0x08, 0x8E);
+    }
+
+    load_idt(&idtp);
 }
 
-static inline void set_vector(uint8_t vector, uintptr_t address) {
-    idt[vector * 8] = address & 0xFF;
-    idt[vector * 8 + 1] = (address >> 8) & 0xFF;
-    idt[vector * 8 + 2] = (address >> 16) & 0xFF;
-    idt[vector * 8 + 3] = (address >> 24) & 0xFF;
-}
-
-static inline void remap_interrupts(void (*handler)(struct interrupt_frame *)) {
-    // Remap interrupt vectors to our custom handler
-    set_vector(0x20, (uintptr_t)handler);
-    set_vector(0x21, (uintptr_t)handler + 1);
-    set_vector(0x22, (uintptr_t)handler + 2);
-    set_vector(0x23, (uintptr_t)handler + 3);
-}
-
-static inline void enable_interrupts() {
-    // Enable interrupts in the CPU
+void enable_interrupts() {
     asm volatile("sti");
 }
 
-static inline void disable_interrupts() {
-    // Disable interrupts in the CPU
+void disable_interrupts() {
     asm volatile("cli");
 }
 
-void custom_handler(struct interrupt_frame *frame) {
-    // Get the interrupt number from the frame
-    uint8_t vector = frame->vector;
-
-    // Check if the interrupt is a software interrupt (vector == 0x20)
-    if (vector == 0x20) {
-        // If it is, check the error code to determine what to do
-        uint32_t err_code = frame->error_code;
-
-        // Handle different software interrupts here
-        switch (err_code) {
-            case 0x12345678:
-                // Do something for this error code
-                break;
-            default:
-                // Default handling for unknown errors
-                break;
-        }
-    } else {
-        // Handle hardware interrupts here
-        switch (vector) {
-            case 0x21:
-                // Handle interrupt 0x21
-                break;
-            case 0x22:
-                // Handle interrupt 0x22
-                break;
-            case 0x23:
-                // Handle interrupt 0x23
-                break;
-        }
-    }
-
-    // Clear the interrupt flag in the CPU
-    asm volatile("sti");
+void default_interrupt_handler(void) {
+    syslog(LOG_INFO, "Default interrupt handler called.");
 }
 
-int main(void) {
-    // Allocate memory for the IDT and GDT
-    idt = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    gdt = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+void custom_interrupt_handler(void) {
+    syslog(LOG_INFO, "Custom interrupt handler executed.");
+}
 
-    // Set up the IDT and GDT
-    for (int i = 0; i < 256; i++) {
-        idt[i] = (i << 3) | 0x80000000;
-    }
+void signal_handler(int sig) {
+    pthread_mutex_lock(&lock);
+    keep_running = false;
+    pthread_mutex_unlock(&lock);
+    syslog(LOG_INFO, "Signal received, shutting down.");
+}
 
-    // Load the IDT and GDT
-    load_idt((uintptr_t)idt);
-    load_gdt((uintptr_t)gdt);
+void* processor_main(void* arg) {
+    int processor_id = *(int*)arg;
+    free(arg);
 
-    // Remap interrupt vectors to our custom handler
-    remap_interrupts(custom_handler);
-
-    // Enable interrupts in the CPU
+    setup_idt();
     enable_interrupts();
 
-    // Wait forever
-    while (true) {
-        // Handle incoming interrupts here
-        asm volatile("sti");
+    while (keep_running) {
+        pause();
     }
+
+    disable_interrupts();
+    syslog(LOG_INFO, "Processor %d: Interrupts disabled, shutting down safely.", processor_id);
+    return NULL;
 }
+
+int main(int argc, char* argv[]) {
+    openlog("system_program", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
+
+    pthread_t threads[MAX_PROCESSORS];
+    int i;
+
+    signal(SIGINT, signal_handler);
+
+    for (i = 0; i < MAX_PROCESSORS; i++) {
+        int* arg = malloc(sizeof(int));
+        *arg = i;
+        if (pthread_create(&threads[i], NULL, processor_main, arg) != 0) {
+            syslog(LOG_ERR, "Failed to create thread for processor %d", i);
+            free(arg);
+        }
+    }
+
+    for (i = 0; i < MAX_PROCESSORS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    closelog();
+    return 0;
+}
+
+__asm__(
+"load_idt:\n"
+"   lidt [rdi]\n"
+"   ret\n"
+
+"load_gdt:\n"
+"   lgdt [rdi]\n"
+"   ret\n"
+);
