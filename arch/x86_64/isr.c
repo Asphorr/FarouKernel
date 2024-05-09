144682e13e0dfeee3d6a33d23d9d3ecf38eecbe6
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2024-05-09 10:05:01 +0700

    Update isr.c

--- arch/x86_64/isr.c
+++ arch/x86_64/isr.c
@@ -106,3 +106,164 @@ void setup_isrs() {
     idt_set_gate(29, (uint64_t)isr29, 0x08, 0x8E);
     idt_set_gate(30, (uint64_t)isr30, 0x08, 0x8E);
     idt_set_gate(31, (uint64_t)isr31, 0x08, 0x8E);
+}
+
+/* Function to set an entry in the IDT */
+void idt_set_gate(uint8_t num, uint64_t base, uint16_t sel, uint8_t flags) {
+    extern struct idt_entry_t idt[];
+
+    idt[num].base_low = base & 0xFFFF;
+    idt[num].base_mid = (base >> 16) & 0xFFFF;
+    idt[num].base_high = (base >> 32) & 0xFFFFFFFF;
+    idt[num].sel = sel;
+    idt[num].always0 = 0;
+    idt[num].flags = flags | 0x60; // Set DPL to 3 for user-mode access if needed
+}
+
+/* IDT entry structure for 64-bit mode */
+struct idt_entry_t {
+    uint16_t base_low;
+    uint16_t sel;
+    uint8_t always0;
+    uint8_t flags;
+    uint16_t base_mid;
+    uint32_t base_high;
+    uint32_t reserved;
+};
+
+/* IDT pointer structure */
+struct idt_ptr_t {
+    uint16_t limit;
+    uint64_t base;
+} __attribute__((packed));
+
+/* Declare an IDT of 256 entries */
+struct idt_entry_t idt[256];
+struct idt_ptr_t idt_ptr;
+
+/* Load the IDT using the lidt instruction */
+extern void idt_load();
+
+/* Initialize the IDT */
+void idt_init() {
+    idt_ptr.limit = sizeof(idt) - 1;
+    idt_ptr.base = (uint64_t)&idt;
+
+    /* Clear out the entire IDT, initializing it to zero */
+    for (int i = 0; i < 256; ++i) {
+        idt[i].base_low = 0;
+        idt[i].base_mid = 0;
+        idt[i].base_high = 0;
+        idt[i].sel = 0;
+        idt[i].always0 = 0;
+        idt[i].flags = 0;
+        idt[i].reserved = 0;
+    }
+
+    /* Setup ISRs for the first 32 IRQs */
+    setup_isrs();
+
+    /* Load the IDT */
+    idt_load();
+}
+
+/* Assembly code to load the IDT */
+__asm__ (
+    ".global idt_load       \n"
+    "idt_load:              \n"
+    "    lea idt_ptr(%rip), %rdi \n"
+    "    lidt (%rdi)        \n"
+    "    ret                \n"
+);
+
+/* Assembly code for ISRs (example for `isr0`) */
+__asm__ (
+    ".global isr0           \n"
+    "isr0:                  \n"
+    "    cli                \n"
+    "    pushq $0           \n" /* Error code placeholder */
+    "    pushq $0           \n" /* Interrupt number */
+    "    pushq %rdi         \n" /* General purpose registers */
+    "    pushq %rsi         \n"
+    "    pushq %rbp         \n"
+    "    pushq %rsp         \n"
+    "    pushq %rbx         \n"
+    "    pushq %rdx         \n"
+    "    pushq %rcx         \n"
+    "    pushq %rax         \n"
+    "    movq %gs, %rax     \n"
+    "    pushq %rax         \n"
+    "    movq %fs, %rax     \n"
+    "    pushq %rax         \n"
+    "    movq %es, %rax     \n"
+    "    pushq %rax         \n"
+    "    movq %ds, %rax     \n"
+    "    pushq %rax         \n"
+    "    call isr_handler   \n"
+    "    popq %rax          \n" /* Restore segment registers */
+    "    movq %rax, %ds     \n"
+    "    popq %rax          \n"
+    "    movq %rax, %es     \n"
+    "    popq %rax          \n"
+    "    movq %rax, %fs     \n"
+    "    popq %rax          \n"
+    "    movq %rax, %gs     \n"
+    "    popq %rax          \n" /* Restore general purpose registers */
+    "    popq %rcx          \n"
+    "    popq %rdx          \n"
+    "    popq %rbx          \n"
+    "    popq %rsp          \n"
+    "    popq %rbp          \n"
+    "    popq %rsi          \n"
+    "    popq %rdi          \n"
+    "    addq $16, %rsp     \n" /* Remove error code and interrupt number */
+    "    sti                \n"
+    "    iretq              \n"
+);
+
+/* Repeat similar ISR assembly stubs for each ISR (isr1, isr2, ..., isr31) */
+
+/* Example for isr1 */
+__asm__ (
+    ".global isr1           \n"
+    "isr1:                  \n"
+    "    cli                \n"
+    "    pushq $0           \n"
+    "    pushq $1           \n"
+    "    pushq %rdi         \n"
+    "    pushq %rsi         \n"
+    "    pushq %rbp         \n"
+    "    pushq %rsp         \n"
+    "    pushq %rbx         \n"
+    "    pushq %rdx         \n"
+    "    pushq %rcx         \n"
+    "    pushq %rax         \n"
+    "    movq %gs, %rax     \n"
+    "    pushq %rax         \n"
+    "    movq %fs, %rax     \n"
+    "    pushq %rax         \n"
+    "    movq %es, %rax     \n"
+    "    pushq %rax         \n"
+    "    movq %ds, %rax     \n"
+    "    pushq %rax         \n"
+    "    call isr_handler   \n"
+    "    popq %rax          \n"
+    "    movq %rax, %ds     \n"
+    "    popq %rax          \n"
+    "    movq %rax, %es     \n"
+    "    popq %rax          \n"
+    "    movq %rax, %fs     \n"
+    "    popq %rax          \n"
+    "    movq %rax, %gs     \n"
+    "    popq %rax          \n"
+    "    popq %rcx          \n"
+    "    popq %rdx          \n"
+    "    popq %rbx          \n"
+    "    popq %rsp          \n"
+    "    popq %rbp          \n"
+    "    popq %rsi          \n"
+    "    popq %rdi          \n"
+    "    addq $16, %rsp     \n"
+    "    sti                \n"
+    "    iretq              \n"
+);
