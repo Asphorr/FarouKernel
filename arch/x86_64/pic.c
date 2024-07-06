208fe18d12fddb557e209e77a76c6d7f813331f6
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2024-07-06 22:06:39 +0300

    Update pic.c

--- arch/x86_64/pic.c
+++ arch/x86_64/pic.c
@@ -1,48 +1,101 @@
 #include <stdint.h>
-#include <io.h>
+#include <stdbool.h>
+#include "io.h"
 
-/* PIC Ports */
 #define PIC1_COMMAND 0x20
-#define PIC1_DATA 0x21
+#define PIC1_DATA    0x21
 #define PIC2_COMMAND 0xA0
-#define PIC2_DATA 0xA1
+#define PIC2_DATA    0xA1
 
-/* PIC Commands */
-#define PIC_EOI 0x20
+#define PIC_EOI      0x20
 
-/* Initialization Control Words (ICW) */
-#define ICW1_INIT 0x10
-#define ICW1_ICW4 0x01
-#define ICW4_8086 0x01
+#define ICW1_INIT    0x10
+#define ICW1_ICW4    0x01
+#define ICW4_8086    0x01
 
-/* Remap IRQs 0-15 to IDT entries 32-47 */
-void remap_pic() {
-    outb(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4);
-    outb(PIC2_COMMAND, ICW1_INIT | ICW1_ICW4);
-    outb(PIC1_DATA, 0x20);  /* Master PIC vector offset */
-    outb(PIC2_DATA, 0x28);  /* Slave PIC vector offset */
-    outb(PIC1_DATA, 0x04);  /* Tell Master PIC about Slave at IRQ2 */
-    outb(PIC2_DATA, 0x02);  /* Tell Slave PIC its cascade identity */
-    outb(PIC1_DATA, ICW4_8086);
-    outb(PIC2_DATA, ICW4_8086);
+#define PIC_MASTER_OFFSET 0x20
+#define PIC_SLAVE_OFFSET  0x28
 
-    /* Mask all interrupts */
-    outb(PIC1_DATA, 0xFF);
-    outb(PIC2_DATA, 0xFF);
+static inline void io_wait(void) {
+    outb(0x80, 0);
 }
 
-/* Send End-of-Interrupt (EOI) signal to PICs */
-void send_eoi(uint8_t irq) {
+static void pic_send_eoi(uint8_t irq) {
     if (irq >= 8) {
         outb(PIC2_COMMAND, PIC_EOI);
     }
     outb(PIC1_COMMAND, PIC_EOI);
 }
 
-/* Initialize the PICs and remap IRQs */
-void init_pic() {
-    remap_pic();
-    /* Unmask desired IRQs here (0: Timer, 1: Keyboard, etc.) */
-    outb(PIC1_DATA, 0xFC);  /* Unmask IRQ0 and IRQ1 */
-    outb(PIC2_DATA, 0xFF);  /* Mask all IRQs on Slave PIC */
+static void pic_remap(uint8_t offset1, uint8_t offset2) {
+    uint8_t a1, a2;
+
+    a1 = inb(PIC1_DATA);
+    a2 = inb(PIC2_DATA);
+
+    outb(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4);
+    io_wait();
+    outb(PIC2_COMMAND, ICW1_INIT | ICW1_ICW4);
+    io_wait();
+    outb(PIC1_DATA, offset1);
+    io_wait();
+    outb(PIC2_DATA, offset2);
+    io_wait();
+    outb(PIC1_DATA, 4);
+    io_wait();
+    outb(PIC2_DATA, 2);
+    io_wait();
+
+    outb(PIC1_DATA, ICW4_8086);
+    io_wait();
+    outb(PIC2_DATA, ICW4_8086);
+    io_wait();
+
+    outb(PIC1_DATA, a1);
+    outb(PIC2_DATA, a2);
+}
+
+void pic_set_mask(uint8_t irq) {
+    uint16_t port;
+    uint8_t value;
+
+    if (irq < 8) {
+        port = PIC1_DATA;
+    } else {
+        port = PIC2_DATA;
+        irq -= 8;
+    }
+    value = inb(port) | (1 << irq);
+    outb(port, value);
+}
+
+void pic_clear_mask(uint8_t irq) {
+    uint16_t port;
+    uint8_t value;
+
+    if (irq < 8) {
+        port = PIC1_DATA;
+    } else {
+        port = PIC2_DATA;
+        irq -= 8;
+    }
+    value = inb(port) & ~(1 << irq);
+    outb(port, value);
+}
+
+void pic_init(void) {
+    pic_remap(PIC_MASTER_OFFSET, PIC_SLAVE_OFFSET);
+    
+    // Mask all interrupts
+    outb(PIC1_DATA, 0xFF);
+    outb(PIC2_DATA, 0xFF);
+
+    // Unmask desired IRQs here
+    pic_clear_mask(0);  // Unmask timer interrupt
+    pic_clear_mask(1);  // Unmask keyboard interrupt
+}
+
+void pic_disable(void) {
+    outb(PIC1_DATA, 0xFF);
+    outb(PIC2_DATA, 0xFF);
 }
