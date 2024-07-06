3d103454bd7b7254249d78452e9a893f456b77c9
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2024-07-06 22:20:12 +0300

    Create irq.h

---
+++ kernel/arch/x86_64/interrupts/irq.h
@@ -0,0 +1,28 @@
+#ifndef IRQ_H
+#define IRQ_H
+
+#include <stdint.h>
+#include "../cpu/isr.h"
+
+#define IRQ0 32
+#define IRQ1 33
+#define IRQ2 34
+#define IRQ3 35
+#define IRQ4 36
+#define IRQ5 37
+#define IRQ6 38
+#define IRQ7 39
+#define IRQ8 40
+#define IRQ9 41
+#define IRQ10 42
+#define IRQ11 43
+#define IRQ12 44
+#define IRQ13 45
+#define IRQ14 46
+#define IRQ15 47
+
+void irq_init(void);
+void irq_install_handler(int irq, void (*handler)(interrupt_frame *r));
+void irq_uninstall_handler(int irq);
+
+#endif
