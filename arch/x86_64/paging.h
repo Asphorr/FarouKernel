0364440d576cbf64d854f5e6e8fb757e00b49dae
ferou <144871094+Asphorr@users.noreply.github.com>
2025-02-13 11:37:01 +0700

    Update paging.h

--- arch/x86_64/paging.h
+++ arch/x86_64/paging.h
@@ -1,8 +1,23 @@
-#ifndef PAGING_H
-#define PAGING_H
-
+// paging.h
+#pragma once
 #include <stdint.h>
+#include <stdbool.h>
 
-void setup_paging();
+#define PAGE_SIZE_2MB (2UL * 1024 * 1024)
+#define PAGE_SIZE_1GB (1UL * 1024 * 1024 * 1024)
+#define PAGE_NX       (1UL << 63)
+#define PAGE_PRESENT  (1UL << 0)
+#define PAGE_RW       (1UL << 1)
+#define PAGE_USER     (1UL << 2)
+#define PAGE_PAT      (1UL << 7)
 
-#endif /* PAGING_H */
+typedef enum {
+    PAGE_MAP_KERNEL_RW = PAGE_PRESENT | PAGE_RW,
+    PAGE_MAP_KERNEL_RX = PAGE_PRESENT,
+    PAGE_MAP_USER_RW = PAGE_PRESENT | PAGE_RW | PAGE_USER,
+    PAGE_MAP_USER_RX = PAGE_PRESENT | PAGE_USER
+} PageFlags;
+
+void paging_init(void);
+bool map_memory(uint64_t vaddr, uint64_t paddr, uint64_t size, PageFlags flags);
+void enable_nx_bit(void);
