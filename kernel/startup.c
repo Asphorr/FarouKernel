313dd6ff7cf7bf6343d168173ead31b7a3e74d61
ferou <144871094+Asphorr@users.noreply.github.com>
2025-02-20 13:14:58 +0700

    Update startup.c

--- kernel/startup.c
+++ kernel/startup.c
@@ -11,6 +11,9 @@
 
 #define STACK_SIZE 0x1000
 #define HEAP_SIZE 0x1000
+#define PF_R 0x1
+#define PF_W 0x2
+#define PF_X 0x4
 
 struct boot_params {
     uint32_t magic;
@@ -19,51 +22,72 @@ struct boot_params {
     uint32_t entry_point;
 };
 
-// Function to handle exiting with an error message
 void exit_with_error(const char *message) {
     perror(message);
     exit(EXIT_FAILURE);
 }
 
-// Maps a file into memory and returns the address
-char *map_file(const char *filename, size_t *size) {
-    int fd = open(filename, O_RDONLY);
-    if (fd < 0) {
+char *map_file(const char *filename, size_t *size, int *fd) {
+    *fd = open(filename, O_RDONLY);
+    if (*fd < 0) {
         exit_with_error("Error opening file");
     }
 
     struct stat st;
-    if (fstat(fd, &st) < 0) {
-        close(fd);
+    if (fstat(*fd, &st) < 0) {
+        close(*fd);
         exit_with_error("Error getting file size");
     }
 
     *size = st.st_size;
-    char *buffer = mmap(NULL, *size, PROT_READ, MAP_PRIVATE, fd, 0);
+    char *buffer = mmap(NULL, *size, PROT_READ, MAP_PRIVATE, *fd, 0);
     if (buffer == MAP_FAILED) {
-        close(fd);
+        close(*fd);
         exit_with_error("Error mapping file");
     }
 
-    close(fd);
     return buffer;
 }
 
-// Unmaps memory and checks for errors
 void unmap_file(char *buffer, size_t size) {
     if (munmap(buffer, size) < 0) {
         exit_with_error("Error unmapping memory");
     }
 }
 
-void load_kernel_segments(const Elf32_Ehdr *ehdr, int fd, const char *buffer) {
+void load_kernel_segments(const Elf32_Ehdr *ehdr, int fd, const char *buffer, size_t file_size) {
+    size_t page_size = sysconf(_SC_PAGESIZE);
     Elf32_Phdr *phdr = (Elf32_Phdr *)(buffer + ehdr->e_phoff);
     for (int i = 0; i < ehdr->e_phnum; ++i) {
         if (phdr[i].p_type == PT_LOAD) {
             void *va = (void *)phdr[i].p_vaddr;
-            size_t size = phdr[i].p_filesz;
-            if (mmap(va, size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_FIXED | MAP_PRIVATE, fd, phdr[i].p_offset) == MAP_FAILED) {
-                exit_with_error("Error mapping segment");
+            size_t filesz = phdr[i].p_filesz;
+            size_t memsz = phdr[i].p_memsz;
+
+            if (phdr[i].p_offset + filesz > file_size) {
+                exit_with_error("Segment extends beyond file size");
+            }
+
+            int prot = 0;
+            if (phdr[i].p_flags & PF_R) prot |= PROT_READ;
+            if (phdr[i].p_flags & PF_W) prot |= PROT_WRITE;
+            if (phdr[i].p_flags & PF_X) prot |= PROT_EXEC;
+
+            size_t start_page = (size_t)va / page_size * page_size;
+            size_t end_page = ((size_t)va + memsz + page_size - 1) / page_size * page_size;
+            size_t map_size = end_page - start_page;
+
+            if (mmap((void *)start_page, map_size, prot, MAP_FIXED | MAP_ANONYMOUS | MAP_PRIVATE, -1, 0) == MAP_FAILED) {
+                exit_with_error("Error mapping segment pages");
+            }
+
+            if (filesz > 0) {
+                if (lseek(fd, phdr[i].p_offset, SEEK_SET) == -1) {
+                    exit_with_error("Error seeking file");
+                }
+                if (read(fd, va, filesz) != filesz) {
+                    exit_with_error("Error reading file");
+                }
             }
         }
     }
@@ -89,26 +113,25 @@ void setup_memory() {
         fprintf(stderr, "Memory allocation failed\n");
         exit(EXIT_FAILURE);
     }
-
-    // Assume `current_process` structure available in the context
-    // current_process.stack = stack;
-    // current_process.heap = heap;
 }
 
 void load_kernel(const char *filename) {
+    int fd;
     size_t file_size;
-    char *buffer = map_file(filename, &file_size);
-  
+    char *buffer = map_file(filename, &file_size, &fd);
+
     Elf32_Ehdr *ehdr = (Elf32_Ehdr *)buffer;
     if (memcmp(ehdr->e_ident, ELFMAG, SELFMAG) != 0) {
         unmap_file(buffer, file_size);
+        close(fd);
         fprintf(stderr, "Invalid ELF header\n");
         exit(EXIT_FAILURE);
     }
-  
-    load_kernel_segments(ehdr, -1, buffer);
+
+    load_kernel_segments(ehdr, fd, buffer, file_size);
     start_kernel(ehdr, buffer);
     unmap_file(buffer, file_size);
+    close(fd);
 }
 
 int main(int argc, char *argv[]) {
