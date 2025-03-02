0b11454d6633fa7b41e475e41aaf02345d392035
ferou <144871094+Asphorr@users.noreply.github.com>
2025-03-03 00:04:21 +0700

    Update Boot.s

--- kernel/boot/Boot.s
+++ kernel/boot/Boot.s
@@ -1,41 +1,117 @@
 [org 0x7c00]
+[bits 16]
 
-; Save the boot drive number
-mov [BOOT_DRIVE], dl
+; Basic setup
+xor ax, ax
+mov ds, ax
+mov es, ax
+mov ss, ax
+mov sp, 0x7C00
+mov [boot_drive], dl
 
-; Check if CPUID is supported
-mov eax, 0x80000000
+; --- Check CPUID availability ---
+pushfd
+pop eax
+mov ebx, eax        ; Save original
+xor eax, 1 << 21    ; Flip ID bit
+push eax
+popfd
+pushfd
+pop eax
+cmp eax, ebx        ; Did it change?
+je .no_avx          ; If not, CPUID not supported
+
+; --- Check for AVX support ---
+mov eax, 1
 cpuid
-cmp eax, 0x80000001
-jb .no_avx
+bt ecx, 28          ; Test AVX bit
+jnc .no_avx
+bt ecx, 26          ; Test OSXSAVE bit
+jnc .no_avx
 
-; Check if AVX is supported
-mov eax, 0x80000001
-cpuid
-test ecx, 1 << 28
-jz .no_avx
-
-; Enable AVX
+; --- Enter Protected Mode ---
+cli
+lgdt [gdt_ptr]
 mov eax, cr0
-and ax, 0xFFFB      ; Clear coprocessor emulation CR0.EM
-or ax, 0x2          ; Set coprocessor monitoring  CR0.MP
+or al, 1
 mov cr0, eax
-mov eax, cr4
-or ax, 3 << 9       ; Set CR4.OSFXSR and CR4.OSXMMEXCPT at the same time
-mov cr4, eax
+jmp dword 0x08:.pm_enter
 
-; Now you can use AVX instructions
+[bits 32]
+.pm_enter:
+    ; Set up segments
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov esp, 0x9000
 
-.no_avx:
-    ; Print an error message
-    mov bx, NO_AVX_MSG
-    call print_string
+    ; --- Enable AVX ---
+    ; Enable XSAVE
+    mov eax, cr4
+    or eax, 1 << 18    ; CR4.OSXSAVE
+    mov cr4, eax
+
+    ; Set XCR0 to enable x87, SSE, AVX states
+    xor ecx, ecx       ; XCR0 register index
+    xgetbv
+    or eax, 7          ; Enable bits 0-2 
+    xsetbv
+
+    ; AVX is now enabled!
+    ; Example: Zero YMM0 register
+    vpxor ymm0, ymm0, ymm0
+    
     jmp $
 
-%include "print_string.asm"
+[bits 16]
+.no_avx:
+    mov si, msg_error
+    call print_string
+    cli
+    hlt
+    jmp $
 
-BOOT_DRIVE: db 0
-NO_AVX_MSG: db 'Error: CPU does not support AVX.', 0
+; String printing routine
+print_string:
+    lodsb
+    test al, al
+    jz .done
+    mov ah, 0x0E
+    int 0x10
+    jmp print_string
+.done:
+    ret
 
+; --- Data ---
+boot_drive db 0
+msg_error db "Error: AVX not available", 0
+
+; --- GDT (Global Descriptor Table) ---
+gdt:
+    dq 0                ; Null descriptor
+    ; Code segment (0x08)
+    dw 0xffff           ; Limit (0-15)
+    dw 0                ; Base (0-15)
+    db 0                ; Base (16-23)
+    db 10011010b        ; Access
+    db 11001111b        ; Flags + Limit (16-19)
+    db 0                ; Base (24-31)
+    ; Data segment (0x10)
+    dw 0xffff
+    dw 0
+    db 0
+    db 10010010b
+    db 11001111b
+    db 0
+gdt_end:
+
+gdt_ptr:
+    dw gdt_end - gdt - 1  ; Size
+    dd gdt                ; Offset
+
+; Boot signature
 times 510-($-$$) db 0
 dw 0xaa55
